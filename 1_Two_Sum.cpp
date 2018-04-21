58a2b4cebfd6ac24a14e543c4ed180b8cc76cd1b
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-04-21 20:25:02 +0800

    0421

---
+++ 1_Two_Sum.cpp
@@ -0,0 +1,117 @@
+/*1. Two Sum
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]
+
+*/
+
+/*49. Group Anagrams
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[["ate","eat","tea"], ["nat","tan"], ["bat"]]
+
+*/
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+/*一开始想多了，这个target必须是由两个数字构成，不可能出现2,3,4的情况*/
+// vector<int> twoSum(vector<int>& nums, int target)
+// {
+	// unordered_map<int,int>temp;
+	// vector<int>res;
+	// for(int i=0;i<nums.size();i++)
+	// {
+		// temp[nums[i]]=i;
+	// }
+	
+	
+	// for(int i=0;i<nums.size();i++)
+	// {
+		// int t = target-nums[i];
+		// if(temp.count(t) && temp[t]!=i)
+		// {
+			// cout<<temp.count(t)<<endl;
+			// res.push_back(i);
+			// res.push_back(temp[t]);
+			// break;
+		// }
+	// }
+	// return res;
+// }
+
+/*给for循环强加条件来优化代码，比如nums[i]<=target,但是没有考虑到负数！另外find()和count()函数都是判断是否存在这个数，
+，*/
+vector<int> twoSum(vector<int>& nums, int target)
+{
+	unordered_map<int,int>temp;
+	vector<int>res;
+	for(int i=0;i<nums.size();i++)
+	{
+		temp[nums[i]]=i;
+	}
+	
+	
+	for(int i=0;i<nums.size();i++)
+	{
+		auto t = temp.find(target-nums[i]); /*先找到这个数，判断是否存在，且位置坐标不是当前循环的i的位置，因为是同一个数，闹乌龙了，然后添加到res里面*/
+		if(t!=temp.end() && t->second != i)
+		{
+			res.push_back(i);
+			res.push_back(t->second);
+			break;
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	vector<int>nums = {0,4,3,0};
+	vector<int>res_inde;
+	int t = 0;
+	res_inde = twoSum(nums,t);
+	for(auto i:res_inde)
+		cout<<i<<" ";
+	
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
