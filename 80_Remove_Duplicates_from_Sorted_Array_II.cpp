1c6e2785fa1cd99a89637c1194f3911a2977a2b8
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-10-25 21:03:10 +0800

    modify 80 commit

---
+++ 80_Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int removeDuplicates(vector<int>&nums)
+{
+    int count = 1,pre = 0,cur = 0;
+    while(cur<nums.size())
+    {
+        if(nums[cur]==nums[pre] && count==0) ++cur;
+        else
+        {
+            if(nums[pre]==nums[cur]) --count;
+            else count=1;
+            nums[++pre] = nums[cur++];
+        }
+    }
+    return pre+1;
+}
+
+
+
+int main()
+{
+    vector<int> res = {1,1,2,3,4,4,4,5,6};
+    int j = removeDuplicates(res);
+    for(int i = 0;i<j;i++)
+    {
+        cout<<res[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
