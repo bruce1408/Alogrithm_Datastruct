db94983093b6aa0c982dcd33003ef1f11b69038d
bruce1408 <summer56567@163.com>
2018-10-25 20:55:02 +0800

    26 modify commit

---
+++ 26_remove_Duplicates_from_sorted_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// 去掉数组中重复的数字，返回的是非重复数组的长度；
+int findDuplicate(vector<int>& nums)
+{
+    int j=0,n = nums.size();
+    for(int i=0;i<n;i++)
+    {
+        if(nums[i]!=nums[j])
+        {
+            nums[++j] = nums[i];
+        }
+    }
+    return j+1;
+}
+
+int main()
+{
+    vector<int> num = {1,1,2,2,3,4,5,5,6};
+    int i = findDuplicate(num);
+    for(int j = 0;j<i;j++)
+    {
+        cout<<num[j]<<" ";
+    }
+}
