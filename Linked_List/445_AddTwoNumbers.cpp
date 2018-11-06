29b248c141e3748e3f511536674fa6d698f4cbc7
bruce1408 <summer56567@163.com>
2018-11-06 20:01:44 +0800

    add sort file

--- Linked_List/445_AddTwoNumbers.cpp
+++ Linked_List/445_AddTwoNumbers.cpp
@@ -1,8 +1,17 @@
 /*两个链表相加的第二种类型
   用到了栈的概念，这里用容器，也是一样的效果。
   这种方法一定要掌握！*/
+#include<iostream>
+#include<vector>
+using namespace std;
+ struct ListNode
+ {
+     int val;
+     ListNode *next;
+     ListNode(int x):val(x),next(nullptr){}
+ };
 
-  
+ 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -19,21 +28,21 @@ public:
     }
 
     int m = nums1.size(), n = nums2.size();
-    int sum = 0, carry = 0;
+    int sumResult = 0, carry = 0;
 
     ListNode *head = nullptr, *p = nullptr;
 	/*算法的核心部分：*/
     for(int i = m - 1, j = n - 1; i >= 0 || j >= 0 || carry > 0; i--, j--) {
-        sum = carry;
+        sumResult = carry;
         if(i >= 0) 
-            sum += nums1[i];
+            sumResult += nums1[i];
 
         if(j >= 0)
-            sum += nums2[j];
+            sumResult += nums2[j];
 
-        carry = sum / 10;
+        carry = sumResult / 10;
 
-        p = new ListNode(sum%10);
+        p = new ListNode(sumResult%10);
 		/*头部插入算法！*/
         p->next = head;
         head = p;
