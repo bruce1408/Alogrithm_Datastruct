030e9b12c41a261095846098aef42f1aeae331c3
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-11-06 19:29:56 +0800

    148 sortList

--- Linked_List/148_Sort_list.cpp
+++ Linked_List/148_Sort_list.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 
+// 综合题目，对一个链表排序，利用归并方法进行排序；
 struct ListNode
 {
     int val;
     ListNode *next;
-    // ListNode(int x):val(x),next(nullptr){}
-    // ListNode(int x, ListNode* next_):val(x),next(next_){}
+    ListNode(int x):val(x),next(nullptr){}
+    ListNode(int x, ListNode* next_):val(x),next(next_){}
 };
 
 
@@ -37,6 +38,7 @@ ListNode * merge(ListNode *l1, ListNode *l2)
         return l2;
     }
 }
+// 方法一；递归且用归并排序
 ListNode * sortList(ListNode * head)
 {
     if(head==nullptr|| head->next==nullptr) return head;
@@ -53,13 +55,57 @@ ListNode * sortList(ListNode * head)
     return merge(sortList(head),sortList(slow));
 }
 
+
+// 方法二： 不用递归，归并排序
+
+ListNode *merge2(ListNode*l1, ListNode *l2)
+{
+    if(!l1) return l2;
+    if(!l2) return l1;
+    ListNode *dummy = new ListNode(0);
+    ListNode *tail = dummy;
+    while(l1 && l2)
+    {
+        if(l1->val < l2->val)
+        {
+            tail->next = l1;
+            l1 = l1->next;
+        }
+        else
+        {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    if(l1||l2) l1?tail->next = l1:tail->next = l2;
+
+    return dummy->next;
+}
+ListNode*sortList2(ListNode*head)
+{
+    if(head==nullptr || head->next==nullptr) return head;
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while(fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    ListNode *mid = slow->next;
+    slow->next = nullptr;
+    print_list(head);
+    print_list(mid);
+    return merge2(sortList2(head), sortList2(mid));
+
+}
 int main()
 {
-	ListNode e1 = {0,nullptr};
-	ListNode d1 = {4,&e1};
-	ListNode c1 = {3,&d1};
-	ListNode b1 = {5,&c1};
-	ListNode a1 = {-1,&b1};
+	ListNode e1(0,nullptr);
+	ListNode d1(4,&e1);
+	ListNode c1(3,&d1);
+	ListNode b1(5,&c1);
+	ListNode a1(-1,&b1);
 	ListNode *head = &a1;
-    print_list(sortList(head));
+    print_list(sortList2(head));
 }
