4c2206f48a952345ed9f708dcdc7cfac6d57e4da
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-11-26 21:54:41 +0800

    11_26

--- String/28_Implement_strStr.cpp
+++ String/28_Implement_strStr.cpp
@@ -10,39 +10,41 @@ int strStr(string haystack, string needle)
     if(needle.empty()) return 0;
     int n = haystack.size(), m = needle.size();
     if(m>n) return -1;
-    for(int i=0;i<n;i++) // n = 5, m=2
+    /*
+        for(int i=0;i<n;i++) // 原来的话是遍历整个n长度的字符串，但是现在我不是遍历那么长的
+        我只要遍历 n-m + 1长度的即可，遍历到剩下的长度和子字符串相等的位置即可,因为可以提高运算效率.
+    */
+    for(int i=0;i<n-m+1;i++) // 这个是唯一我改的地方 就是i的取值范围
     {
-        int count = m;
         int j=i;
         int k = 0;
         while(haystack[j]==needle[k] && k<m)
         {
             j++;
             k++;
-            count--;
         }
-        if(count==0) return j-m;
+        if(k==needle.size()) return i;
     }
     return -1;
 }
 
 // 优化算法，时间4ms
-int strStr(string haystack, string needle) 
-{
-    if (needle.empty()) return 0;
-    int m = haystack.size(), n = needle.size();
-    if (m < n) return -1;
-    for (int i = 0; i <= m - n; ++i) 
-    {
-        int j = 0;
-        for (j = 0; j < n; ++j) 
-        {
-            if (haystack[i + j] != needle[j]) break;
-        }
-        if (j == n) return i;
-    }
-    return -1;
-}
+// int strStr(string haystack, string needle) 
+// {
+//     if (needle.empty()) return 0;
+//     int m = haystack.size(), n = needle.size();
+//     if (m < n) return -1;
+//     for (int i = 0; i <= m - n; ++i) 
+//     {
+//         int j = 0;
+//         for (j = 0; j < n; ++j) 
+//         {
+//             if (haystack[i + j] != needle[j]) break;
+//         }
+//         if (j == n) return i;
+//     }
+//     return -1;
+// }
 
 int main()
 {
