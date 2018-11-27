6f012608160246151063676276826aa24150b4b0
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-11-28 02:01:15 +0800

    11_27

---
+++ Math/268_Missing_Number.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+// 方法一：利用n项和
+int missingNumber(vector<int>& nums)
+{
+    int n = nums.size();
+    int sum = (n+1)*n/2;
+    for(int i=0;i<nums.size();i++)
+    {
+        sum-=nums[i];
+    }
+    return sum;
+}
+
+// 方法二：利用位运算！
+int missingNumber1(vector<int>& nums) 
+{
+    int res = 0;
+    for (int i = 0; i < nums.size(); ++i) res ^= (i + 1) ^ nums[i];
+    return res;
+}
+
+// 方法三：二分查找来做。
+int main()
+{
+    vector<int>nums = {3,0,1};
+    cout<<missingNumber(nums);
+    return 0;
+}
