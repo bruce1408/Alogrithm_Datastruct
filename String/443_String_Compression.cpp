ca412170368c26b39ac0d78f03a98010cbeeaceb
bruce1408 <cdd16@mails.tsinghua.edu.cn>
2018-12-02 00:58:28 +0800

    12_1

--- String/443_String_Compression.cpp
+++ String/443_String_Compression.cpp
@@ -1,28 +1,55 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 #include<string>
 using namespace std;
 
+/*
+    字符串压缩，
+    Input:
+    ["a","a","b","b","c","c","c]
+
+    Output:
+    ["a","2","b","2","c","3"]
+*/
 // int compress(vector<char>& chars) 
 // {
 //     int n = chars.size();
-//     for(int i=0;i<n;i++)
+//     int cur = 0;
+//     for(int i=0; i<n; i++)
 //     {
-//         int j=i+1;
-//         while(j<n)
-//         {
-//             if(chars[i]=)
-//           }
-
+//         int j=i;
+//         while(j<n-1 && )
 //     }
 // }
 
+
+int compress(vector<char>& chars) 
+{
+    const int n = chars.size();
+    int p = 0;
+    for (int i = 1; i <= n; ++i) 
+    {
+        int count = 1;
+        while (i < n && chars[i] == chars[i - 1]) { ++i; ++count; }
+        chars[p++] = chars[i - 1];
+        if (count == 1) continue;
+        for (char c : to_string(count))
+        chars[p++] = c;
+    }
+    for(auto i:chars)
+    {
+        cout<<i<<" ";
+    }
+    return p;
+}
+
+
 int main()
 {
-    vector<int>res = {1,2,3};
-    for(auto i:res)
-    {
-        cout<<i<<endl;
-    }
+    vector<char>res = {'a','a','a','b','b'};
+    cout<<compress(res);
+    // for(auto i:res)
+    // {
+    //     cout<<i<<endl;
+    // }
 }
