2e0395d594834f2ce87a660220f0ad183b7448ad
bruce1408 <summer56567@163.com>
2019-05-27 01:32:15 +0800

    rotate array

--- extraCode/11_rotate_min_num.cpp
+++ extraCode/11_rotate_min_num.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 /**
  * 题目： 数组开始的若干位旋转到数组的末尾，输入时一个部分递增的数组，例如[3,4,5,1,2]
  * 旋转后是[1,2,3,4,5]，那么最小的旋转数字是 1, 时间复杂度是O(logN)
  * 方法 
- * 思路：
+ * 思路：首先明确一点，就是这个判断最小的翻转的数肯定是要和之前的一个数比较才能看出是最小，那么这个比较的话，必须是两个不同的数，
+ * 所以我们认为是left和right之间相差1；我们有一个mid中位数，如果下标是中位数的数大于左边的数，那么说明最小的数不在这个范围区间，
+ * 如果下标的中位数小于右边的数，那么说明最小的数不在右边的范围区间，因为他们都是升序连续的数组，然后不断的尝试，最后找到了这两个数，比如是 5，1 那么
+ * 我们只需要取最小的数即可；
  * 发散：leetcode 153. Find Minimum in Rotated Sorted Array
 */
+
+int min_num1(vector<int> res) // 本题目的最好的解法！
+{
+	int left = 0, right = res.size()-1;
+	if(res[left]<res[right]) return res[0];
+	else
+	{
+		int mid = (left + right)/2;
+		if(res[mid]>res[left]) left = mid;
+		else right = mid;
+	}
+	return min(res[left], res[right]);
+}
 
 // void sortAges(vector<int> &res)
 // {
@@ -38,7 +55,7 @@ using namespace std;
 // 	}
 // }
 
-int min_num(vector<int> res)
+int min_num2(vector<int> res)
 {
 	int n = res.size();
 	if(n<=0) return 0;
@@ -66,7 +83,7 @@ int main()
 	// vector<int>res = {3,4,5,1,2};
 	vector<int> res = {4, 5, 6, 7, 0, 1, 2};
 	// sortAges(res);
-	cout<<min_num(res)<<endl;	
+	cout<<min_num1(res)<<endl;	
 }
 
 
