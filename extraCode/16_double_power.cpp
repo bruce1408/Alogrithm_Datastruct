13ad1aa0f77d5c93837c20d7cb4dda657642288b
bruce1408 <summer56567@163.com>
2019-05-27 18:47:47 +0800

    cjob_16

--- extraCode/16_double_power.cpp
+++ extraCode/16_double_power.cpp
@@ -1,13 +1,20 @@
-/*	一是考虑边界条件，负面测试，功能测试
-	二是尽量不用乘除操作，而是用与，或操作来替换，这样效率会很高
-	三是 左移相当于除2，右移是乘以2，和1做位与操作可以判断奇数还是偶数，异或也可以。
-*/
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 using namespace std;
+
+/**
+ * 题目：数值的整数次方,不是用函数power，而是直接求解数值的帧数次方
+ * 方法 
+ * 思路：
+ * 一是考虑边界条件，负面测试，功能测试
+ * 二是尽量不用乘除操作，而是用与，或操作来替换，这样效率会很高
+ * 三是 左移相当于除2，右移是乘以2，和1做位与操作可以判断奇数还是偶数，异或也可以。
+*/
+
 //方法1，没有考虑指数是负数的情况；
-double num_N(double num,double n)
+double num_N(double num, double n)
 {
 	if(n>0)
 	{
@@ -31,7 +38,6 @@ double num_N(double num,double n)
 		return 1.0;
 }
 
-
 //方法2：
 // bool g_Invaildinput = false;
 // double powerNum(double base,int exponent)
@@ -39,18 +45,15 @@ double num_N(double num,double n)
 	// g_Invaildinput = false;
 // }
 
-
-
-
 int main()
 {
-	solution 1;
+	// solution 1;
 	double n;
 	double num;
 	cout<<"plea input a num"<<endl;
 	cin>>num;
 	cout<<"plea input a n"<<endl;
 	cin>>n;
-	cout<<"the num:"<<num<<": ^n is:"<<num_N(num,n);
+	cout<<"the num:"<<num<<": ^n is:"<<num_N(num,n)<<endl;
 
 }
