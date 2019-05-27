744cbe8d2e3bb446ab71a33e240cfd9ee3001210
bruce1408 <summer56567@163.com>
2019-05-27 21:29:01 +0800

    0527

--- extraCode/16_double_power.cpp
+++ extraCode/16_double_power.cpp
@@ -10,15 +10,15 @@ using namespace std;
  * 思路：
  * 一是考虑边界条件，负面测试，功能测试
  * 二是尽量不用乘除操作，而是用与，或操作来替换，这样效率会很高
- * 三是 左移相当于除2，右移是乘以2，和1做位与操作可以判断奇数还是偶数，异或也可以。
+ * 三是 左移相当于乘以 2，右移是除以 2，和 1 做位与操作可以判断奇数还是偶数，异或也可以。
 */
 
-//方法1，没有考虑指数是负数的情况；
+// 时间复杂度是O(n)
 double num_N(double num, double n)
 {
 	if(n>0)
 	{
-		vector<double>temp(n+1,0.0);
+		vector<double>temp(n+1, 0.0);
 		temp[0] = 0;
 		temp[1] = num;
 		temp[2] = num*num;
@@ -32,28 +32,32 @@ double num_N(double num, double n)
 		return 0.0;
 	else if(n<0)
 	{
-		return 1.0/num_N(num,abs(n));
+		return 1.0/num_N(num, abs(n));
 	}
 	else
 		return 1.0;
 }
 
-//方法2：
-// bool g_Invaildinput = false;
-// double powerNum(double base,int exponent)
-// {
-	// g_Invaildinput = false;
-// }
+// 方法2：
+double myPow(double x, int n)
+{
+	if(n==0) return 1.0;
+	double half = myPow(x, n/2);
+	if(n%2==0) return half * half;
+	if(n>0) return half * half * x;
+	return half * half /x;
+}
 
 int main()
 {
 	// solution 1;
-	double n;
-	double num;
-	cout<<"plea input a num"<<endl;
-	cin>>num;
-	cout<<"plea input a n"<<endl;
-	cin>>n;
-	cout<<"the num:"<<num<<": ^n is:"<<num_N(num,n)<<endl;
+	// double n;
+	// double num;
+	// cout<<"plea input a num"<<endl;
+	// cin>>num;
+	// cout<<"plea input a n"<<endl;
+	// cin>>n;
+	// cout<<"the num:"<<num<<": ^n is:"<<num_N(num,n)<<endl;
+	cout<<myPow(4.0, -3)<<endl;
 
 }
