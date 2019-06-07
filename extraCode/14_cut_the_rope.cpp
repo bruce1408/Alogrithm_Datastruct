3f28c7a218ed7b13aecd69bd6aba45a958a43823
bruce1408 <summer56567@163.com>
2019-06-07 21:09:59 +0800

    dp

--- extraCode/14_cut_the_rope.cpp
+++ extraCode/14_cut_the_rope.cpp
@@ -16,6 +16,7 @@ int getfunc_N_num(int n)
 	if(n==2) return 1;
 	if(n==3) return 2;
 	vector<int>temp(n+1,0);
+	// 这些都是子问题的最优解，可以认为是不需要剪切的绳子长度；
 	temp[0]=0;
 	temp[1]=1;
 	temp[2]=2;
@@ -24,18 +25,38 @@ int getfunc_N_num(int n)
 	for(int i=4;i<=n;i++) //从4开始计算。前面都已经有了结果
 	{
 		max_num=0;
-		for(int j=1;j<n/2;j++)
+		for(int j=1;j<=n/2;j++)
 		{
 			max_num = max(temp[j]*temp[i-j],max_num);
-			temp[i] = max_num;
 		}
+		temp[i] = max_num;
 	}
-	// max_num = temp[n]; 可以取到n,不用再次赋值
 	return max_num;
+}
 
+// 方法2 使用规律，证明一个数，最多是拆成两个2和
+int maxProduct(int n)
+{
+	// boundary condition
+	if(n==3) return 2;
+	if(n==2) return 1;
+	if(n<2) return 0;
+
+	int res = 1;
+	if(n%3==1) res *= 4, n-=4; // 如果是余1的话，那么就是两个2相乘，然后再减去4
+	if(n%3==2) res *= 2, n-=2; // 如果是余2的话，那么就是只有1个2，减去2；
+	while(n)
+	{
+		n/=3;
+		res*=3;
+	}
+	return res;
 }
 
 int main()
 {
-	cout<<getfunc_N_num(4)<<endl;
+	cout<<getfunc_N_num(8)<<endl;
+
+	cout<<maxProduct(4)<<endl;
 }
+
