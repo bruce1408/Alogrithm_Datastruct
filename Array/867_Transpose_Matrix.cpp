e07238f1209bfb6a5327a77d2b1f0059a2a08e2c
bruce1408 <summer56567@163.com>
2019-09-03 23:15:06 +0800

    0903

--- Array/867_Transpose_Matrix.cpp
+++ Array/867_Transpose_Matrix.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 using namespace std;
 
-
 /**
  * Given a matrix A, return the transpose of A.
  * The transpose of a matrix is the matrix flipped over 
@@ -17,11 +16,20 @@ using namespace std;
  * Output: [[1,4],[2,5],[3,6]]
  * 转置矩阵
 */
+
+/**
+ * 方法 1 最开始想的就是把原来矩阵转置，即 行和列互换然后赋值
+ * 也就是说，n x n 的一个矩阵是没问题的，但是如果行和列不相同
+ * 就没办法了，所以新建一个矩阵，行列互换。然后开始赋值操作即可。
+ * 学习一下二维数组的初始化
+*/
 vector<vector<int>> transpose(vector<vector<int>> &A)
 {
     int m = A.size(), n = A[0].size();
 
-    vector<vector<int>> res(n, vector<int>(m));
+    // vector<vector<int>> res(n, vector<int>(m));
+    vector<vector<int>> res(n, vector<int>(m, 0));
+
     for (int i = 0; i < m; ++i)
     {
         for (int j = 0; j < n; ++j)
@@ -31,7 +39,6 @@ vector<vector<int>> transpose(vector<vector<int>> &A)
     }
     return res;
 }
-
 
 int main()
 {
