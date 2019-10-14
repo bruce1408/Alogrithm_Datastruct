f9bfa5f01168f92c722a1e191097984d4932bf7b
bruce1408 <summer56567@163.com>
2019-10-14 21:48:49 +0800

    1014

---
+++ Array/215_Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+using namespace std;
+
+/**
+ * Find the kth largest element in an unsorted array. 
+ * Note that it is the kth largest element in the sorted order,
+ * not the kth distinct element.
+ * 
+ * Example 1:
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ * Example 2:
+ * 
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ * 找到一个数组的第k个最大的数字
+**/ 
+
+int findKthLargest(vector<int>& nums, int k) {
+    int len = nums.size();
+    priority_queue<int>mypq;
+    for(int i=0;i<len;i++)
+    {
+        mypq.emplace(nums[i]);
+    }
+    for(int i=0;i<k-1;i++)
+    {
+        mypq.pop();
+    }
+    return mypq.top();
+
+    
+}
+
+int main()
+{
+    vector<int> nums = {3,2,3,1,2,4,5,5,6};
+    cout << findKthLargest(nums, 4) << endl;
+
+    return 0;
+}
