94fd1d49121239891d23ed0b01c179046041a2d6
bruce1408 <summer56567@163.com>
2019-12-04 10:06:24 +0800

    1204mac

--- Array/989_Add_to_Array-Form_of_Integer.cpp
+++ Array/989_Add_to_Array-Form_of_Integer.cpp
@@ -29,7 +29,7 @@ vector<int> addToArrayForm(vector<int> &A, int K)
         sumNum = sumNum * 10 + A[i];
     }
     sumNum = sumNum + K;
-    
+
     vector<int> res;
     stack<int> temp;
     while (sumNum)
@@ -42,14 +42,95 @@ vector<int> addToArrayForm(vector<int> &A, int K)
         res.push_back(temp.top());
         temp.pop();
     }
+    if (res.empty())
+    {
+        return {0};
+    }
     return res;
+}
+
+/**
+ * 方法 2，利用的是每一位相加，然后有进位
+*/
+vector<int> addToArrayForm2(vector<int> &A, int K)
+{
+    vector<int> temp;
+    vector<int> res;
+    int n = A.size();
+    while (K)
+    {
+        temp.push_back(K % 10);
+        K = K / 10;
+    }
+    for (auto i : temp)
+    {
+        cout << i << endl;
+    }
+    int digit = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int num = 0;
+        if (n - i - 1 < temp.size())
+        {
+            num = temp[n - i - 1] + A[i] + digit;
+        }
+        else
+        {
+            num = A[i] + digit;
+        }
+
+        if (num >= 10 && i == 0)
+        {
+            A.insert(A.begin(), 1, num / 10);
+            num = num % 10;
+            A[1] = num;
+        }
+
+        else if (num >= 10 && i > 0)
+        {
+            A[i] = num % 10;
+            digit = 1;
+        }
+        else if (num < 10)
+        {
+            A[i] = num;
+            digit = 0;
+        }
+    }
+    if (temp.size() > A.size())
+    {
+        for (int i = 0; i < temp.size() - n; i++)
+        {
+            A.insert(A.begin(), 1, temp[n + i] + digit);
+            digit = 0;
+        }
+    }
+    return A;
+}
+/**
+ * 方法 3，不用新的数组，直接在原来的数组上进行操作，最优方法
+*/
+vector<int> addToArrayForm3(vector<int> &A, int K)
+{
+    for (int i = A.size() - 1; i >= 0 && K > 0; i--)
+    {
+        A[i] += K;
+        K = A[i] / 10;
+        A[i] = A[i] % 10;
+    }
+    while (K > 0)
+    {
+        A.insert(A.begin(), K % 10);
+        K = K / 10;
+    }
+    return A;
 }
 
 int main()
 {
-    vector<int> res = {1, 2, 0, 0};
-    for(auto i:addToArrayForm(res, 34))
+    vector<int> res = {6};
+    for (auto i : addToArrayForm3(res, 809))
     {
-        cout<<i<<" ";
+        cout << i << " ";
     }
 }
