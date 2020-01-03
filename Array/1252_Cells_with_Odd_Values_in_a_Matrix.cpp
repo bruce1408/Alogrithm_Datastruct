3a637dd4cf9903f243c8c335cba2046049e437cf
bruce1408 <summer56567@163.com>
2020-01-04 00:24:48 +0800

    0103 1252

--- Array/1252_Cells_with_Odd_Values_in_a_Matrix.cpp
+++ Array/1252_Cells_with_Odd_Values_in_a_Matrix.cpp
@@ -52,7 +52,11 @@ int oddCells(int n, int m, vector<vector<int>> &indices)
 
 /**
  * 方法 2, 奇数偶数判断我们使用位运算来做也是可以的
- * 
+ * 异或的话是奇数次是1，偶数次的话就是0，所以最后行表示加了偶数次还是
+ * 奇数次，列也同样可以表示成这样；
+ * 最后行的每一个元素和列的每一个元素再次异或操作，得到的就是整个
+ * 数组的异或结果 
+ * O(n * m + k)
 */
 int oddCells1(int n, int m, vector<vector<int>> &indices)
 {
@@ -74,9 +78,36 @@ int oddCells1(int n, int m, vector<vector<int>> &indices)
     return countNum;
 }
 
+/**
+ * 方法 3，利用特殊的公式来计算，奇数行*偶数列 + 偶数行*奇数列
+ * 就是最后的数组包含的奇数的元素，因为只有奇数行，偶数列 或者是
+ * 偶数行 奇数列才能构成一个数组是奇数的元素；
+*/
+int oddCells2(int n, int m, vector<vector<int>> &indices)
+{
+    vector<int> row(n);
+    vector<int> column(m);
+    int countNum = 0;
+    int contRow = 0, contCol = 0;
+    for (auto element : indices)
+    {
+        row[element[0]] ^= 1;
+        column[element[1]] ^= 1;
+    }
+    for(auto x:row)
+    {
+        contRow += x ? 1:0;
+    }
+    for(auto y:column)
+    {
+        contCol += y ? 1:0;
+    }
+    return  (n-contRow)*contCol + (m-contCol)*contRow;
+}
+
 int main()
 {
     vector<vector<int>> indices = {{0, 1}, {1, 1}};
     cout << oddCells(2, 3, indices) << endl;
-    cout << oddCells1(2, 3, indices) << endl;
+    cout << oddCells2(2, 3, indices) << endl;
 }
