8a4166152e18ae96baa7b1149d439805291dcad2
bruce1408 <summer56567@163.com>
2020-03-25 21:36:13 +0800

    0325

--- Array/1128_Number_of_Equivalent_Domino_Pairs.cpp
+++ Array/1128_Number_of_Equivalent_Domino_Pairs.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <map>
 using namespace std;
 
 /**
- * 第一种方法超时了，效果不是很好
+ * 第一种方法超时了，效果不是很好，O(n^2)的时间复杂度
+ * 逻辑就是按照题目要求的那样去解决就好了，但是时间复杂度太高了
 */
 int numEquivDominoPairs(vector<vector<int>> &dominoes)
 {
@@ -15,6 +17,7 @@ int numEquivDominoPairs(vector<vector<int>> &dominoes)
     {
         for (after = cur + 1; after < count; after++)
         {
+            // 满足题目要去就继续加1
             if (((dominoes[cur][0] == dominoes[after][0]) &&
                  (dominoes[cur][1] == dominoes[after][1])) ||
                 ((dominoes[cur][0] == dominoes[after][1]) &&
@@ -27,23 +30,87 @@ int numEquivDominoPairs(vector<vector<int>> &dominoes)
 }
 
 /**
- * 方法 2 使用hash_map的思路来解决,不会超时
+ * 方法 2， 二维数组变成1位数组即可
+ * 使用unordered_map来使用，这里我们考虑，[1,2] = [2,1]的话那么有一种办法就是把二维数组变成一维数组，
+ * 让[1,2]=12;[2,1]=21,然后比较两个是否相同，如果相同的话，那么对应的哈希表的元素则加上1，
+ * 最后遍历这个哈希表，然后使用组合排列法则来计算，C N 2 = (N-1)*N/2;利用排列法则来计算这个结果即可
 */
+int reverseNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum * 10 + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
 int numEquivDominoPairs1(vector<vector<int>> &dominoes)
 {
-    int count = 1;
-    unordered_map<vector<int>, int> temp;
-    for (auto i : dominoes)
+    unordered_map<int, int> temp;
+    for (int i = 0; i < dominoes.size(); i++)
     {
-        vector<int> res = {i[1], i[0]};
-        unordered_map<vector<int>, int>::iterator it;
-        it = temp.find(i)
-        if (temp.find(i) == temp.end() && temp.find(res) == temp.end())
+        int sumElement = 0;
+        for (int j = 0; j < dominoes[i].size(); j++)
         {
-            count += 1;
+            sumElement = sumElement * 10 + dominoes[i][j];
         }
+        if ((temp.count(sumElement) > 0) || (temp.count(reverseNum(sumElement)) > 0))
+        {
+            if (temp.count(sumElement) > 0)
+                temp[sumElement] += 1;
+            else
+                temp[reverseNum(sumElement)] += 1;
+        }
+        else
+            temp[sumElement] = 1;
     }
-    return count;
+    int maxNum = 0;
+    for (unordered_map<int, int>::iterator iter = temp.begin(); iter != temp.end(); iter++)
+    {
+        maxNum += (iter->second - 1) * (iter->second) / 2;
+    }
+    return maxNum;
+}
+
+/**
+ * 方法2的改进版本：不再使用双层for循环了
+*/
+
+/**
+ * 方法3， 只用一层for循环即可，整体思路还是二维数组变成一维数组，但是有技巧，我们不再把每个数组的元素相加变成一个整数了
+ * 而是使用使用map来存储每个子数组，map<pair<int, int>, int>temp;遍历二维数组，如果每次遍历的时候，子数组的第一个数字大于第二个数字，那么
+ * 就交换即可，然后再添加到map的临时容器中，最后按照排列法则去排列即可
+*/
+int numEquivDominoPairs2(vector<vector<int>> &dominoes)
+{
+    int result = 0;
+    map<pair<int, int>, int> temp;
+    for (auto &i : dominoes)
+    {
+        if (i[0] > i[1]) swap(i[0], i[1]);
+        temp[make_pair(i[0], i[1])] += 1;
+    }
+    for (auto p = temp.begin(); p != temp.end(); p++)
+    {
+        result += p->second * (p->second - 1) / 2;
+    }
+    return result;
+}
+
+/**
+ * 方法4 不是很能理解下面的写法
+*/
+int numEquivDominoPairs5(vector<vector<int>> &dominoes)
+{
+    unordered_map<int, int> count;
+    int res = 0;
+    for (auto &d : dominoes)
+    {
+        res += count[min(d[0], d[1]) * 10 + max(d[0], d[1])]++;
+    }
+    return res;
 }
 
 int main()
@@ -53,5 +120,7 @@ int main()
         {2, 1},
         {3, 4},
         {5, 6}};
-    cout << numEquivDominoPairs1(res) << endl;
+    cout << numEquivDominoPairs5(res) << endl;
+    // cout << numEquivDominoPairs2(res) << endl;
+    // cout << reverseNum(12) << endl;
 }
--- exercise/11_1_map.cpp
+++ exercise/11_1_map.cpp
@@ -1,51 +1,68 @@
-/*主要介绍C++11 新特性： unordered_map 与 map 的对比*/
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <map>
+using namespace std;
+/**
+ * 主要介绍C++11 新特性： unordered_map 与 map 的对比
+ * 
+ *                   | map             | unordered_map
+ * ---------------------------------------------------------
+ * Ordering        | increasing  order   | no ordering
+ *                 | (by default)        |
+ * 
+ * Implementation  | Self balancing BST  | Hash Table
+ *                 | like Red-Black Tree |  
+ * 
+ * search time     | log(n)              | O(1) -> Average 
+ *                 |                     | O(n) -> Worst Case
+ * 
+ * Insertion time  | log(n) + Rebalance  | Same as search
+ *                       
+ * Deletion time   | log(n) + Rebalance  | Same as search
+ * 
+ * */
+struct person
+{
+    string name;
+    int age;
+    person(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+    bool operator<(const person &p) const
+    {
+        return this->age < p.age;
+    }
+};
 
-// #include<string>
-// #include<iostream>
-// #include<cstring>
-// #include<map>
-
-// using namespace std;
-
-// struct person
-// {
-// string name;
-// int age;
-
-// person(string name, int age)
-// {
-// this->name =  name;
-// this->age = age;
-// }
-
-// bool operator < (const person& p) const
-// {
-// return this->age < p.age;
-// }
-// };
-
-// /*map example*/
-// map<person,int> m;
-// int main()
-// {
-// person p1("Tom1",20);
-// person p2("Tom2",22);
-// person p3("Tom3",22);
-// person p4("Tom4",23);
-// person p5("Tom5",24);
-// m.insert(make_pair(p3, 100));
-// m.insert(make_pair(p4, 100));
-// m.insert(make_pair(p5, 100));
-// m.insert(make_pair(p1, 100));
-// m.insert(make_pair(p2, 100));
-
-// for(map<person, int>::iterator iter = m.begin(); iter != m.end(); iter++)
-// {
-// cout<<iter->first.name<<"\t"<<iter->first.age<<endl;
-// }
-
-// return 0;
-// }
+/*map example*/
+int main()
+{
+    map<person, int> m;
+    person p1("Tom1", 20);
+    person p2("Tom2", 22);
+    person p3("Tom3", 22);
+    person p4("Tom4", 23);
+    person p5("Tom5", 24);
+    m.insert(make_pair(p3, 100));
+    m.insert(make_pair(p4, 100));
+    m.insert(make_pair(p5, 100));
+    m.insert(make_pair(p1, 100));
+    m.insert(make_pair(p2, 100));
+    //迭代方法1 map
+    for (map<person, int>::iterator iter = m.begin(); iter != m.end(); iter++)
+    {
+        cout << iter->first.name << "\t" << iter->first.age << endl;
+    }
+    // 迭代方法2 map
+    for (auto i = m.begin(); i != m.end(); i++)
+    {
+        cout << i->first.age << " " << i->first.name << endl;
+    }
+    return 0;
+}
 
 // #include<string>
 // #include<iostream>
@@ -113,14 +130,14 @@
 //   return 0;
 // }
 
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// using namespace std;
 
-int main()
-{
-    vector<int> res = {9, 4};
-    unordered_map<vector<int>, int> temp;
-    temp.insert({1, 2}, 4);
-}
+// int main()
+// {
+//     vector<int> res = {9, 4};
+//     unordered_map<vector<int>, int> temp;
+//     temp.insert({1, 2}, 4);
+// }
