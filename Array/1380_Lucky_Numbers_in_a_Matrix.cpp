bdcc997a0ce4efc5b1a011bedad327580d78e22b
bruce1408 <summer56567@163.com>
2020-04-21 01:41:53 +0800

    1380

--- Array/1380_Lucky_Numbers_in_a_Matrix.cpp
+++ Array/1380_Lucky_Numbers_in_a_Matrix.cpp
@@ -18,25 +18,61 @@ using namespace std;
 */
 
 /**
- * 方法 1，
+ * 方法 1，使用逻辑算法，设置两个数组，一个用来存放行的最小值所在的列，还有一个是用来存放列的最大值所在的行
+ * 如果最后，行的最小值数组进行遍历，如果这个行的最小值所在数组遍历的结果是的值是列，它和列的最小值的数组的
+ * 行相同的话，接直接输出当前的这个数字。
 */
 vector<int> luckyNumbers(vector<vector<int>> &matrix)
 {
-    int maxRow = 0, rowIndex = 0, colIndex = 0;
-    for(int i=0;i<matrix.size();i++)
+
+    vector<int> minRow(matrix.size(), 0);
+    vector<int> maxCol(matrix[0].size(), 0);
+    vector<int> res;
+    // 行要找的是最小的值
+    for (int i = 0; i < matrix.size(); i++)
     {
-        
+        int col = 0, minNum = INT_MAX;
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            if (matrix[i][j] < minNum)
+            {
+                minNum = matrix[i][j];
+                col = j;
+            }
+        }
+        minRow[i] = col;
     }
-
-
+    // 列要找的是最大值
+    for (int j = 0; j < matrix[0].size(); j++)
+    {
+        int row = 0, maxNum = INT_MIN;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            if (matrix[i][j] > maxNum)
+            {
+                maxNum = matrix[i][j];
+                row = i;
+            }
+        }
+        maxCol[j] = row;
+    }
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        int num = minRow[i];
+        if (maxCol[num] == i)
+        {
+            res.push_back(matrix[i][num]);
+        }
+    }
+    return res;
 }
 
 int main()
-{  
-    vector<vector<int>>res = {
-        {3,7,8},
-        {9,11,13},
-        {15,16,17}
-    };
-    luckyNumbers(res);
+{
+    vector<vector<int>> res = {
+        {1, 10, 4, 2}, {9, 3, 8, 7}, {15, 16, 17, 12}};
+    for (auto i : luckyNumbers(res))
+    {
+        cout << i << " ";
+    }
 }
