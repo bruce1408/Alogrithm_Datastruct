2bc878befe8fe9a2f353c4cc5d5f7bdc59b6c667
bruce1408 <summer56567@163.com>
2020-04-23 14:26:13 +0800

    1346

---
+++ Array/1346_Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/**
+ * 给定一个数组，然后看数组里面是否有 数字*2=其他数字 存在
+ * 否则返回false
+*/
+bool checkIfExist(vector<int>& arr) 
+{
+    sort(arr.begin(), arr.end());
+    for(int i=0;i<arr.size();i++)
+    {
+        
+    }
+}
+
+int main()
+{
+
+}
