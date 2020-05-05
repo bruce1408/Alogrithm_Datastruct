01457063de37803e314659251f71c85d9a1308f5
bruce1408 <summer56567@163.com>
2020-05-05 17:52:30 +0800

    160

--- Linked_List/160_Intersection_ofTwoLinkedLists.cpp
+++ Linked_List/160_Intersection_ofTwoLinkedLists.cpp
@@ -39,6 +39,121 @@ ListNode *getIntersectionNode1(ListNode *headA, ListNode *headB)
 	return nullptr;
 }
 
+/**
+ * 方法 2，使用的是长度比较法，第一是比较两个链表的长度,然后是长度长的那个减去短的，长的链表从差值处开始和
+ * 短的链表进行循环。不过链表的交点不是数值相同
+ */
+int getLength(ListNode *head)
+{
+	int cnt = 0;
+	while (head)
+	{
+		head = head->next;
+		cnt += 1;
+	}
+	return cnt;
+}
+
+ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB)
+{
+	ListNode *a = headA, *b = headB;
+	int lena = getLength(a), lenb = getLength(b);
+	if (lena < lenb)
+	{
+		for (int i = 0; i < lenb - lena; i++)
+		{
+			b = b->next;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < lena - lenb; i++)
+		{
+			a = a->next;
+		}
+	}
+
+	while (a && b && a != b)
+	{
+		a = a->next;
+		b = b->next;
+	}
+	return (a && b) ? a : NULL;
+}
+
+/**
+ * 方法 3，利用环来做
+*/
+ListNode *getIntersectionNode2_1(ListNode *headA, ListNode *headB)
+{
+	ListNode *pA = headA, *pB = headB;
+	bool continueA = false, continueB = false;
+	if (pA == NULL || pB == NULL)
+		return NULL;
+	while ((pA != NULL) && (pB != NULL))
+	{
+		if (pA->val == pB->val)
+			break;
+		pA = pA->next;
+		pB = pB->next;
+
+		if (pA == NULL)
+		{
+			if (continueA)
+				break;
+			pA = headB;
+			continueA = true;
+		}
+		if (pB == NULL)
+		{
+			if (continueB)
+				break;
+			pB = headA;
+			continueB = true;
+		}
+	}
+	return pA;
+}
+
+/**
+ * 方法 3，是利用环的思想。
+ * 题目中强调了链表中不存在环，但是我们可以用环的思想来做，
+ * 我们让两条链表分别从各自的开头开始往后遍历，当其中一条遍历到末尾时，我们跳到另一个条链表的开头继续遍历。
+ * 两个指针最终会相等，而且只有两种情况，一种情况是在交点处相遇，另一种情况是在各自的末尾的空节点处相等。
+ * 为什么一定会相等呢，因为两个指针走过的路程相同，是两个链表的长度之和，所以一定会相等。
+ * 
+ * 比如是
+ * a = 4 1 8 4 5
+ * b = 5 0 1 8 4 5
+ * 如果a 和 b 都走完了，那么a 走 b的长度，b 走 a的长度
+ * 如下：
+ * a = 4 1 8 4 5 | 5 0 1 8 4 5
+ * b = 5 0 1 8 4 5 | 4 1 8 4 5
+ * 那么a b 走的路径长度之和一样。最后知道知道a b 链表相同的即可，记住不是val值相同。
+ */
+ListNode *getIntersectionNode3(ListNode *headA, ListNode *headB)
+{
+	if (!headA || !headB)
+		return NULL; // 边界条件给忘了？
+	ListNode *a = headA, *b = headB;
+	while (a != b)
+	{
+		a = a ? a->next : headB;
+		b = b ? b->next : headA;
+	}
+	return a;
+}
+
+void printf_list(ListNode *u) //输出链表的函数
+{
+	while (u)
+	{
+		cout << u->val << "->";
+		u = u->next;
+	}
+	cout << "end" << endl;
+}
+
 int main()
 {
 	ListNode *d1 = new ListNode(4);
@@ -63,143 +178,6 @@ int main()
 	//打印出两个链表的值
 	printf_list(p1);
 	printf_list(p2);
-	getIntersectionNode1(p1, p2);
+	cout << getIntersectionNode2_1(p1, p2)->val << endl;
 	return 0;
 }
-
-void printf_list(ListNode *u) //输出链表的函数
-{
-	while (u)
-	{
-		cout << u->val << "->";
-		u = u->next;
-	}
-	cout << "end" << endl;
-}
-
-/*找出两个链表的交集，(链表的值是数字，不是其他类型的)
-  这个前提是已经排好序了，然后利用下面的算法来找，
-  */
-// #include<iostream>
-// using namespace std;
-// struct linkList
-// {
-// 	string val;
-// 	linkList *next;
-// };
-
-// void printf_list(struct linkList *u);
-
-// int main()
-// {
-// 	//生成第一个链表
-// 	linkList e1 = {"c3",NULL};
-// 	linkList d1 = {"c2",&e1};
-// 	linkList c1 = {"c1",&d1};
-// 	linkList b1 = {"a2",&c1};
-// 	linkList a1 = {"a1",&b1};
-// 	linkList *head1 = &a1;
-// 	linkList*p1 = head1;
-
-// 	//生成第二个链表
-// 	linkList f2 = {"c3",NULL};
-// 	linkList e2 = {"c2",&f2};
-// 	linkList d2 = {"c1",&e2};
-// 	linkList c2 = {"b3",&d2};
-// 	linkList b2 = {"b2",&c2};
-// 	linkList a2 = {"b1",&b2};
-// 	linkList *head2 = &a2;
-// 	linkList*p2 = head2;
-
-// 	//打印出两个链表的值
-// 	cout<<"两个链表的如下："<<endl;
-// 	printf_list(p1);
-// 	printf_list(p2);
-
-// 	/*找出两组链表的交集：*/
-// 	while(p1&&p2)
-// 	{
-// 		if(p1->val==p2->val)
-// 		{
-// 			cout<<p1->val<<"->";
-// 			p1 = p1->next;
-// 			p2 = p2->next;
-// 		}
-// 		else if(p1->val>p2->val)
-// 		{
-// 			p2 = p2->next;
-// 		}
-// 		else
-// 		{p1 = p1->next;}
-
-// 	}
-
-// 	cout<<"end"<<endl;
-
-// 	return 0;
-
-// }
-
-// void printf_list(struct linkList *u)//输出链表的函数
-// {
-// 	while(u)
-// 	{
-// 		cout<<u->val<<"->";
-// 		u = u->next;
-// 	}
-// 	cout<<"end"<<endl;
-
-// }
-
-// //2019-3-5 第一是比较长度,然后是长度长的那个减去短的，差值进行循环。
-// ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-// {
-// 	ListNode *a = headA, *b = headB;
-// 	int lena = getLength(a), lenb = getLength(b);
-// 	if(lena < lenb)
-// 	{
-// 		for (int i=0;i<lenb-lena;i++)
-// 		{
-// 			b = b->next;
-// 		}
-// 	}
-// 	else
-// 	{
-// 		for (int i=0;i<lena-lenb;i++)
-// 		{
-// 			a = a ->next;
-// 		}
-// 	}
-
-// 	while(a && b && a!=b)
-// 	{
-// 		a = a->next;
-// 		b = b->next;
-// 	}
-// 	return (a && b)? a : NULL;
-// }
-
-// int getLength(ListNode *head)
-// {
-// 	int cnt=0;
-// 	while(head)
-// 	{
-// 		head = head->next;
-// 		cnt+=1;
-// 	}
-// 	return cnt;
-// }
-
-// // 方法二是利用环的思想。
-// ListNode *getIntersectionNode1(ListNode *headA, ListNode *headB)
-// {
-// 	if(!headA || !headB) return NULL;// 边界条件给忘了？
-// 	ListNode *a = headA, *b = headB;
-// 	while(a!=b)
-// 	{
-// 		a = a? a->next:headB;
-// 		b = b? b->next:headA;
-// 	}
-// 	return a;
-
-// }
