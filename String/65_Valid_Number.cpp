b1fb7cdb40d243a7113747aa1bfb964340301eca
bruce1408 <summer56567@163.com>
2020-07-06 16:08:08 +0800

    modify

--- String/65_Valid_Number.cpp
+++ String/65_Valid_Number.cpp
@@ -51,16 +51,58 @@ bool isNumber(string s){
  * 方法 2,使用条件语句,也是最一般的写法
  * 首先,去掉收尾的空格符号,然后开始讨论,
  * 分成dot小数点, 正负号+/-号,数字,还有科学计数符号e
+ * 首字符必须是数字或者是+/-符号,然后中间数字的话,e的前面的数字不能是+-符号,且之前没有出现e,
+ * 如果当前符号是正负号,那么他前面的数字只能是e符号,
+ * 如果当前的符号是逗号,那么他之前不能有逗号和e符号出现,否则返回false
+ * 
+ * 最后判断最后一位,最后是数字返回true,最后是小数点的话,那么前面不能有小数点,e符号,且前面数字存在
 */
 
 bool isNumber1(string s)
 {
-    
+    int len = s.size();
+    int left = 0, right = len - 1;
+    bool eExisted = false;
+    bool dotExisted = false;
+    bool digitExisited = false;
+    // Delete spaces in the front and end of string
+    while (s[left] == ' ') ++left;
+    while (s[right] == ' ') --right;
+    // If only have one char and not digit, return false
+    if (left >= right && (s[left] < '0' || s[left] > '9')) return false;
+    //Process the first char
+    if (s[left] == '.') dotExisted = true;
+    else if (s[left] >= '0' && s[left] <= '9') digitExisited = true;
+    else if (s[left] != '+' && s[left] != '-') return false;
+    // Process the middle chars
+    for (int i = left + 1; i <= right - 1; ++i)
+    {
+        if (s[i] >= '0' && s[i] <= '9') digitExisited = true;
+        else if (s[i] == 'e' || s[i] == 'E')
+        { // e/E cannot follow +/-, must follow a digit
+            if (!eExisted && s[i - 1] != '+' && s[i - 1] != '-' && digitExisited) eExisted = true;
+            else return false;
+        } 
+        else if (s[i] == '+' || s[i] == '-') 
+        { // +/- can only follow e/E
+            if (s[i - 1] != 'e' && s[i - 1] != 'E') return false;                
+        } 
+        else if (s[i] == '.') 
+        { // dot can only occur once and cannot occur after e/E
+            if (!dotExisted && !eExisted) dotExisted = true;
+            else return false;
+        } 
+        else return false;
+    }
+    // Process the last char, it can only be digit or dot, when is dot, there should be no dot and e/E before and must follow a digit
+    if (s[right] >= '0' && s[right] <= '9') return true;
+    else if (s[right] == '.' && !dotExisted && !eExisted && digitExisited) return true;
+    else return false;
 }
 
 
 int main()
 {
-    string s = "abc";
-    cout<<isNumber(s)<<endl;
+    string s = "-0.98";
+    cout<<isNumber1(s)<<endl;
 }
