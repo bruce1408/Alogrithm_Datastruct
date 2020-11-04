24bfce87c0837518921bcb92d88410213df90ce6
bruce1408 <summer56567@163.com>
2020-11-04 17:08:31 +0800

    modify in learn 5_longest

--- String/5_Longest_Palindromic_Substring.cpp
+++ String/5_Longest_Palindromic_Substring.cpp
@@ -4,22 +4,31 @@
 #include<string>
 using namespace std;
 
+/**
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+*/
 
+/**
+ * 方法 1,查找字符子串的最长回文串长度, 两层for循环来做.babad
+*/
 string longestPalindrome(string s) 
 {
-    int n = s.size();
-    for(int i=0;i<n/2;i++)
-    {
-        int j = i;
-        for(int k=0;k<j;k++)
-        {
-            if(s[k]!=s[j-k-1]) 
-        }
-    }
+    int len = s.size(), left = 0, right = len-1;
+    if(len==1) return s;
+    
+    
+
+    return s;
 }
 
 int main()
 {
     string s = "babad";
-    cout<<longestPalindrome(s);
+    cout<<longestPalindrome(s)<<endl;
 }
