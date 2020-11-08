797f74b08220c0efcc4b31c8e6bea6ad726b1a07
bruce1408 <summer56567@163.com>
2020-11-08 21:59:35 +0800

    acwing 763

---
+++ exercise/21_1_string5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s1 = "hello", s2 = "llo";
+    // for(int i=0;i<s1.size();i++)
+    // {
+    //     s1 = s1.substr(1) + s1[0];
+    //     cout<<s1<<endl;
+    // }
+    cout<<s1.find(s2)<<endl;
+
+}
