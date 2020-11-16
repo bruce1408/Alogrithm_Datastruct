34640c9ff3cda2f3ef838d7a272b31ab40e48852
bruce1408 <summer56567@163.com>
2020-11-16 22:27:05 +0800

    string

--- exercise/21_1_string5.cpp
+++ exercise/21_1_string5.cpp
@@ -1,15 +1,14 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    string s1 = "hello", s2 = "llo";
+    string s1 = "hellohello", s2 = "llo";
     // for(int i=0;i<s1.size();i++)
     // {
     //     s1 = s1.substr(1) + s1[0];
     //     cout<<s1<<endl;
     // }
-    cout<<s1.find(s2)<<endl;
-
+    cout << s1.find(s2) << endl;
 }
--- exercise/6_1_point.cpp
+++ exercise/6_1_point.cpp
@@ -1,22 +1,21 @@
-#iclude<iostream>
+#iclude < iostream>
 using namespace std;
-
+/**
+ * 指针保存的就是变量的地址
+*/
 int main()
 {
 	//单独给a，b指针，然后让a，b指向空NULL，然后依然不能赋值；但是new可以。
 	int *a = new int;
-	int *b = new int ;
+	int *b = new int;
 	*a = 10;
 	*b = 11;
-	cout<<*a<<endl;
-	cout<<*b<<endl;
+	cout << *a << endl;
+	cout << *b << endl;
 	delete a;
 	a = nullptr;
 	delete b;
 	b = nullptr;
 
-	
-	
 	return 0;
-		
 }
