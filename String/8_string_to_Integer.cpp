e9650c6480e0023c398331d1369dda3411805645
bruce1408 <summer56567@163.com>
2020-11-18 14:21:51 +0800

    string2int

--- String/8_string_to_Integer.cpp
+++ String/8_string_to_Integer.cpp
@@ -3,7 +3,9 @@
 #include<string>
 using namespace std;
 
-// 字符转数字 利用流方法简单
+/**
+ * 方法 1、字符转数字 利用流方法简单
+ * */ 
 int strTonum(string a)
 {
     int res;
@@ -13,7 +15,10 @@ int strTonum(string a)
     return res;
 }
 
-// 方法二；不是利用流，而是更加一般的方法；
+/**
+ * 方法 2、不是利用流，而是更加一般的方法； 按照几个条件去处理，空格->正负号->字符到数字->越界->输出
+
+ * */
 int strTonum2(string str)
 {
     if(str.empty()) return 0;
@@ -25,7 +30,6 @@ int strTonum2(string str)
     {
         sign = (str[i++]=='+') ? 1:-1;
     }
-    
     while(str[i]>='0'&&str[i]<='9'&&i<n)
     {
         // 考虑溢出问题。如果是这个数大于21xxxxxx467，那么就是取最大的那个越界边界。
@@ -37,9 +41,39 @@ int strTonum2(string str)
     }
     return res*sign;
 }
+
+/**
+ * 方法 三、使用常规方法来做，先考虑空格，然后考虑正负号；
+ * 按照几个条件去处理，空格->正负号->字符到数字->越界->输出
+ * 然后在考虑是否越界
+*/
+int strTonum3(string str) 
+{
+    int k = 0; 
+    //去空格
+    while(k < str.size() && str[k] == ' ') k++;
+    bool is_minus = false;
+    long long num = 0;
+    //判正负
+    if(str[k] == '+') k++;
+    else if(str[k] == '-') k++, is_minus = true;
+    //字符变数字
+    while(k < str.size() && str[k] >= '0' && str[k] <= '9')
+    {
+        num = num * 10 + str[k] - '0';
+        k++;
+    }
+    //处理特例
+    if(is_minus) num *= -1;
+    if(num > INT64_MAX) num = INT64_MAX;
+    if(num < INT64_MIN) num = INT64_MIN;
+    return (int) num;
+}
+
+
 int main()
 {
-    string a = "-9807";
-    cout<<strTonum2(a)<<endl;
+    string a = "     -9807";
+    cout<<strTonum(a)<<endl;
     return 0;
 }
