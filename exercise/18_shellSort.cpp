5e2530410ec13fa30cbac3c7ba4e8141473a84a0
bruce1408 <summer56567@163.com>
2020-11-20 21:14:36 +0800

    increament

--- exercise/18_shellSort.cpp
+++ exercise/18_shellSort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 #include"../utils/cout_vec.h"
 using namespace std;
 
@@ -14,6 +15,11 @@ using namespace std;
 /**
  * 方法 1生成一个增量的数组，这个增量数组中最后一位必须是数字1，
  * 然后遍历增量数组，每次调用插入排序的函数
+ * 原本数组是[7,6,4,3,4,1],然后通过增量计算，得到两个数一个是1，一个是4，把数组按照增量
+ * 进行预处理，得到的是[4,3,4,1,7,6] 大致有序的一个结果，然后再次遍历增量，得到
+ * 最后的完整排序结果[1,3,4,4,6,7]
+ * 所以增量的选择是很关键的，有可能增量的选择到最后是没有任何时间复杂度上的降低，所以这个通常采用
+ * 2^k-1 这种增量计算方式来计算即可；
  * 
 */
 void insertSort(vector<int>&res, int len, int g)
@@ -35,11 +41,18 @@ vector<int>shellSort(vector<int>&res)
 {
     int len = res.size();
     vector<int>incr;
+
+    // 两种计算增量的方式，第一个是按照 h*3+1 来计算，还有一个是按照 参考资料的 2^k-1 来计算增量的
     for(int h=1;;)// 生成一个增量数组
     {
         if(h > len) break;
         incr.push_back(h);
         h = h*3+1;
+    }
+    for(int i=1;i<len;i++)
+    {
+        i = pow(2, i)-1;
+        incr.push_back(i);
     }
     for(int i=incr.size()-1;i>=0;i--) // 调用插入排序，
     {
@@ -83,6 +96,7 @@ int main()
 {
     vector<int> res = {7,6,4,3,4,1};
     // shellSort1(res);
-    shellSort2(res);
+    // shellSort2(res);
+    shellSort(res);
     print(res);
 }
