a4e8eed0f464df23354c1a6eb6b47f5c4922cd67
bruce1408 <summer56567@163.com>
2020-11-21 15:47:46 +0800

    modify the code

--- exercise/18_heapSort.cpp
+++ exercise/18_heapSort.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<vector>
+#include"../utils/cout_vec.h"
 using namespace std;
 
 /**
+ * 堆排序的方法就是构建最大堆或者是最小堆，最大堆的根节点是最大值，最小堆根节点为最小值，然后即使去掉这个根节点，其余结点通过调整
+ * 也可以成为最大堆或者是最小堆，这样每次数组根节点的元素就是一个有序的数组；
+ * 
  * 方法 1，构建最大堆，利用数组来存放堆,
  * 堆的最主要的问题就是
  * --插入元素 --删除最大元素(记住是删除最大的元素，不是任意元素)
@@ -15,10 +19,7 @@ using namespace std;
  * 
  * 优先队列是基于二叉堆实现的，主要是插入和删除，插入是先插到最后，
  * 然后上浮到正确位置；删除是调换位置后再删除，然后下沉到正确位置
- */
-
-
-/**
+ * 
  * swim 上浮的方法，最大堆的构建方式；
  * 由下至上，对元素进行上浮，因为堆的某个元素比它的父节点还要大，
  * 我们需要交换它和它的父节点来修复堆，
@@ -35,7 +36,7 @@ void swim(vector<int>& res, int k)
 
 /**
  * sink 下沉的方法，最大堆的构建方式；
- * 由下至上构建这个二插堆，
+ * 由下至上构建这个二叉堆，
  * 因为某个节点比它的子节点还要小，通过和子节点中较大的那个来交换，然后恢复堆；
  * 这一步做完之后，我们还要继续比较子节点交换之后是不是不满足条件；
 */
@@ -78,7 +79,6 @@ void sinkMin(vector<int>&res, int k)
 
 void heapSort(vector<int>&res)
  {
-
     int length = res.size();
     for(int i=(length)>>1;i>=1;i--)
     {
@@ -87,7 +87,6 @@ void heapSort(vector<int>&res)
     while(length>1)
     {
         swap(res[1], res[length-1]);
-        // cout<<"=======the min num is=======:"<<res[length-1]<<endl;
         length--;
         sink(res, 1, length);
     }
@@ -97,9 +96,6 @@ int main()
 {
     vector<int>res = {0,50,10,90,30,70,40,80,60,20};
     heapSort(res);
-    for(auto i:res)
-    {
-        cout<<i<<" ";
-    }
+    print(res);
 }
 
