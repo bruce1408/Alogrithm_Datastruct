dc9c477c7a274497f80f9c45e881ee67d1d07956
bruce1408 <summer56567@163.com>
2020-11-27 22:47:32 +0800

    bigNumMulti

---
+++ exercise/algorithm_intro/793_bigNumMult.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> multi(vector<int>&res1, int res2)
+{
+
+}
+
+int main()
+{
+    string a;
+    cin>>a;
+    int b;
+    cin>>b;
+    vector<int>res1;
+    for(int i=a.size()-1; i>=0;i++)
+    {
+        res1.push_back(a[i]);
+    }
+}
