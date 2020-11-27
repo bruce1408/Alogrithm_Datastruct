f518185d3c7bdf26ba121e917a7a18858b9b4eb3
bruce1408 <summer56567@163.com>
2020-11-27 22:50:08 +0800

    bigNum

--- exercise/algorithm_intro/793_bigNumMult.cpp
+++ exercise/algorithm_intro/793_bigNumMult.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 vector<int> multi(vector<int>&res1, int res2)
 {
-
+    vector<int>res;
+    for(int i=0;i<res1.size();i++)
+    {
+        
+    }
 }
 
 int main()
@@ -17,5 +21,10 @@ int main()
     for(int i=a.size()-1; i>=0;i++)
     {
         res1.push_back(a[i]);
+    }
+    auto res = multi(res1, b);
+    for(int i=res.size()-1;i>=0;i--)
+    {
+        printf("%d", res[i]);
     }
 }
