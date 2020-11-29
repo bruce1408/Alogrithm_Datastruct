c1903ed78edd3578bf88b5311ea57f6aa9a74d57
bruce1408 <summer56567@163.com>
2020-11-29 17:20:28 +0800

    differenceSum

--- exercise/algorithm_intro/797_difference_matrix.cpp
+++ exercise/algorithm_intro/797_difference_matrix.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<vector>
+#include"../../utils/cout_vec.h"
+
 using namespace std;
+
+void insert(vector<int>&b, int l, int r, int c)
+{
+    b[l]+=c;
+    b[r+1]-=c;
+}
+
 
 int main()
 {
+    // int n, m;
+    // cin>>n>>m;
+    int n = 6, m = 3;
+    vector<int>a = {0, 1, 2, 2, 1, 2, 1};
+    vector<int>b(n+1);
+    // for(int i=1; i<=n; i++)
+    //     cin>>a[i]; 
+    for(int i=1;i<=n;i++) insert(b, i, i, a[i]);
+    print(b);
     
 }
