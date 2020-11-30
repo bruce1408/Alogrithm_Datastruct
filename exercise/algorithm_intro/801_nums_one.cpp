d1078b162b776883e1c14f5d1b1167c6839e16ef
bruce1408 <summer56567@163.com>
2020-12-01 00:40:13 +0800

    lowbit

--- exercise/algorithm_intro/801_nums_one.cpp
+++ exercise/algorithm_intro/801_nums_one.cpp
@@ -7,7 +7,7 @@ using namespace std;
  * 
  * 
 */
-void nums_one(vector<int> &a)
+void nums_one(vector<int> a)
 {
     int n = a.size();
     for (int i = 0; i < n; i++)
@@ -15,7 +15,8 @@ void nums_one(vector<int> &a)
         int count = 0, j = 31;
         while (j--)
         {
-            if(a[i] & 1) count++;
+            if (a[i] & 1)
+                count++;
             a[i] = a[i] >> 1;
         }
         cout << count << " ";
@@ -24,14 +25,13 @@ void nums_one(vector<int> &a)
 }
 
 /**
- * 方法 2, lowbit(x) = n & (-n)
+ * 方法 2, lowbit(x) = n & (-n)；
+ * lowbit表示每次减去最后一位1，然后，比如10的二进制是1010， lowbit操作之后返回10，只返回最后一位的1；
+ * 然后每次这个数减去lowbit(x)，统计减去的次数就是一个数字里面1的个数
 */
-int lowbit(vector<int>&res)
+int lowbit(int x)
 {
-    for(int i=0;i<res.size();i++)
-    {
-        while(res[i]) res[i] &(-res[i]);
-    }
+    return x & (-x);
 }
 
 int main()
@@ -41,5 +41,19 @@ int main()
     // vector<int>a(n);
     // for(int i=0;i<n;i++) cin>>a[i];
     vector<int> a = {1, 2, 3, 4, 5};
-    nums_one(a);
+    nums_one(a); // 方法 1
+
+    // 方法 2
+    int n = a.size(), i = 0;
+    while (i < n)
+    {
+        int count = 0;
+        while (a[i])
+        {
+            a[i] -= lowbit(a[i]);
+            count += 1;
+        }
+        i++;
+        cout << count << " ";
+    }
 }
