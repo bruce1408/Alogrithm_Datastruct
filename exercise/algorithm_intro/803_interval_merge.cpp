77e1e9a1edde5e46d72b8af5bb7f917321497b42
bruce1408 <summer56567@163.com>
2020-12-01 21:32:58 +0800

    intervel_merge

--- exercise/algorithm_intro/803_interval_merge.cpp
+++ exercise/algorithm_intro/803_interval_merge.cpp
@@ -17,15 +17,21 @@ bool cmp(cord a, cord b)
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n = 5;
+    // cin >> n;
     vector<cord> res(n);
-    for (int i = 0; i < n; i++)
-    {
-        int l, r;
-        cin >> l >> r;
-        res[i].l = l, res[i].r = r;
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int l, r;
+    //     cin >> l >> r;
+    //     res[i].l = l, res[i].r = r;
+    // }
+    res[0].l = 1, res[0].r = 2;
+    res[1].l = 2, res[1].r = 4;
+    res[2].l = 5, res[2].r = 6;
+    res[3].l = 7, res[3].r = 8;
+    res[4].l = 7, res[4].r = 9;
+
     sort(res.begin(), res.end(), cmp);
     for (int i = 0; i < n; i++)
     {
@@ -33,12 +39,17 @@ int main()
     }
 
     int count = 1, l = res[0].l, r = res[0].r;
-    for(int i=1; i<n; i++)
+    for (int i = 1; i < n; i++)
     {
-        if(l<= res[i].l <= r)
+        if (l <= res[i].l && res[i].l <= r)
         {
             r = max(res[i].r, r);
         }
-
+        else
+        {
+            l = res[i].l, r = res[i].r;
+            count += 1;
+        }
     }
+    cout << count << endl;
 }
