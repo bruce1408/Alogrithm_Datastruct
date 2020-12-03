1efbaddb1a0dd62aa7586a079ecb9f0e41832ff1
bruce1408 <summer56567@163.com>
2020-12-03 23:39:15 +0800

    trie

---
+++ exercise/algorithm_intro/154_slide_window.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+const int N = 1000010;
+/**
+ *  单调队列问题
+ * 最小值和最大值分开来做，两个for循环完全类似，都做以下四步：
+ * 
+ * 解决队首已经出窗口的问题;
+ * 解决队尾与当前元素a[i]不满足单调性的问题;
+ * 将当前元素下标加入队尾;
+ * 如果满足条件则输出结果;
+ * 
+ * 需要注意的细节：
+ * 上面四个步骤中一定要先3后4，因为有可能输出的正是新加入的那个元素;
+ * 队列中存的是原数组的下标，取值时要再套一层，a[q[]];
+ * 算最大值前注意将hh和tt重置;
+ * 此题用cout会超时，只能用printf;
+ * hh从0开始，数组下标也要从0开始。
+*/
+
+int q[8];
+int main()
+{
+    int n = 8, m = 3;
+    int head = -1, rear = -1;
+    int a[8] = {1, 3, -1, -3, 5, 3, 6, 7};
+    for (int i = 0; i < n; i++)
+    {
+        // 队列头部是否滑出了窗口，然后看起点大于的话，那么就head++
+        if (head != rear && i - m + 1 > q[head + 1])
+            head++;
+        新插入的数字如果小于队尾的话，那么就
+        while (head != rear && a[q[rear]] >= a[i])
+            rear--;
+        q[++rear] = i;
+        cout << "head: " << head << endl;
+        if (i >= m - 1) // 前m个数字就输出，不足m个数字就不用输出
+            printf("%d ", a[q[head + 1]]);
+    }
+    cout << endl;
+    head = -1, rear = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (head != rear && i - m + 1 > q[head + 1])
+            head++;
+        while (head != rear && a[q[rear]] <= a[i])
+            rear--;
+        q[++rear] = i;
+        if (i >= m - 1) // 前m个数字就输出，不足m个数字就不用输出
+            printf("%d ", a[q[head + 1]]);
+    }
+    cout << endl;
+}
---
+++ exercise/algorithm_intro/831_kmp.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/**
+ * kmp 算法
+ *  给定一个模式串S，以及一个模板串P，所有字符串中只包含大小写英文字母以及阿拉伯数字。
+ * 模板串P在模式串S中多次作为子串出现。
+ * 求出模板串P在模式串S中所有出现的位置的起始下标。
+*/
+
+/**
+ * 方法 1，使用双指针来做，可能会超时
+*/
+void kmpBF()
+{
+    int m = 3, n = 5;
+    string s = "ababa";
+    string p = "aba";
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = true;
+        for (int j = 0; j < m; j++)
+        {
+            if (s[i + j] != p[j])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+/**
+ * 方法 2，优化双指针算法，使用next数组，用kmp算法来做
+*/
+const int N = 1000010, M = 1000010;
+
+int n, m;
+int ne[N];
+char s[M], p[N];
+
+int main()
+{
+    cin >> n >> p + 1 >> m >> s + 1;
+    for (int i = 2, j = 0; i <= n; i++)
+    {
+        while (j && p[i] != p[j + 1])
+            j = ne[j];
+        if (p[i] == p[j + 1])
+            j++;
+        ne[i] = j;
+    }
+    for (int i = 1, j = 0; i <= m; i++)
+    {
+        while (j && s[i] != p[j + 1])
+            j = ne[j];
+        if (s[i] == p[j + 1])
+            j++;
+        if (j == n)
+        {
+            printf("%d ", i - n);
+            j = ne[j];
+        }
+    }
+    return 0;
+}
