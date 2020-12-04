bfc77364a6d1b26ea09fe6e4a47ef5851d66525a
bruce1408 <summer56567@163.com>
2020-12-04 22:15:55 +0800

    BILIstLink

--- exercise/algorithm_intro/827_biLinklist.cpp
+++ exercise/algorithm_intro/827_biLinklist.cpp
@@ -8,41 +8,31 @@ const int N = 100010;
  * 双向链表的操作指南,e表示值是多少，l表示左边的点，r表示右边的点
  * idx表示指向的当前工作的点
  * */
-int head, l[N], r[N], e[N], tail;
+int l[N], r[N], e[N], idx;
 
 void init()
 {
-    
     // 0表示左端点，1表示右端点,0的右边是1，1的左边是0，idx从2开始
     r[0] = 1;
     l[1] = 0;
     idx = 2;
-
-
 }
 
-// 链表插入，这里指针指向某个节点就是直接复制。
-void insert(int x)
+// 双向链在节点k的右边插入一个数
+void insert(int k, int x)
 {
     e[idx] = x;
-    ne[idx] = head; // 这里就相当于ne[idx] = head->next;
-    head = idx;     // 然后head指向idx
-    idx++;
+    r[idx] = r[k];
+    l[idx] = k;
+    r[l[k]] = idx;
+    r[k] = idx++;
 }
 
-// 在第k个点的后面插入
-void insert_k(int x, int k)
-{
-    e[idx] = x;
-    ne[idx] = ne[k];
-    ne[k] = idx;
-    idx++;
-}
-
-// 链表删除操作,下标是k的点后面的删掉
+// 链表删除操作, 删除第k个点
 void delete_k(int k)
 {
-    ne[k] = ne[ne[k]];
+    r[l[k]] = r[k];
+    l[r[k]] = l[k];
 }
 
 int main()
@@ -54,28 +44,35 @@ int main()
     while (m--)
     {
         int x, k;
-        char op;
+        string op;
         cin >> op;
-        if (op == 'H')
+        if (op == "L")
         {
             cin >> x;
-            insert(x);
+            insert(0, x);
         }
-        else if (op == 'D')
+        else if (op == "R")
+        {
+            cin >> x;
+            insert(l[1], x);
+        }
+        else if (op == "D")
         {
             cin >> k;
-            if (k == 0)
-                head = ne[head];
-            else
-                delete_k(k - 1);
+            delete_k(k + 1);
+        }
+        else if (op == "IL")
+        {
+            cin >> k >> x;
+            insert(l[k + 1], x);
         }
         else
         {
             cin >> k >> x;
-            insert_k(x, k - 1);
+            insert(k + 1, x);
         }
     }
-    for (int i = head; i != -1; i = ne[i])
+    for (int i = r[0]; i != 1; i = r[i])
         cout << e[i] << " ";
     cout << endl;
 }
