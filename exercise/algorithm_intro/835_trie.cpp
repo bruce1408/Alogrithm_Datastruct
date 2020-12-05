06674dc9dbfa6815b048484de5b2152e9358b7ec
bruce1408 <summer56567@163.com>
2020-12-05 20:44:13 +0800

    trie

--- exercise/algorithm_intro/835_trie.cpp
+++ exercise/algorithm_intro/835_trie.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 const int N  = 100010;
-// son表示N节点的所有子节点存的位置
+/**
+ * trie树用来高效存储和查找字符串集合的数据结构
+ * son表示N节点的所有子节点存的位置
+ * */ 
+
 int son[N][26], cnt[N], idx; // 下标是0，是根节点也是空节点
 int main()
 {
