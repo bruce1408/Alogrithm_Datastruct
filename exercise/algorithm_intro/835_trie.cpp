4603727649145addbba7f1678a71b7db731628c3
bruce1408 <summer56567@163.com>
2020-12-05 21:06:32 +0800

    kmp

--- exercise/algorithm_intro/835_trie.cpp
+++ exercise/algorithm_intro/835_trie.cpp
@@ -8,7 +8,42 @@ const int N  = 100010;
  * */ 
 
 int son[N][26], cnt[N], idx; // 下标是0，是根节点也是空节点
+char str[N];
+
+void insert(char *str)
+{
+    int p = 0;
+    for(int i=0; str[i]; i++ )
+    {
+        int u = str[i] - 'a';
+        if(!son[p][u]) son[p][u] = ++idx;
+        p = son[p][u];
+    }
+
+    cnt[p]++;
+}
+
+int query(char *str)
+{
+    int p = 0;
+    for(int i=0; str[i]; i++)
+    {
+        int u = str[i] - 'a';
+        if(!son[p][u]) return 0;
+        p = son[p][u];
+    }
+    return cnt[p];
+}
+
 int main()
 {
-
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        char op[2];
+        scanf("%s%s", op, str);
+        if(op[0]=='I') insert(str);
+        else printf("%d\n",query(str));
+    }
 }
