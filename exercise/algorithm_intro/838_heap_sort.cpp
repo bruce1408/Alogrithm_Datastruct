6361c6f0ce637a12ecb2c15809e424861d828838
bruce1408 <summer56567@163.com>
2020-12-06 22:01:30 +0800

    heap

--- exercise/algorithm_intro/838_heap_sort.cpp
+++ exercise/algorithm_intro/838_heap_sort.cpp
@@ -8,22 +8,33 @@ int h[N], se;
 /**
  * 堆排序，构建堆，使用down 或者是 up 操作来插入元素然后
  * 保证调整新插入之后的元素依然是大顶堆或者是小顶堆
+ * 本题使用的是最小堆，然后每个根节点的值小于等于左右孩子节点即可
 */
 void down(int index)
 {
+    // 临时变量t保存最开始的index
     int t = index;
-
+    // index 是否终于左孩子节点且左孩子节点值最小
     if (2 * index <= se && h[2 * index] < h[t])
-        t = 2 * index;
+        t = 2 * index; // 左孩子节点赋值给t
+    // index 节点是否有右孩子节点且右孩子节点值最小
     if (2 * index + 1 <= se && h[2 * index + 1] < h[t])
-        t = 2 * index + 1;
-    if (index != t)
+        t = 2 * index + 1; // 右孩子节点赋值给t
+    if (index != t)        // 如果index和当前的t不相等，说明需要交换
     {
         swap(h[t], h[index]);
-        down(t);
+        down(t); // 交换之后再从t继续判断；
     }
 }
 
+void up(int u)
+{
+    while(u/2 && h[u] < h[u/2])
+    {
+        swap(h[u], h[u/2]);
+        u/=2;
+    }
+}
 int main()
 {
     int n, m;
