f2f889135af673e40e5f7243aca92e95c43fa3cb
bruce1408 <summer56567@163.com>
2020-12-06 22:28:45 +0800

    heap

---
+++ exercise/algorithm_intro/839_heap_construct.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+const int N = 100010;
+int h[N], cnt, ph[N], hp[N];
+
+void down(int index)
+{
+    int t = index;
+    
+    if(2 * index <= se && h[2*index] < h[t]) t = 2 * index;
+    if(2 * index + 1 <= se && h[2*index+1] < h[t]) t = 2 * index+1;
+    if(index != t) 
+    {
+        swap(h[t], h[index]);
+        down(t);
+    }
+    
+}
+
+int main()
+{
+    int n, m = 0;
+    cin>>n>>m;
+
+    while(n--)
+    {
+        string s;
+        cin>>s;
+        int k, x;
+        if(s=="I")
+        {
+            cnt++;
+            m++;
+
+            h[cnt] = x;
+            up(cnt);
+        }
+        
+    }
+}
