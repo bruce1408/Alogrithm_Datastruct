4124237c4d745ee382ff29c29c703c0a9231a743
bruce1408 <summer56567@163.com>
2020-12-07 17:47:49 +0800

    hash

--- exercise/algorithm_intro/840_hash.cpp
+++ exercise/algorithm_intro/840_hash.cpp
@@ -2,8 +2,44 @@
 #include <vector>
 using namespace std;
 
-const int N = 100003;
-int idx;
+const int N = 200003, null = 0x3f3f3f3f;
+
+int h[N];
+
+int find(int x)
+{
+    int t = (x % N + N) % N;
+    while (h[t] != null && h[t] != x)
+    {
+        t ++ ;
+        if (t == N) t = 0;
+    }
+    return t;
+}
+
+int main()
+{
+    memset(h, 0x3f, sizeof h);
+
+    int n;
+    scanf("%d", &n);
+
+    while (n -- )
+    {
+        char op[2];
+        int x;
+        scanf("%s%d", op, &x);
+        if (*op == 'I') h[find(x)] = x;
+        else
+        {
+            if (h[find(x)] == null) puts("No");
+            else puts("Yes");
+        }
+    }
+
+    return 0;
+}
+
 /**
  * 模拟散列表，这里使用拉链法来模拟哈希两种操作
  * 第一就是存储
@@ -12,9 +48,12 @@ int idx;
  * 拉链法来解决冲突；拉链法使用链表来模拟，
  * 如果这个下标冲突了，那么就在这个下标下继续构建一个单链表存储即可；
 */
+const int N = 100003;
+int idx;
+
 void insert(int x, vector<int> &s, vector<int> &ne, vector<int> &h)
 {
-    int u = (x % N + N) % N;
+    int u = (x % N + N) % N; // 这里注意负数的话余数处理
     s[idx] = x;
     ne[idx] = h[u];
     h[u] = idx;
