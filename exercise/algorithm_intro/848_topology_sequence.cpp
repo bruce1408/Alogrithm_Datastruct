dd6a216e46ce1a63e1899db69b8ab958b4129cde
bruce1408 <summer56567@163.com>
2020-12-09 02:16:11 +0800

    topology

--- exercise/algorithm_intro/848_topology_sequence.cpp
+++ exercise/algorithm_intro/848_topology_sequence.cpp
@@ -1,3 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+
 /**
  * 拓扑序列：
  * 有向图，所有的边都是从前指向后的，
@@ -11,3 +17,64 @@
  * 入队
  * 然后接着进行宽搜的类型； 
 */
+
+const int N  = 100010;
+int n, m;
+int h[N], e[N], ne[N];
+int q[N], d[N], idx;
+
+void add(int a, int b)
+{
+    e[idx] = b;
+    ne[idx] = h[a];
+    h[a] = idx; 
+    idx++;
+}
+
+bool topsort()
+{
+    int head = 0, rear = -1;
+    for(int i=1;i<=n;i++)
+    {
+        if(!d[i])
+        {
+            q[++rear] = i;
+        }
+    }
+    while(head<=rear)
+    {
+        int t = q[head++];
+        for(int i= h[t];i!=-1;i=ne[i])
+        {
+            int j = e[i];
+            d[j]--;
+            if(d[j]==0)
+            {
+                q[++rear] = j;
+            }
+        }
+    }
+    return rear == n-1;
+}
+
+int main()
+{
+    cin>>n>>m;
+    memset(h, -1, sizeof h);
+    for(int i=0;i<m;i++)
+    {
+        int a, b;
+        cin>>a>>b;
+        add(a, b);
+        d[b]++; // 入度+1
+    }
+    if(topsort())
+    {
+        for(int i=0; i<n; i++)
+        {
+            cout<<q[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else cout<<-1<<endl;
+}
