157bc2ccd3c750bfa0a05d701bad9488f8090ba4
bruce1408 <summer56567@163.com>
2020-12-09 01:54:01 +0800

    direction_graph

--- exercise/algorithm_intro/847_bfs_graph.cpp
+++ exercise/algorithm_intro/847_bfs_graph.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
 /**
@@ -8,10 +10,55 @@ using namespace std;
  * 这里宽度优先搜索的办法就是利用一个队列；
  * 然后存放队头元素，
  * 然后判断队列不是空，然后取出队头元素出来，
- * 扩展队头元素的所有邻接点，然后判断这个点有没有遍历过，如果没有，继续
+ * 扩展队头元素的所有邻接点，然后判断这个点有没有遍历过，如果没有，
+ * 这个点入队，同时更新x的距离即可
  * 进行即可；
 */
+
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+int d[N], q[N], n, m;
+
+void add(int a, int b)
+{
+    e[idx] = b;
+    ne[idx] = h[a];
+    h[a] = idx;
+    idx++;
+}
+
+int bfs()
+{
+    int head = 0, rear = 0;
+    q[0] = 1;
+    memset(d, -1, sizeof d);
+    d[1] = 0;
+    while (head <= rear)
+    {
+        int t = q[head++];
+        for (int i = h[t]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if (d[j] == -1)
+            {
+                d[j] = d[t] + 1;
+                q[++rear] = j;
+            }
+        }
+    }
+    return d[n];
+}
+
 int main()
 {
-    
+    cin >> n >> m;
+    memset(h, -1, sizeof h);
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+    }
+    cout << bfs() << endl;
 }
