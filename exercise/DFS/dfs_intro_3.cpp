a15ff77eb62639085c42e70e4091ad82c8ca0183
bruce1408 <summer56567@163.com>
2020-12-09 20:43:29 +0800

    dfs

--- exercise/DFS/dfs_intro_3.cpp
+++ exercise/DFS/dfs_intro_3.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 #include<vector>
-int n;
+int n, maxValue;
 const int N = 100010;
-int nums[N];
+int nums[N], visited[N];
+/**
+ * 给定N个整数，从中选择k个数使得这k个数字的和为恰好为给定的整数x，
+ * 这k个数的平方和最大，假定方案是唯一的
+ * 例如{2, 3, 3, 4}, k = 2, x = 6;
+ * 满足的有{2, 4} 、{3, 3}，那么{2, 4}平方和最大输出即可
+*/
 
 void dfs()
 {
@@ -14,5 +20,5 @@ int main()
     cin>>n;
     for(int i=0;i<n;i++)
         cin>>nums[i];
-    dfs()
+    dfs(0, );
 }
