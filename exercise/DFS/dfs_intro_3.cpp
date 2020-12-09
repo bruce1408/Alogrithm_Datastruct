86d5f24a5271f50c4df78b266c4bbc52819ac97e
bruce1408 <summer56567@163.com>
2020-12-10 01:04:55 +0800

    dfs

--- exercise/DFS/dfs_intro_3.cpp
+++ exercise/DFS/dfs_intro_3.cpp
@@ -6,7 +6,7 @@ int n, maxValue, x, k;
 const int N = 100010;
 int visited[N];
 int nums[] = {2, 3, 3, 4};
-vector<int>temp, ans;
+vector<int> temp, ans;
 /**
  * 给定N个整数，从中选择k个数使得这k个数字的和为恰好为给定的整数x，
  * 这k个数的平方和最大，假定方案是唯一的
@@ -16,34 +16,27 @@ vector<int>temp, ans;
 
 void dfs(int index, int newK, int sum, int sum2)
 {
+
+    // 如果是超过了k个数，或者处理完了n个数，或者是和超过了x，返回
+    if (index == n || newK > k || sum > x)
+        return;
+
+    // 如果满足条件
     if (newK == k && sum == x)
     {
-        if(sum2 > maxValue)
+        if (sum2 > maxValue)
         {
             maxValue = sum2;
             ans = temp;
         }
-        return ;
+        return;
     }
-    // 如果是超过了k个数，或者处理完了n个数，或者是和超过了x，返回
-    if(index == n || nowK > k || sum>x) return ;
+    // 选择index号数字
+    temp.push_back(nums[index]);
+    dfs(index + 1, newK + 1, sum + nums[index], sum2 + nums[index] * nums[index]);
+    temp.pop_back();
+    // 不选index号数字
     dfs(index + 1, newK, sum, sum2);
-    if (sum < x)
-    {
-        for (int i = index; i < n - 1; i++)
-        {
-            if (sum + nums[i] <= x)
-            {
-                ans = max(ans, sum2 + nums[i] * nums[i]);
-                return;
-                // dfs(i, newK + 1, sum + nums[i], sum2 + nums[i] * nums[i]);
-            }
-            else
-            {
-                dfs(i + 1, newK + 1, sum + nums[i], sum2 + nums[i] * nums[i]);
-            }
-        }
-    }
 }
 
 int main()
@@ -53,5 +46,5 @@ int main()
     //     cin>>nums[i];
     n = 4;
     dfs(0, 0, 0, 0);
-    cout << ans << endl;
+    cout << maxValue << endl;
 }
