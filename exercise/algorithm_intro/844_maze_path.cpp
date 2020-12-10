7b5c9cd1c88356593f7e37b2cdd1295a3f209792
bruce1408 <summer56567@163.com>
2020-12-10 20:44:44 +0800

    bfs

--- exercise/algorithm_intro/844_maze_path.cpp
+++ exercise/algorithm_intro/844_maze_path.cpp
@@ -15,9 +15,9 @@ int n, m;
 int path[N][N]; // 保存的是整个地图
 int d[N][N];    // 保存的是距离长度
 PII q[N * N];
-PII rute[N][N]; // 路径保存
+PII rute[N][N];
 
-int bfs(vector<vector<int>> &d)
+int bfs1(vector<vector<int>> &d, vector<vector<int>> &rute1)
 {
     // 上右下左四个点的坐标
     vector<int> dx = {-1, 0, 1, 0};
@@ -36,11 +36,11 @@ int bfs(vector<vector<int>> &d)
                 // cout<<x<<" "<<y<<endl;
                 d[x][y] = d[t.first][t.second] + 1;
                 q[++rear] = {x, y}; // 入队
+                rute1.push_back({t.first, t.second});
                 rute[x][y] = t;
             }
         }
     }
-    // 打印最短路径部分
     int x = n - 1, y = m - 1;
     while (x || y)
     {
@@ -51,6 +51,41 @@ int bfs(vector<vector<int>> &d)
 
     return d[n - 1][m - 1];
 }
+
+/**
+ * 自己手写第二遍
+*/
+int n, m;
+const int N = 110;
+int path[N][N], visited[N][N], dis[N];
+pair<int, int> q[N * N];
+
+int bfs2(vector<vector<int>> &visited)
+{
+    // 上右下左
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, 1, 0, -1};
+
+    int head = 0, rear = 0;
+    q[0] = {0, 0};
+    visited[0][0] = 0;
+
+    while (head <= rear)
+    {
+        auto t = q[head++];
+        for (int i = 0; i < 4; i++)
+        {
+            int x = t.first + dx[i], y = t.second + dy[i];
+            if (x < n && x >= 0 && y >= 0 && y < m && path[x][y] != 1 && visited[x][y] == -1)
+            {
+                q[++rear] = {x, y};
+                visited[x][y] = visited[t.first][t.second] + 1;
+            }
+        }
+    }
+    return visited[n - 1][m - 1];
+}
+
 int main()
 {
     cin >> n >> m;
@@ -65,6 +100,6 @@ int main()
      * 用来判断这个点是否已经走过了，初始化用-1表示没有走过,
      * 如果走过了，就这个点的坐标+1，最后输出的是最后右下角点的值，即路径长度
      * */
-    vector<vector<int>> dis(N, vector<int>(N, -1));
-    cout << bfs(dis) << endl;
+    vector<vector<int>> visited(N, vector<int>(N, -1));
+    cout << bfs2(visited) << endl;
 }
