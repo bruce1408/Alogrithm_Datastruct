a553dfd4e7845a6d2f4b737f381f16c640b9a112
bruce1408 <summer56567@163.com>
2020-12-11 17:21:17 +0800

    tree

--- Tree/94_Binary_Tree_Inorder_Traversal.cpp
+++ Tree/94_Binary_Tree_Inorder_Traversal.cpp
@@ -1,15 +1,14 @@
-/*94. Binary Tree Inorder Traversal
-二叉树的中序遍历
-
-
+/**
+ * 94. Binary Tree Inorder Traversal
+ * 二叉树的中序遍历
 */
 
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
-struct TreeNode 
+struct TreeNode
 {
 	int val;
 	TreeNode *left;
@@ -17,109 +16,116 @@ struct TreeNode
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// 方法1：使用递归的方式，但是res每次递归的时候要重建，所以索性把
-// res变成静态变量。或者是其他方法。比如方法2
-// class Solution
-// {
-	// public:
-	// vector<int> inorderTraversal(TreeNode* root)
-	// {
-		
-		// if(!root)
-			// return {};
-		// else
-		// {
-			// inorderTraversal(root->left);
-			// res.push_back(root->val);
-			// inorderTraversal(root->right);
-		// }
-		// return res;
-	// }
-	
-	// private: //加了静态变量，res每次都在
-	// vector<int>res;
-// };
-
-//方法2：不用静态变量
-
-class Solution
+/**
+ * 方法1：使用递归的方式，但是res每次递归的时候要重建，所以索性把
+ * res变成静态变量。或者是其他方法。比如方法2
+ * */
+class Solution1
 {
-	public:
-	void midTraversal(TreeNode *root ,vector<int> &res) //res必须加上引用
+public:
+	vector<int> inorderTraversal(TreeNode *root)
 	{
-		if(!root) return;
+		if (!root)
+			return {};
 		else
 		{
-			midTraversal(root->left,res);
+			inorderTraversal(root->left);
 			res.push_back(root->val);
-			midTraversal(root->right,res);
-		}	
+			inorderTraversal(root->right);
+		}
+		return res;
 	}
-	
+
+private: //加了静态变量，res每次都在
+	vector<int> res;
+};
+
+/**
+ * 方法 2，不用静态变量
+ * */
+class Solution2
+{
+public:
+	void midTraversal(TreeNode *root, vector<int> &res) //res必须加上引用
+	{
+		if (!root)
+			return;
+		else
+		{
+			midTraversal(root->left, res);
+			res.push_back(root->val);
+			midTraversal(root->right, res);
+		}
+	}
+
 	vector<int> inorderTraversal(TreeNode *root)
 	{
 		vector<int> res; //这个作用是传res
-		midTraversal(root,res);	
+		midTraversal(root, res);
 		return res;
 	}
 };
 
-//方法3.1：利用迭代的方法，也用了栈的思想，但是没有具体用stack的标准库
-// vector<int> inorderTraversal(TreeNode *root)
-// {
-	// vector<int> res;
-	// TreeNode* p;
-	// vector<TreeNode*> Q(20,0);
-	// int top = -1;
-	// if(root!=NULL)
-	// {
-		// p = root;
-		// while(top>-1 || p!=NULL)
-		// {
-			// while(p!=NULL)
-			// {
-				// Q[++top]=p;
-				// p=p->left;
-			// }
-			// if(top>-1)
-			// {
-				// p = Q[top--];
-				// res.push_back(p->val);
-				// p=p->right;
-			// }
-		// }
-	// }
-	// return res;
-// }
-
-//方法3.2：用栈的方法stack,思路和上面的一摸一样
-vector<int> inorderTraversal(TreeNode *root)
+/**
+ * 方法 3.1, 中序遍历二叉树，利用迭代的方法,而没有使用递归来遍历二叉树
+ * */
+vector<int> inorderTraversal3_1(TreeNode *root)
 {
 	vector<int> res;
-	stack<TreeNode*>Q;
-	TreeNode* p; 
-	if(!root)
-		return {};
-	p = root;
-	while(!Q.empty() || p)
+	TreeNode *p;
+	vector<TreeNode *> Q(20, 0);
+	int top = -1;
+	if (root != NULL)
 	{
-		while(p)
+		p = root;
+		while (top > -1 || p != NULL)
 		{
-			Q.push(p);
-			p = p->left;
-		}
-		if(!Q.empty())
-		{
-			p = Q.top();
-			Q.pop();
-			res.push_back(p->val);
-			p = p->right;	
+			while (p != NULL)
+			{
+				cout << "p value: " << p->val << endl;
+				Q[++top] = p;
+				cout << "top: " << top << endl;
+				p = p->left;
+			}
+			cout << top << endl;
+			if (top > -1)
+			{
+				p = Q[top--];
+				res.push_back(p->val);
+				p = p->right;
+			}
 		}
 	}
 	return res;
 }
 
-
+/**
+ * 方法 3.2, 用栈的方法stack,思路和上面的一摸一样
+ * */
+vector<int> inorderTraversal3_2(TreeNode *root)
+{
+	vector<int> res;
+	stack<TreeNode *> Q;
+	TreeNode *p;
+	if (!root) return {};
+	p = root;
+	while (!Q.empty() || p!=NULL)
+	{
+		while (p)
+		{
+			Q.push(p);
+			p = p->left;
+		}
+		if (!Q.empty())
+		{
+			p = Q.top();
+			Q.pop();
+			res.push_back(p->val);
+			p = p->right;
+		}
+	}
+	return res;
+}
 
 int main()
 {
@@ -133,10 +139,8 @@ int main()
 	h2->left = h3;
 	h2->right = h4;
 	vector<int> rest;
-	rest = inorderTraversal(head);
-	for(auto i:rest)
-		cout<<i<<" ";
-	
-	
+	rest = inorderTraversal3_2(head);
+	for (auto i : rest)
+		cout << i << " ";
 	return 0;
 }
