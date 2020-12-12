adba42f79f70cb5c1bfa07bd489851af6cddff30
bruce1408 <summer56567@163.com>
2020-12-12 20:32:03 +0800

    avl

--- exercise/algorithm_intro/3_chapter_AVL_tree.cpp
+++ exercise/algorithm_intro/3_chapter_AVL_tree.cpp
@@ -3,49 +3,106 @@
 using namespace std;
 
 /**
- * 
+ * 二叉查找树如果使用有序序列
+ * 1，2，3，4，5 进行构建二叉查找树，那么可能出现的树是一颗链式的长度。所以为了防止
+ * 1
+ *  \ 
+ *   2 
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ * 这种情况，使用平衡二叉树来构建
 */
+// struct node
+// {
+//     int data;
+//     node *left;
+//     node *right;
+//     node(int x) : data(x), left(NULL), right(NULL) {}
+// };
+
+// void preorder(node *root)
+// {
+//     if (root == nullptr)
+//         return;
+//     preorder(root->left);
+//     cout << root->data << " ";
+//     preorder(root->right);
+// }
+
+// void insert(node *&root, int x)
+// {
+//     if (root == nullptr)
+//     {
+//         root = new node(x);
+//         return;
+//     }
+//     if (root->data > x)
+//     {
+//         insert(root->left, x);
+//     }
+//     else
+//     {
+//         insert(root->right, x); // 这里其实包含了等于的情况，如果等于的话就在右子树上继续构建
+//     }
+// }
+
+// int main()
+// {
+//     node *root = NULL; // 定义头结点
+//     vector<int> res = {1, 2, 3, 4, 5};
+//     for (int i = 0; i < res.size(); i++)
+//     {
+//         insert(root, res[i]);
+//     }
+//     preorder(root);
+// }
+
 struct node
 {
-    int data;
+    int val, height;
     node *left;
     node *right;
-    node(int x) : data(x), left(NULL), right(NULL) {}
+    node(int x) : val(x), height(0), left(NULL), right(NULL) {}
 };
 
-void preorder(node *root)
+/**
+ * 生成一个新节点，v是节点的权值
+ * height是高度，起始高度设置为1
+*/
+node *newNode(int v)
 {
-    if (root == nullptr)
-        return;
-    preorder(root->left);
-    cout << root->data << " ";
-    preorder(root->right);
+    node *root = new node(x);
+    root->height = 1;
+    root->left = root->right = nullptr;
+    return root;
 }
 
-void insert(node *&root, int x)
+/**
+ * 得到节点的height高度
+ * */
+int getHeight(node *root)
 {
     if (root == nullptr)
-    {
-        root = new node(x);
-        return;
-    }
-    if (root->data > x)
-    {
-        insert(root->left, x);
-    }
-    else
-    {
-        insert(root->right, x); // 这里其实包含了等于的情况，如果等于的话就在右子树上继续构建
-    }
+        return 0;
+    return root->height;
 }
 
-int main()
+/**
+ * 计算root节点的平衡因子，
+ * 左子树的高度减去右子树的高度
+*/
+int getBalance(node *root)
 {
-    node *root = NULL; // 定义头结点
-    vector<int> res = {1, 2, 3, 4, 5};
-    for (int i = 0; i < res.size(); i++)
-    {
-        insert(root, res[i]);
-    }
-    preorder(root);
+    return getHeight(root->left) - getHeight(root->right); // 返回左子树高度减去右子树高度
 }
+
+/**
+ * 更新root节点的高度height
+*/
+
+
+
