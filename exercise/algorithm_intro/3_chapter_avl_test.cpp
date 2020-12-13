40e8fde654bf71473d749a0111505a5be9827676
bruce1408 <summer56567@163.com>
2020-12-13 16:04:42 +0800

    avl

---
+++ exercise/algorithm_intro/3_chapter_avl_test.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct node
+{
+    int val, height;
+    node *left, *right;
+    node(int x):val(x), height(0),left(NULL), right(NULL) {}
+};
+
+
+node *newNode(int x)
+{
+    
+    node *root = new node(x);
+    root->height = 1;
+    root->left = root->right = nullptr;
+    return node;
+}
+
+node *insert(node *&root, int x)
+{
+    if(root==nullptr)
+    {
+        node * root = newNode(x);
+    }
+    if(root->val > x)
+    {
+        
+    }
+}
+
+int main()
+{
+
+}
