0cce5967e8d0664bcb1e7022a19a966501656be9
bruce1408 <summer56567@163.com>
2020-12-14 21:17:51 +0800

    dijkstra

--- exercise/algorithm_intro/4_chapter_shortest_path.cpp
+++ exercise/algorithm_intro/4_chapter_shortest_path.cpp
@@ -18,11 +18,12 @@ bool visited[N];
 int dijkstra()
 {
     fill(dis, dis + N, inf);    // 使用fill函数把数组dis整个值赋值为最大值inf
-    dis[1] = 0;                 // 起点s到达自身的距离为0
+    dis[1] = 0;                 // 节点1到达自身的距离为0
     for (int i = 0; i < n; i++) // 循环n次，n个节点
     {
         int u = -1, MIN = inf; // u使得d[u] 最小，MIN存放该最小的d[u];
-        for (int j = 1; j <= n; j++) // 从1开始是因为节点范围是1-n, 这层循环是使u=dis[u]最小的还未被访问的顶点的标号
+        // 从1开始是因为节点范围是1-n, 这层循环是使u=dis[u]最小的还未被访问的顶点的标号，找到未访问的最小d[u]对应的节点
+        for (int j = 1; j <= n; j++)
         {
             if (visited[j] == false && dis[j] < MIN)
             {
@@ -30,14 +31,15 @@ int dijkstra()
                 u = j;
             }
         }
-        if (u == -1)
+        if (u == -1) // 找不到小于inf距离的节点u，那么说明剩下的顶点和起点不连通
             return -1;
-        visited[u] = true;
+        visited[u] = true; // 节点u已经被访问
         for (int v = 1; v <= n; v++)
         {
+            // 如果v未被访问&&u能到达v&&u到达v的距离+起点到u的距离d[u]<起点到v的距离d[v] 那么就更新dis[v]
             if (visited[v] == false && g[u][v] != inf && dis[u] + g[u][v] < dis[v])
             {
-                dis[v] = g[u][v] + dis[u];
+                dis[v] = g[u][v] + dis[u]; // 更新优化dis[v]
             }
         }
     }
