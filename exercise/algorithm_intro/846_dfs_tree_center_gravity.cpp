c88458f12038e719bc4afd0836fbc61034630706
bruce1408 <summer56567@163.com>
2020-12-14 16:43:32 +0800

    dfs_graph

--- exercise/algorithm_intro/846_dfs_tree_center_gravity.cpp
+++ exercise/algorithm_intro/846_dfs_tree_center_gravity.cpp
@@ -1,57 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
 #include <algorithm>
-using namespace std;
-/**
- * 图的遍历
- * 使用深度优先遍历 和 宽度优先遍历
-*/
-int n;
-const int N = 100010, M = N * 2;
-int h[N], value[M], ne[M], idx;
-bool st[N]; // 是否已经遍历过了
-int ans = N;
+#include <cstring>
 
-// 在a的结点插入一条新的边b
-void add(int a, int b)
-{
-    value[idx] = b;
-    ne[idx] = h[a];
-    h[a] = idx;
-    idx++;
+using namespace std;
+
+const int N = 1e5 + 10; //数据范围是10的5次方
+const int M = 2 * N; //以有向图的格式存储无向图，所以每个节点至多对应2n-2条边
+
+int h[N]; //邻接表存储树，有n个节点，所以需要n个队列头节点
+int e[M]; //存储元素
+int ne[M]; //存储列表的next值
+int idx; //单链表指针
+int n; //题目所给的输入，n个节点
+int ans = N; //表示重心的所有的子树中，最大的子树的结点数目
+
+bool st[N]; //记录节点是否被访问过，访问过则标记为true
+
+//a所对应的单链表中插入b  a作为根 
+void add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
 }
 
-// 返回以u为根节点的子树的大小
-int dfs(int u)
-{
-    st[u] = true;
-    int sum = 1, res = 0; // sum表示当前节点个数， res 表示连通块的大小的最大值
-    for (int i = h[u]; i != -1; i = ne[i])
-    {
-        int j = value[i];
-        if (!st[j])
-        {
-            int s = dfs(j); // 如果这个点之前没有遇到，那么继续遍历这个点
-            res = max(res, s);
-            sum += s;
+// dfs 框架
+/*
+void dfs(int u){
+    st[u]=true; // 标记一下，记录为已经被搜索过了，下面进行搜索过程
+    for(int i=h[u];i!=-1;i=ne[i]){
+        int j=e[i];
+        if(!st[j]) {
+            dfs(j);
         }
     }
-    res = max(res, n - sum);
-    ans = min(res, ans);
+}
+*/
+
+//返回以u为根的子树中节点的个数，包括u节点
+int dfs(int u) 
+{
+    int res = 0; //存储 删掉某个节点之后，最大的连通子图节点数
+    st[u] = true; //标记访问过u节点
+    int sum = 1; //存储 以u为根的树 的节点数, 包括u，如图中的4号节点
+
+    //访问u的每个子节点
+    for (int i = h[u]; i != -1; i = ne[i]) 
+    {
+        int j = e[i];
+        //因为每个节点的编号都是不一样的，所以用编号为下标来标记是否被访问过
+        if (!st[j]) {
+            int s = dfs(j);  // u节点的单棵子树节点数 如图中的size值
+            res = max(res, s); // 记录最大联通子图的节点数
+            sum += s; //以j为根的树 的节点数
+        }
+    }
+
+    //n-sum 如图中的n-size值，不包括根节点4；
+    res = max(res, n - sum); // 选择u节点为重心，最大的 连通子图节点数
+    ans = min(res, ans); //遍历过的假设重心中，最小的最大联通子图的 节点数
     return sum;
 }
-int main()
-{
-    cin >> n;
-    memset(h, -1, sizeof h);
-    for (int i = 0; i < n - 1; i++)
-    {
+
+int main() {
+    memset(h, -1, sizeof h); //初始化h数组 -1表示尾节点
+    cin >> n; //表示树的结点数
+
+    // 题目接下来会输入，n-1行数据，
+    // 树中是不存在环的，对于有n个节点的树，必定是n-1条边
+    for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
-        add(a, b);
-        add(b, a);
+        add(a, b), add(b, a); //无向图
     }
-    dfs(1);
+
+    dfs(1); //可以任意选定一个节点开始 u<=n
+
     cout << ans << endl;
+
+    return 0;
 }
