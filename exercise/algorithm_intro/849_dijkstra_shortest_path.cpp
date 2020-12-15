c2fd3a34ed0090ceef69728e02e6a0778225fc3f
bruce1408 <summer56567@163.com>
2020-12-15 12:13:51 +0800

    dijsk

---
+++ exercise/algorithm_intro/849_dijkstra_shortest_path.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int N = 510;
+int n, m;
+int g[N][N];
+int dijkstra()
+{
+
+}
+int main()
+{
+    cin>>n>>m;
+    while(m--)
+    {
+        int a, b, c;
+        cin>>a>>b>>c;
+        
+    }
+}
