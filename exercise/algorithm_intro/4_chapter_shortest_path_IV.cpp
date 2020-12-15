f29f7c6ef48d930ff44e7f91df75102c3e1ed8fe
bruce1408 <summer56567@163.com>
2020-12-15 23:22:50 +0800

    dijkstra

--- exercise/algorithm_intro/4_chapter_shortest_path_IV.cpp
+++ exercise/algorithm_intro/4_chapter_shortest_path_IV.cpp
@@ -5,17 +5,14 @@ const int N = 510;
 const int inf = 1e9;
 
 int n, m, s, t;
-int g[N][N], cost[N][N], c[N], d[N], pre[N];
+int g[N][N], cost[N][N], d[N];
 bool visited[N];
+vector<int> pre[N];
 
 void dijkstra(int s)
 {
     fill(d, d + N, inf);
-    fill(c, c + N, 0);
     d[s] = 0;
-    c[s] = 0;
-    for (int i = 0; i < n; i++)
-        pre[i] = i;
     for (int i = 0; i < n; i++)
     {
         int u = -1, MIN = inf;
@@ -36,53 +33,32 @@ void dijkstra(int s)
                 if (d[u] + g[u][v] < d[v])
                 {
                     d[v] = d[u] + g[u][v];
-                    c[v] = c[u] + cost[u][v];
-                    pre[v] = u;
+                    pre[v].clear();
+                    pre[v].push_back(u);
                 }
                 else if (d[u] + g[u][v] == d[v])
                 {
-                    if (c[u] + cost[u][v] < c[v])
-                    {
-                        c[v] = c[u] + cost[u][v];
-                        pre[v] = u;
-                    }
+                    pre[v].push_back(u);
                 }
             }
         }
     }
 }
 
-void dfs(int s, int t)
-{
-    if (s == t)
-    {
-        cout << t << " ";
-        return;
-    }
-    else
-    {
-        dfs(s, pre[t]);
-        cout << t << " ";
-    }
-}
-
 int main()
 {
-    cin >> n >> m >> s >> t;
-    fill(g[0], g[0] + N * N, inf);
-    while (m--)
-    {
-        int a, b, c, d;
-        cin >> a >> b >> c >> d;
-        g[a][b] = g[b][a] = c;
-        cost[a][b] = cost[b][a] = d;
-    }
-    dijkstra(0);
-    cout << "最短路径为:" << endl;
-    dfs(s, t);
-    cout << endl;
-    cout << "起点到该点的最短路径长度为: " << d[t] << endl;
-    cout << "花费代价最小为: " << c[t] << endl;
+    // cin >> n >> m >> s >> t;
+    // fill(g[0], g[0] + N * N, inf);
+    // while (m--)
+    // {
+    //     int a, b, c, d;
+    //     cin >> a >> b >> c >> d;
+    //     g[a][b] = g[b][a] = c;
+    // }
+    // dijkstra(0);
+    // cout << "最短路径为:" << endl;
+    // cout << endl;
+    // cout << "起点到该点的最短路径长度为: " << d[t] << endl;
     /**
      * 输入如下：
      *  4 5 0 3
@@ -96,4 +72,17 @@ int main()
      * 3
      * 40
     */
+    vector<int> res[3];
+    res[0].push_back(0);
+    res[1].push_back(3);
+    res[1].push_back(4);
+    res[2].push_back(5);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
