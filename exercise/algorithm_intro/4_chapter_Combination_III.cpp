0b8d254e61b0ceaade79153ce5ded5230c51e005
bruce1408 <summer56567@163.com>
2020-12-19 23:29:35 +0800

    combination

--- exercise/algorithm_intro/4_chapter_Combination_III.cpp
+++ exercise/algorithm_intro/4_chapter_Combination_III.cpp
@@ -7,9 +7,9 @@ typedef long long LL;
 /**
  * 组合数的计算mod P的计算
  * 组合数的计算公式如下：
- *      m       n!
- *    C n =  --------- 
- *            m!(n-m)!
+ *                   n!
+ *    C(n, m) =  ---------- 
+ *                m!(n-m)!
  * 主要用来计算组合数模一个数的结果
 */
 
@@ -33,36 +33,64 @@ LL comb1mod(LL n, LL m, LL p)
 }
 
 /**
- * 方法 3，类似菲波那切数列的情况，会重复计算很多数字，所以开一个数组，用来记录已经
+ * 方法 2，类似菲波那切数列的情况，会重复计算很多数字，所以开一个数组，用来记录已经
  * 计算过的数字，如果下次再碰到就直接返回这个结果而不用重计算
  * 这里可以使用vector或者是全局数组都可以
 */
-// LL res[67][67];
-LL comb3(LL n, LL m, vector<vector<int>> &res)
+LL res[67][67];
+LL comb2mod(LL n, LL m, int p)
 {
     if (m == 0 || m == n)
         return 1;
     if (res[n][m] != 0)
         return res[n][m];
-    return res[n][m] = comb3(n - 1, m, res) + comb3(n - 1, m - 1, res);
+    return res[n][m] = (comb1(n - 1, m) + comb1(n - 1, m - 1)) % p;
 }
 
 /**
- * 方法 4，递推公式展开计算
+ * 方法 3，根据定义来做
+ * 使用快速幂+质因子的办法
+ * 计算C(n, m) % p;
 */
+const int N = 1e6;
+int prime[N];
+
+// cal函数用来求解n的阶乘的质因子数为p的个数，不是n
+int cal(int n, int p)
+{
+    int ans = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        int temp = i;
+        while (temp % p == 0)
+        {
+            temp /= p;
+            ans++;
+        }
+    }
+    return ans;
+}
+int binaryPow(int a, int b, int c)
+{
+    for(int i=2;i<=n)
+}
 LL comb4(LL n, LL m)
 {
     LL ans = 1;
-    for (LL i = 1; i <= m; i++)
+    // 遍历不超过所有n的指数质数
+    for (LL i = 0; prime[i] <= n; i++)
     {
-        ans = ans * (n - m + i) / i;
+        // 计算C(n,m)中prime[i]的指数c，cal(n, k)为n!含有质因子k的个数
+        int c = comb4(n, prime[i]) - comb4(m, prime[i]) - comb4(n - m, prime[i]);
+        // 快速幂计算prime[i]^c%p
+        ans = ans * binaryPow(prime[i], c, p) % p;
     }
     return ans;
 }
 int main()
 {
     int n = 4, m = 2, p = 5; // 这个数据范围使用定义法会越界，
-    cout << comb1mod(n, m, p) << endl;
+    cout << comb2mod(n, m, p) << endl;
     vector<vector<int>> res(67, vector<int>(67, 0));
     // cout << comb2(n, m, res) << endl;
     // cout << comb4(n, m) << endl;
