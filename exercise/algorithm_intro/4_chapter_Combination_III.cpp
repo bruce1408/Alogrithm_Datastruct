ea47926e3a66071876c53462cbe7b78494c67797
bruce1408 <summer56567@163.com>
2020-12-19 22:50:33 +0800

    combinatioin

--- exercise/algorithm_intro/4_chapter_Combination_III.cpp
+++ exercise/algorithm_intro/4_chapter_Combination_III.cpp
@@ -5,47 +5,32 @@ using namespace std;
 
 typedef long long LL;
 /**
- * 组合数的计算
+ * 组合数的计算mod P的计算
  * 组合数的计算公式如下：
  *      m       n!
  *    C n =  --------- 
  *            m!(n-m)!
- * 由公式可知上面的式子是通过3个整数的阶乘得到的
- * 一般有三种算法可以计算，定义直接计算法、递推公式计算法、定义式变形计算法
+ * 主要用来计算组合数模一个数的结果
 */
 
 /**
- * 方法 1，定义直接计算法
- * 先计算n！，然后再计算(n-m)!和m!, 使用long long 数据范围是小于等于20不会越界，否则会发生越界
-*/
-LL comb(LL n, LL m)
-{
-    LL ans1 = 1;
-    for (LL i = 1; i <= n; i++)
-    {
-        ans1 *= i;
-    }
-    for (LL i = 1; i <= m; i++)
-    {
-        ans1 /= i;
-    }
-    for (LL i = 1; i <= (n - m); i++)
-    {
-        ans1 /= i;
-    }
-    return ans1;
-}
-
-/**
- * 方法 2，递推公式来计算
+ * 方法 1递推公式来计算
  * C(n,m) = C(n-1, m) + C(n-1, m-1)
  * 这个递推公式可以最后使得n或者是m变成相同或者是让m变为0，可以作为递推边界
 */
+
 LL comb1(LL n, LL m)
 {
     if (m == n || m == 0)
         return 1;
-    return comb1(n - 1, m) + comb1(n - 1, m - 1);
+    return (comb1(n - 1, m) + comb1(n - 1, m - 1));
+}
+
+LL comb1(LL n, LL m, LL p)
+{
+    if (m == n || m == 0)
+        return 1;
+    return (comb1(n - 1, m) % p + comb1(n - 1, m - 1)) % p;
 }
 
 /**
@@ -77,9 +62,9 @@ LL comb4(LL n, LL m)
 }
 int main()
 {
-    int n = 21, m = 10; // 这个数据范围使用定义法会越界，
-    // cout<<comb1(n, m)<<endl;
+    int n = 4, m = 2, p = 5; // 这个数据范围使用定义法会越界，
+    cout << comb1(n, m, p) << endl;
     vector<vector<int>> res(67, vector<int>(67, 0));
     // cout << comb2(n, m, res) << endl;
-    cout << comb4(n, m) << endl;
+    // cout << comb4(n, m) << endl;
 }
