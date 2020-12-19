96996eddd1fbb5598a14154cfcb68c0450aab1c4
bruce1408 <summer56567@163.com>
2020-12-19 14:00:55 +0800

    inverse

--- exercise/algorithm_intro/876_binaryPow_inverse.cpp
+++ exercise/algorithm_intro/876_binaryPow_inverse.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 typedef long long LL;
-
+/**
+ * 快速幂求解逆元
+ * 若整数b，m互质，并且对于任意的整数 a，
+ * 如果满足b|a，则存在一个整数x，使得a/b≡a∗x(mod m)，则称x为b的模m乘法逆元，记为 b−1(mod m) 。
+ * b存在乘法逆元的充要条件是b与模数m互质。当模数m为质数时，bm−2即为b的乘法逆元。
+*/
 LL binaryPow(int a, int b, int c)
 {
     LL res = 1;
