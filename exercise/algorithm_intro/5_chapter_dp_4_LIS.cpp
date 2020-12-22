bf4c257140f0e5b8fcc10591d29a5bda96cdf3a3
bruce1408 <summer56567@163.com>
2020-12-22 13:21:54 +0800

    longestsubsequence

--- exercise/algorithm_intro/5_chapter_dp_4_LIS.cpp
+++ exercise/algorithm_intro/5_chapter_dp_4_LIS.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 /**
  * 最长不下降子序列
@@ -8,10 +9,27 @@ using namespace std;
  * 不下降子序列是
  * 1 2 3 7 9
  * 长度是5
+ * 
+ * 设置一个数组d[i]表示以A[i]结尾的最长不下降子序列
 */
+const int N = 20;
+int d[N];
 int main()
 {
-    vector<int> res = {1, 2, 3, -1, -2, 7, 9};
+    vector<int> res = {1, 2, 3, -1, -2, 7, 9, 10, -2, -9}; // 最长子序列是1,2,3,7,9,10
     int n = res.size();
-    
+    int maxnum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        d[i] = 1; // 边界初始条件
+        for (int j = 0; j < i; j++)
+        {
+            if (res[i] > res[j] && (d[j] + 1 > d[i]))
+            {
+                d[i] = d[j] + 1;
+            }
+        }
+        maxnum = max(maxnum, d[i]);
+    }
+    cout << maxnum << endl;
 }
