420bfb172383004dba9ce6ad42d4a0ee5da3842a
bruce1408 <summer56567@163.com>
2020-12-22 11:07:13 +0800

    maxsequecesum

--- exercise/algorithm_intro/5_chapter_dp_3_max_continue_subsequence_sum.cpp
+++ exercise/algorithm_intro/5_chapter_dp_3_max_continue_subsequence_sum.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
+/**
+ * 给定一个数字序列
+ * a1, a2, a3....an求i到j使得ai + ...aj最大，并输出这个最大和
+ * -2, 11, -4, 13, -5, -2 最大和是20
+ * 用dp[i]表示i为节点
+*/
 int main()
 {
-    
 }
