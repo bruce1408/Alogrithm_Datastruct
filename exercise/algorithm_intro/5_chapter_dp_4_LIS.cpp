c56d9a9ebe87ea049837b01295838012d119b763
bruce1408 <summer56567@163.com>
2020-12-22 13:02:18 +0800

    longest_subsequence

---
+++ exercise/algorithm_intro/5_chapter_dp_4_LIS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/**
+ * 最长不下降子序列
+ * 在一个数字序列中，找到最长的子序列保证它是不下降的
+ * 例如 
+ * 1 2 3 -1 -2 7 9
+ * 不下降子序列是
+ * 1 2 3 7 9
+ * 长度是5
+*/
+int main()
+{
+    vector<int> res = {1, 2, 3, -1, -2, 7, 9};
+    int n = res.size();
+    
+}
