1c570b42a2e0fd6675bae0da679cf11d60bb617e
bruce1408 <summer56567@163.com>
2020-12-24 00:41:15 +0800

    binaryIndextree

---
+++ Linked_List/109_SortedListToBST.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点的左右两个子树的高度差的绝对值不超过1。
+
+*/
+TreeNode* sortedListToBST(ListNode* head) 
+{
+    
+
+}
