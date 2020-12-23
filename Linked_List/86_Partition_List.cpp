2c7ab65c484cfa181fd1e0d72147db51cf33cafe
bruce1408 <summer56567@163.com>
2020-12-23 23:14:12 +0800

    partitionlist

--- Linked_List/86_Partition_List.cpp
+++ Linked_List/86_Partition_List.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 /**
- * 就是将所有小于给定值的节点取出组成一个新的链表，此时原链表中剩余的节点的值都大于或等于给定值，
+ * 就是将所有小于给定值的节点取出组成一个新的链表，
+ * 此时原链表中剩余的节点的值都大于或等于给定值，
  * 只要将原链表直接接在新链表后即可，代码如下：
  * Original: 4 -> 3 -> 5 -> 2 
  * New:　  1 -> 2
@@ -8,72 +12,77 @@
  * Original: 
  * New:　  1 -> 2 -> 2 -> 4 -> 3 -> 5
  * */
-#include<iostream>
-#include<vector>
-using namespace std;
-
 
 struct ListNode
 {
     int val;
     ListNode *next;
-    ListNode(int x):val(x),next(NULL){}
-}; 
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
-ListNode* partition(ListNode* head, int x)
+/**
+ * 方法 1，
+ * 使用两个虚拟头结点，然后小于x的放第一个，大于等于x的节点放第二个，
+ * 最后把两个节点合并起来就可以了，注意最后要加上末尾的节点为空NULL
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode *partition(ListNode *head, int x)
 {
-    ListNode *dummy = new ListNode(-1);
-    ListNode *newdummy = new ListNode(-1);
-    dummy->next = head;
-    ListNode *pre = dummy;
-    ListNode *cur = newdummy;
-    while(pre->next)
+    if (!head)
+        return head;
+    ListNode *left = new ListNode(-1), *curL = left;
+    ListNode *right = new ListNode(-2), *curR = right;
+    ListNode *cur = head;
+    while (cur)
     {
-        if(pre->next->val<x)
+        if (cur->val < x)
         {
-            cur->next = pre->next;
-            cur = cur->next;
-            pre->next = pre->next->next;
-            cur->next = nullptr;
+            curL->next = cur;
+            curL = curL->next;
         }
         else
         {
-            pre = pre->next;
+            curR->next = cur;
+            curR = curR->next;
         }
+        cur = cur->next;
     }
-    cur->next = dummy->next;
-    return newdummy->next;
+    curL->next = right->next;
+    curR->next = NULL;
+    return left->next;
 }
 
 void print_list(struct ListNode *head);
 
 int main()
 {
-	
-	ListNode *e = new ListNode(5);
-	ListNode *d = new ListNode(2);
-	ListNode *c = new ListNode(3);
-	ListNode *b = new ListNode(4);
-	ListNode *a = new ListNode(1);
-	a->next = b;
-	b->next = c;
-	c->next = d;
-	d->next = e;
-	
-	
-	// print_list(head);
+    ListNode *e = new ListNode(5);
+    ListNode *d = new ListNode(2);
+    ListNode *c = new ListNode(3);
+    ListNode *b = new ListNode(4);
+    ListNode *a = new ListNode(1);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    // print_list(head);
     print_list(partition(a, 3));
-
-	return 0;
-	
+    return 0;
 }
 
 void print_list(struct ListNode *head)
 {
-	while(head)
-	{
-		cout<<head->val<<"->";
-		head = head->next;
-	}
-	cout<<"end"<<endl;
+    while (head)
+    {
+        cout << head->val << "->";
+        head = head->next;
+    }
+    cout << "end" << endl;
 }
