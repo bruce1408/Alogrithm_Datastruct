e12a165e367db1fcadcd82c041125a477a0d4659
bruce1408 <summer56567@163.com>
2020-12-23 13:33:38 +0800

    reverseLinklist

--- Linked_List/206_ReverseLinkedList.cpp
+++ Linked_List/206_ReverseLinkedList.cpp
@@ -38,17 +38,61 @@ ListNode *reverseList1(ListNode *pHead)
 */
 ListNode *reverseList2(ListNode *head)
 {
-	ListNode *p = head;
+	if (head == nullptr)
+		return head;
 	ListNode *dummy = new ListNode(-1);
 	dummy->next = head;
-	while (p->next)
+	ListNode *cur = head;
+	while (cur->next)
 	{
-		ListNode *tmp = p->next;
-		p->next = tmp->next;
-		tmp->next = dummy->next;
-		dummy->next = tmp;
+		ListNode *p = cur->next;
+		cur->next = p->next;
+		p->next = dummy->next;
+		dummy->next = p;
 	}
 	return dummy->next;
+}
+
+/**
+ * 方法 3，使用两个指针来维护，一个从头指针开始a，一个从
+ * 头指针的下一位开始b，然后每次后面的指针指向前一位，b->next = a;
+ * a = b;b再指向下一位，所以再来一个临时指针，一开始就指向b
+*/
+ListNode *reverseList3(ListNode *head)
+{
+	if (head == nullptr)
+		return head;
+	ListNode *a = head, *b = a->next;
+	while (b)
+	{
+		ListNode *c = b->next;
+		b->next = a;
+		a = b;
+		b = c; // b要指向自己的下一位，所以一开始设c保存b的初始位置
+	}
+	head->next = nullptr;
+	return a;
+}
+/**
+ * 方法 4，使用递归的话，在方法 3的基础上做一个黑盒测试即可
+ * reverseList4函数就是翻转head作为头结点的链表，然后返回的是
+ * 最后一个节点，也是翻转之后的第一个节点；
+ * 		1->2->3->4->5 翻转之前
+ * 		1         2<-3<-4<-5  翻转之后
+ * 		head   head->next
+ * 
+ * 然后把head->next->next  = head，也就是2的指向1，然后1next指向NULL
+ * head->next = nullptr即可
+*/
+//这个函数以head为头结点翻转
+ListNode *reverseList4(ListNode *head)
+{
+	if (head == nullptr || head->next == nullptr)
+		return head;
+	auto tail = reverseList4(head->next);
+	head->next->next = head;
+	head->next = nullptr;
+	return tail;
 }
 
 int main()
@@ -67,7 +111,7 @@ int main()
 
 	print_list(a1);
 	// print_list(ReverseList(a1));
-	print_list(reverseList2(a1));
+	print_list(reverseList4(a1));
 	delete a1;
 	delete a2;
 	delete a3;
