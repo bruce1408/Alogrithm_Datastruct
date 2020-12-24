c43e3a0293b83d303d8c1989310ab6317e26f4eb
bruce1408 <summer56567@163.com>
2020-12-24 13:02:02 +0800

    avl

--- exercise/algorithm_intro/3_chapter_BST_AVL_tree.cpp
+++ exercise/algorithm_intro/3_chapter_BST_AVL_tree.cpp
@@ -16,51 +16,6 @@ using namespace std;
  *         5
  * 这种情况，使用平衡二叉树来构建
 */
-// struct node
-// {
-//     int data;
-//     node *left;
-//     node *right;
-//     node(int x) : data(x), left(NULL), right(NULL) {}
-// };
-
-// void preorder(node *root)
-// {
-//     if (root == nullptr)
-//         return;
-//     preorder(root->left);
-//     cout << root->data << " ";
-//     preorder(root->right);
-// }
-
-// void insert(node *&root, int x)
-// {
-//     if (root == nullptr)
-//     {
-//         root = new node(x);
-//         return;
-//     }
-//     if (root->data > x)
-//     {
-//         insert(root->left, x);
-//     }
-//     else
-//     {
-//         insert(root->right, x); // 这里其实包含了等于的情况，如果等于的话就在右子树上继续构建
-//     }
-// }
-
-// int main()
-// {
-//     node *root = NULL; // 定义头结点
-//     vector<int> res = {1, 2, 3, 4, 5};
-//     for (int i = 0; i < res.size(); i++)
-//     {
-//         insert(root, res[i]);
-//     }
-//     preorder(root);
-// }
-
 struct node
 {
     int data, height;
@@ -194,7 +149,7 @@ void R(node *&root)
  * -----------------------------------------------------------------------------
  * LR   bf(root)=3-1=2, bf(root->left)=1-2=-1     对root->left左旋，然后对root右旋  
  * -----------------------------------------------------------------------------
- * RR   bf(root)=1-3=-1, bf(root->right)=-1       对root左旋
+ * RR   bf(root)=1-3=-2, bf(root->right)=-1       对root左旋
  * -----------------------------------------------------------------------------
  * RL   bf(root)=1-3=-2, bf(root->right)=1        对root->right右旋，然后root左旋
  * -----------------------------------------------------------------------------
