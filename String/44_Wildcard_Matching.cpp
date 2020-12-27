871dfd0734aa3b0eafe70c29849b02a945547679
bruce1408 <summer56567@163.com>
2020-12-28 00:23:11 +0800

    wildcard

--- String/44_Wildcard_Matching.cpp
+++ String/44_Wildcard_Matching.cpp
@@ -1,31 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 /**
- * 
+ * 44 通配符匹配，和leetcode 10 题很类似
+ * 这里？表示可以匹配所有字符，*表示可以匹配任意长度字符
 */
-bool isMatch(string s, string p) {
+
+bool isMatch(string s, string p)
+{
     // 和 10题那个正则表达式子不同的是，这里的*可以前面不用有字符
     int n = s.size(), m = p.size();
-    vector<vector<bool>>dp(n+1, vector<bool>(m+1, false));
+    vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
     dp[0][0] = true;
-    for(int i=1; i<=m; i++)
-    {   
+    for (int i = 1; i <= m; i++)
+    {
         // 如果s个数0，那么p只要是*，都满足条件
-        if(p[i-1]=='*') dp[0][i] = dp[0][i-1];
+        if (p[i - 1] == '*')
+            dp[0][i] = dp[0][i - 1];
     }
 
-    for(int i=1; i<=n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for(int j=1; j<=m; j++)
+        for (int j = 1; j <= m; j++)
         {
-            if(p[j-1] == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+            if (p[j - 1] == '*')
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
             else
-            {  
-                dp[i][j] = (s[i-1]==p[j-1] || p[j-1]=='?') && dp[i-1][j-1];
+            {
+                dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
             }
         }
     }
     return dp[n][m];
-
 }
