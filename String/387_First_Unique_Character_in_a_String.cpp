b003da24752e58f3d5d582e76d41bb5f49ea42e3
bruce1408 <summer56567@163.com>
2021-01-02 01:35:18 +0800

    387

--- String/387_First_Unique_Character_in_a_String.cpp
+++ String/387_First_Unique_Character_in_a_String.cpp
@@ -1,51 +1,40 @@
-/*387. First Unique Character in a String
-第一个没有重复字符的下标
-
-
-s = "leetcode"
-return 0.
-
-s = "loveleetcode",
-return 2.
-*/
-
-#include<iostream>
-#include<string>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
+/**
+ * 387. First Unique Character in a String
+ * 第一个没有重复字符的下标
+ * 
+ * s = "leetcode" 
+ * return 0.
+ * 
+ * s = "loveleetcode", 
+ * return 2.
+*/
+
+/**
+ * 方法 1，使用哈希表来做即可
+ * 存储每个字符出现的次数，然后再次遍历这个字符串，如果当前的哈希表中次数为1，那么返回下标即可，
+ * 如果遍历结束都没有满足，那么返回-1即可
+*/
 int firstUniqChar(string s)
 {
-	unordered_map<char,int>res;
-	for(char i:s)
+	unordered_map<char, int> res;
+	for (char i : s)
 		++res[i];
-	for(int i=0;i<s.size();i++)
+	for (int i = 0; i < s.size(); i++)
 	{
-		if(res[s[i]]==1)
+		if (res[s[i]] == 1)
 			return i;
 	}
 	return -1;
-		
 }
 
 int main()
 {
-	string s="leetcode";
-	string t="loveleetcode";
-	cout<<firstUniqChar(t);
-	
+	string s = "leetcode";
+	string t = "loveleetcode";
+	cout << firstUniqChar(t);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
