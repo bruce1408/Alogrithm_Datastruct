6d970603c1fc40c84ec8cd1ccefd55c3deb4e03a
bruce1408 <summer56567@163.com>
2021-01-06 16:25:21 +0800

    273

--- String/227_Basic_Calculator_II.cpp
+++ String/227_Basic_Calculator_II.cpp
@@ -1,27 +1,74 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
 using namespace std;
 
+/**
+ * 227,基本计算器
+ * 给定一个字符串，例如"3 + 2 /1",然后返回这个表达式的计算结果
+ * 这里使用两个栈，一个是运算符，一个是数字
+ * 每次遍历当前字符，如果是空格直接跳过，如果是数字的话，那么遍历到数字结尾，然后把这个
+ * 字符串数字转成数字即可，然后对当前这个数字保存到栈里面去，并把指针指向前一个位置
+ * 如果当前不是数字，那么可能是运算符，所以首先需要判断运算符栈是不是空栈，如果是空栈，直接保存运算符即可
+ * 否则就执行eval函数，eval就是把数字栈中的两个数字取出来，然后和运算符里面的进行运算，结果再保存到
+ * 数字栈即可。
+*/
 stack<int> nums;
 stack<char> op;
-int calculate(string s) 
+
+void eval()
 {
-    for(int i=0;i<s.size();i++)
+    int r;
+    int b = nums.top();
+    nums.pop();
+    int a = nums.top();
+    nums.pop();
+    char c = op.top();
+    op.pop();
+    if (c == '-')
+        r = a - b;
+    else if (c == '+')
+        r = a + b;
+    else if (c == '*')
+        r = a * b;
+    else
+        r = a / b;
+    nums.push(r);
+}
+
+int calculate(string s)
+{
+    unordered_map<char, int> pr;
+    pr['+'] = pr['-'] = 1, pr['*'] = pr['/'] = 2;
+    for (int i = 0; i < s.size(); i++)
     {
-        if(s[i]==' ') continue;
-        if(isdigit(s[i]))
+        auto c = s[i];
+        if (c == ' ')
+            continue;
+        if (isdigit(c))
         {
-            int j = i;
-            while(j<s.size() && isdigit(s[j]))
-                j++;
-            
-            
+            int j = i, num = 0;
+            while (j < s.size() && isdigit(s[j]))
+                num = num * 10 + (s[j++] - '0');
+            nums.push(num); // 把当前的数字存入栈中
+            i = j - 1;
+        }
+        else
+        {
+            while (op.size() && pr[op.top()] >= pr[c])
+                eval();
+            op.push(c);
         }
     }
+    while (op.size())
+        eval();
+    return nums.top();
 }
 
 int main()
 {
-
+    string a = "3/2";
+    cout << calculate(a) << endl;
 }
--- String/273_Integer_to_English_Words.cpp
+++ String/273_Integer_to_English_Words.cpp
@@ -1,218 +1,57 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
 using namespace std;
 
-// 超级经典的题
-class Solution
+/**
+ * 273 把数字转换为字符表示
+ * 123 -> "One Hundred Twenty Three"
+ * */
+
+/**
+ * 方法 1，先把是1-999的范围的数字表示成为一个字符串，然后再按照位数添加
+ * 然后这里小于20的数不一样，有单独的表达，所以三位数还要看一下十位是不是小于20
+*/
+string convertHundred(int num);
+
+string numberToWords(int num)
 {
-    public:
-    // typedef vector<string> VS; // typedef 新的定义
-    string numberToWords(int num)
+    string res = convertHundred(num % 1000);
+    vector<string> v = {"Thousand", "Million", "Billion"};
+    for (int i = 0; i < 3; i++)
     {
-        string res = convertHundred(num%1000);
-        vector<string> v = {"Thousand", "Million", "Billion"};
-        for(int i=0;i<3;i++)
-        {
-            num/=1000;
-            res = num%1000 ? convertHundred(num%1000)+" " + v[i]+" " + res:res;
-        }
-        while(res.back()==' ')res.pop_back(); // 123000 比如后面都是0删除 ' '
-        return res.empty()? "Zero":res;
+        num /= 1000;
+        res = num % 1000 ? convertHundred(num % 1000) + " " + v[i] + " " + res : res;
     }
-    string convertHundred(int num)
-    {
-        vector<string> v1 = {"","One","Two", "Three", "Four", "Five", 
-        "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 
-        "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    while (res.back() == ' ')
+        res.pop_back(); // 123000 比如后面都是0删除 ' '
+    return res.empty() ? "Zero" : res;
+}
 
-        vector<string> v2 = {"","","Twenty", "Thirty", "Forty", "Fifty", "Sixty",
-        "Seventy", "Eighty", "Ninety"};
-    
-        string res;
-        int a = num/100, b = num%100, c=num%10;
-        res = b<20 ? v1[b] : v2[b/10] + ( c? " " + v1[c]:"");
-        if(a>0) res = v1[a] + " Hundred" +(b ? " "+res:"");
-        return res;
-    }
-};
+string convertHundred(int num)
+{
+    vector<string> v1 = {"", "One", "Two", "Three", "Four", "Five",
+                         "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+                         "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
+    vector<string> v2 = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+                         "Seventy", "Eighty", "Ninety"};
 
-// class Solution
-// {
-//     public:
-//     typedef vector<string> VS;
-//     string numberToWords(int num)
-//     {
-//         string res = convertHundred(num%1000);
-//         VS v = {"Thousand", "Million", "Billion"};
-//         for(int i=0;i<3;i++)
-//         {
-//             num/=1000;
-//             res = num%1000 ? convertHundred(num%1000)+" " + v[i]+" " + res:res;
-//         }
-//         while(res.back()==' ')res.pop_back(); // 123000 比如后面都是0删除 ' '
-//         return res.empty()? "Zero":res;
-//     }
+    string res;
+    int a = num / 100, b = num % 100, c = num % 10;
+    res = b < 20 ? v1[b] : v2[b / 10] + (c ? " " + v1[c] : "");
+    if (a > 0)
+        res = v1[a] + " Hundred" + (b ? " " + res : "");
+    return res;
+}
 
-//     string convertHundred(int num)
-//     {
-//         VS v1 = {"","One","Two", "Three", "Four", "Five", 
-//         "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 
-//         "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-
-//         VS v2 = {"","","Twenty", "Thirty", "Forty", "Fifty", "Sixty",
-//         "Seventy", "Eighty", "Ninety"};
-    
-//         string res;
-//         int a = num/100, b = num%100, c=num%10;
-//         res = b<20 ? v1[b] : v2[b/10] + ( c? " " + v1[c]:"");
-//         if(a>0) res = v1[a] + " Hundred" +(b ? " "+res:"");
-//         return res;
-//     }
-// };
-
+/**
+ * 方法 2
+*/
 
 int main()
 {
-    Solution s;
-    cout<<s.numberToWords(294564)<<endl;
-}
 
-// 方法二。
-// class Solution2 
-// {
-//     int findPlace (int placeNum)
-//     {
-//         if (placeNum % 3 == 0)
-//             return 100;
-//         else if ((placeNum+1) % 3 == 0)
-//             return 10;
-//         else
-//             return 1;
-//     }
-//     string findOnes(char digit)
-//     {
-//         switch(digit)
-//         {
-//             case '9':
-//                 return "Nine ";
-//             case '8':
-//                 return "Eight ";
-//             case '7':
-//                 return "Seven ";
-//             case '6':
-//                 return "Six ";
-//             case '5':
-//                 return "Five ";
-//             case '4':
-//                 return "Four ";
-//             case '3':
-//                 return "Three ";
-//             case '2':
-//                 return "Two ";
-//             case '1':
-//                 return "One ";
-//         }
-//     }
-//     string findTens(char digit)
-//     {
-//         switch(digit){
-//             case '9':
-//                 return "Ninety ";
-//             case '8':
-//                 return "Eighty ";
-//             case '7':
-//                 return "Seventy ";
-//             case '6':
-//                 return "Sixty ";
-//             case '5':
-//                 return "Fifty ";
-//             case '4':
-//                 return "Forty ";
-//             case '3':
-//                 return "Thirty ";
-//             case '2':
-//                 return "Twenty ";
-//         }
-//     }
-//     string findSpecialTens(char digit)
-//     {
-//         switch(digit)
-//         {
-//             case '9':
-//                 return "Nineteen ";
-//             case '8':
-//                 return "Eighteen ";
-//             case '7':
-//                 return "Seventeen ";
-//             case '6':
-//                 return "Sixteen ";
-//             case '5':
-//                 return "Fifteen ";
-//             case '4':
-//                 return "Fourteen ";
-//             case '3':
-//                 return "Thirteen ";
-//             case '2':
-//                 return "Twelve ";
-//             case '1':
-//                     return "Eleven ";
-//             case '0':
-//                     return "Ten ";
-//         }   
-//     }
-// public:
-//     string numberToWords(int num) 
-//     {
-//         if(num == 0)
-//             return "Zero";
-//         char lastNum = '0';
-//         string result = "";
-//         string number = to_string(num);
-//         int place = number.size();
-//         for(int i = 0; i < number.size(); i++)
-//         {
-//             if(number[i] != '0')
-//             {
-//                 lastNum = number[i];
-//                 int subPlace = (findPlace(place));
-//                 switch(subPlace)
-//                 {
-//                     case 100:
-//                         result += findOnes(number[i]) + "Hundred ";
-//                         break;
-//                     case 10:
-//                         if(number[i] == '1'){
-//                             ++i;
-//                             place--;
-//                             result += findSpecialTens(number[i]);                    
-//                         }                  
-//                         else
-//                             result += findTens(number[i]);
-//                         break;
-//                     case 1:
-//                         result += findOnes(number[i]);
-//                     default:
-//                         break;
-//                 }
-//             }
-//             if(place == 4 || place == 7 || place == 10)
-//             {
-//                 if(lastNum != '0')
-//                 {
-//                     if(place == 4)
-//                         result += "Thousand ";
-//                     if(place == 7 )
-//                         result += "Million ";
-//                     if(place == 10)
-//                         result += "Billion ";
-//                     lastNum = '0';
-//                 }
-//             }
-//             --place;
-//         }
-//         return result.substr(0, result.size()-1);
-//     }
-// };
+    cout << numberToWords(294564) << endl;
+}
