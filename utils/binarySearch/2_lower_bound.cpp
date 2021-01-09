12e9b489c908574461c68054c6d0c9eba93a8c14
bruce1408 <summer56567@163.com>
2021-01-09 17:15:00 +0800

    220 lowerbound

--- utils/binarySearch/2_lower_bound.cpp
+++ utils/binarySearch/2_lower_bound.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <set>
 using namespace std;
-
+/**
+ * 返回的是lower_bound 是大于等于x的最小下标
+*/
 void minnum(vector<int> &num)
 {
     multiset<int> res;
@@ -11,10 +13,9 @@ void minnum(vector<int> &num)
     {
         res.insert(num[i]);
     }
+    cout << "大于等于当前数7的第一个数是: " << *res.lower_bound(7) << endl;
     for (multiset<int>::iterator iter = res.begin(); iter != res.end(); iter++)
-    {
-        cout << *res.lower_bound(7) << endl;
-    }
+        cout << *iter << endl;
 }
 int main()
 {
