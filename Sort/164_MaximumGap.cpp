e6f0c01bda93bccbc4076a25947dc4d78adb40dc
bruce1408 <summer56567@163.com>
2021-01-09 11:35:40 +0800

    179

--- Sort/164_MaximumGap.cpp
+++ Sort/164_MaximumGap.cpp
@@ -12,6 +12,8 @@ using namespace std;
 /**
  * 方法 1，最简单的思路就是使用排序，然后排序之后两两比较，这样算可以算出来，但是肯定能不满足
  * 题目的线性时间复杂度和空间复杂度，超时
+ * 所以使用桶排序的思想，对n个数字，设置n-1个区间，然后尽可能的让每个数均匀的落在每个区间即可
+ * 
 */
 struct bucket
 {
@@ -32,19 +34,30 @@ int maximumGap(vector<int> &nums)
 	if (n < 2 || Max == Min)
 		return 0;
 	vector<bucket> r(n - 1);				 // 区间数
-	int len = (Max - Min - n - 2) / (n - 1); // 这一步没有看懂
+	int len = (Max - Min + n - 2) / (n - 1); // 这一步没有看懂
 	for (auto x : nums)						 // 找到每一个区间的最小值和最大值
 	{
 		if (x == Min)
 			continue;
-		int k = (x - Min - 1) / (n - 1);
+		int k = (x - Min - 1) / len;
 		r[k].used = true;
 		r[k].min = min(r[k].min, x);
 		r[k].max = max(r[k].max, x);
 	}
-	
+	int res = 0;
+	for (int i = 0, last = Min; i < n - 1; i++)
+	{
+		if (r[i].used)
+		{
+			res = max(res, r[i].min - last);
+			last = r[i].max;
+		}
+	}
+	return res;
 }
 
 int main()
 {
+	vector<int> res = {3, 6, 9, 1};
+	cout << maximumGap(res) << endl;
 }
--- Sort/179_LargestNumber.cpp
+++ Sort/179_LargestNumber.cpp
@@ -1,47 +1,86 @@
-// given [3, 30, 34, 5, 9], the largest formed number is 9534330
-/*sort的比较函数的写法是很关键的，还有就是求最小的组合的
-  时候要用的是小于号，防止0打头的情况出现。
-  */
-#include<iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution
-{
-	public:
-	string largestNumber(vector<int> &nums)
-	{
-		sort(nums.begin(),nums.end(),compare);
-		string res;
-		for(auto i:nums)
-		{
-			res+=to_string(i);
-		}
-		//以下两句是说去掉最小数的时候0打头的情况，一般最大数的同时为0的情况也需要判断
-		auto pos = res.find_first_not_of('0');
-        return pos == string::npos ? "0" : res.substr(pos);
-		// return res; //在求最大的数的时候没有问题，但是如果是最小数的话那么可能有0打头。
-	}
-	
-	
-	/*   类内的比较函数一定是静态函数   */
-	static bool compare(int a,int b)
-	{
-		auto stra = to_string(a);
-		auto strb = to_string(b);
-		return (stra+strb>strb+stra);//小于号就是返回最小数。
-	}
-	
-};
+/**
+ * 179 找到数组中数字元素组合的最大数字
+ * 比如：
+ * given [3, 30, 34, 5, 9], the largest formed number is 9534330
+*/
 
+/**
+ * 方法 1，定义一个新的排序操作，
+ * 就是a<=b等价于ab <= ba;
+ * a==b 等价于 ab == ba;
+ * 当时这种新的比较符号是不是可以进行比较呢？
+ * 比如：
+ * a<b; b<c; c<a;
+ * 不满足这个比较定义；
+ * 所以需要证明一下新的比较运算是不是满足全序关系，参考链接如下
+ * https://baike.baidu.com/item/%E5%85%A8%E5%BA%8F%E5%85%B3%E7%B3%BB/943310?fromtitle=%E5%85%A8%E5%BA%8F&fromid=10577699
+*/
+static bool compare(int a, int b)
+{
+	auto stra = to_string(a);
+	auto strb = to_string(b);
+	return (stra + strb > strb + stra); //小于号就是返回最小数。
+}
+
+string largestNumber1(vector<int> &nums)
+{
+	sort(nums.begin(), nums.end(), compare);
+	string res;
+	for (auto i : nums)
+	{
+		res += to_string(i);
+	}
+	//以下两句是说去掉最小数的时候0打头的情况，一般最大数的同时为0的情况也需要判断
+	auto pos = res.find_first_not_of('0');
+	return pos == string::npos ? "0" : res.substr(pos);
+	// return res; //在求最大的数的时候没有问题，但是如果是最小数的话那么可能有0打头。
+}
+
+/**
+ * 方法 2，使用dfs深度优先搜索来找到所有的可能结果，然后进行筛选即可
+*/
+void dfs(vector<int> &nums, int n, vector<string> &res, int k, string s, unordered_map<int, bool> &used)
+{
+	if (k == n)
+	{
+		res.push_back(s);
+		return;
+	}
+	// 关键是当下如何选择，下一步和当前一步是一样的
+	for (int i = 0; i < n; i++)
+	{
+		if (used[nums[i]] == false)
+		{
+			used[nums[i]] = true;
+			string t = to_string(nums[i]);
+			dfs(nums, n, res, k + 1, s + t, used);
+			used[nums[i]] = false;
+		}
+	}
+}
+
+string largestNumber2(vector<int> &nums)
+{
+	int n = nums.size();
+	vector<string> res;
+	unordered_map<int, bool> used;
+	for (auto i : nums)
+		used[i] = false;
+	dfs(nums, n, res, 0, "", used);
+	for (auto i : res)
+		cout << i << endl;
+	return "";
+}
 
 int main()
 {
-	
-	vector<int> num = {0,0,0};
-	Solution s;
-	
-	cout<<s.largestNumber(num)<<endl;
+	vector<int> num = {0, 1, 4};
+	cout << largestNumber1(num) << endl;
 }
