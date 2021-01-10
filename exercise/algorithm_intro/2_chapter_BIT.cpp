37ae3c3d060aaa5b63cf9700b8ee24ddf36e964a
bruce1408 <summer56567@163.com>
2021-01-10 22:10:30 +0800

    binaryindexedtree

---
+++ exercise/algorithm_intro/2_chapter_BIT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/**
+ * 树状数组，binary indexed tree，用来解决两类问题
+ * 第一就是快速求解前缀和
+ * 第二就是修改某一个数
+ * 上面两个步骤的时间复杂度都是O(logn)
+*/
+
+
+int main()
+{
+}
