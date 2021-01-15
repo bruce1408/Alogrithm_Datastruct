f1e58db8b573bf7a3b96f050fef98d71a5e2323a
bruce1408 <summer56567@163.com>
2021-01-15 12:30:57 +0800

    bit

--- exercise/algorithm_intro/2_chapter_BIT_IV.cpp
+++ exercise/algorithm_intro/2_chapter_BIT_IV.cpp
@@ -4,15 +4,24 @@
 using namespace std;
 /**
  * 楼兰图腾数字
- * 
+ * 给出一个长度为nn的数组，完成以下两种操作：
+ * 1. 将第ii个数加上kk
+ * 2. 输出区间[i,j][i,j]内每个数的和
+ * 详细步骤思路参考：
+ * 从左向右依次遍历每个数a[i]，使用树状数组统计在i位置之前所有比a[i]大的数的个数、以及比a[i]小的数的个数。
+ * 统计完成后，将a[i]加入到树状数组。
+ * 从右向左依次遍历每个数a[i]，使用树状数组统计在i位置之后所有比a[i]大的数的个数、以及比a[i]小的数的个数。
+ * 统计完成后，将a[i]加入到树状数组
+ * https://www.acwing.com/solution/content/13818/
 */
 const int N = 2000010;
-
 int c[N], a[N], n;
 
 int g[N], l[N];
 typedef long long LL;
 #define lowbit(i) ((i)&(-i))
+
+
 
 // 更新函数
 void update(int x, int v)
@@ -45,16 +54,16 @@ int main()
         update(y, 1);
     }
     
-    LL res1 = 0, res2 =0;
+    LL resV = 0, resA =0;
     memset(c, 0 , sizeof c);
     for(int i=n;i>0;i--)
     {
         int y = a[i];
-        res1+=g[i] * (LL)(getSum(n) - getSum(y));
-        res2+=l[i] *(LL)(getSum(y));
+        resV+=g[i] * (LL)(getSum(n) - getSum(y));
+        resA+=l[i] *(LL)(getSum(y));
         update(y, 1);
     }
     
-    printf("%lld %lld\n", res1, res2);
+    printf("%lld %lld\n", resV, resA);
     
 }
