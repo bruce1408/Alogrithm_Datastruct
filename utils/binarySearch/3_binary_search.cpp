d7219c9c3ca5cfdd223cf4cde3dd843bb6024eea
bruce1408 <summer56567@163.com>
2021-01-22 15:56:43 +0800

    binary

--- utils/binarySearch/3_binary_search.cpp
+++ utils/binarySearch/3_binary_search.cpp
@@ -2,7 +2,16 @@
 #include <vector>
 using namespace std;
 
-int findNums(vector<int> &res, int k)
+/**
+ * 二分搜索模板的情况
+ * firstK 表示的是 第一个大于等于的情况
+ * lastK 表示的是 最后一个小于等于的情况
+*/
+
+/**
+ * 返回第一个大于等于该数的位置
+*/
+int firstK(vector<int> &res, int k)
 {
     int l = 0, r = res.size();
     while (l < r)
@@ -16,13 +25,16 @@ int findNums(vector<int> &res, int k)
     return l;
 }
 
-int lastGreater(vector<int> &res, int k)
+/**
+ * 返回最后一个小于等于该数的位置
+*/
+int lastK(vector<int> &res, int k)
 {
     int l = 0, r = res.size();
     while (l < r)
     {
         int mid = (l + r + 1) >> 1;
-        if (res[mid] < k)
+        if (res[mid] <= k)
             l = mid;
         else
             r = mid - 1;
@@ -33,14 +45,14 @@ int lastGreater(vector<int> &res, int k)
 int main()
 {
 
-    vector<int> res = {1, 2, 3, 4, 5, 7, 8, 9};
+    vector<int> res = {1, 2, 3, 3, 5, 7, 8, 9};
     int n, k;
     cout << "input a nums n" << endl;
     cin >> n;
     while (n--)
     {
         cin >> k;
-        cout << "target x first location is: " << findNums(res, k) << endl;
-        cout << "target x last location is: " << lastGreater(res, k) << endl;
+        cout << "target x first location is: " << firstK(res, k) << endl;
+        cout << "target x last location is: " << lastK(res, k) << endl;
     }
 }
