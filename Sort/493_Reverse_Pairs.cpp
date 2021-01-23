4fa27f11f4c214812b5e31aed2ff4874ca458599
bruce1408 <summer56567@163.com>
2021-01-23 22:09:19 +0800

    bit

--- Sort/493_Reverse_Pairs.cpp
+++ Sort/493_Reverse_Pairs.cpp
@@ -29,6 +29,10 @@ int reversePairs(vector<int> &nums)
 /**
  * 方法 2，使用树状数组来做
  * 和327 题目非常类似
+ * 首先将 nums 中的数字和每个数字的 2 倍统一进行离散化到 [1, 2n]。
+ * 接下来仿照求逆序对的思路，从数组最后开始往前遍历，遍历过程中，在树状数组中查找小于等于 nums[i] 离散化后的值 x 再减 1 的个数。然后将 nums[i]*2 离散化后的值加入到树状数组中。
+ * 时间复杂度 离散化的时间复杂度为 O(nlogn)O(nlog⁡n)，树状数组每次操作的时间复杂度为 O(logn)O(log⁡n)，故总时间复杂度为 O(nlogn)O(nlog⁡n)。
+ * 空间复杂度 需要额外 O(n)O(n) 的空间存储离散化数组和树状数组。
 */
 typedef long long LL;
 int m;
@@ -61,7 +65,7 @@ int reverseParis2(vector<int> &nums)
     for (int i = 0; i < n; i++)
     {
         b[i] = nums[i];
-        b[i + n] = 2 * nums[i];
+        b[i + n] = 2 * (LL)nums[i]; // 防止越界
     }
     sort(b.begin(), b.end());
     m = unique(b.begin(), b.end()) - b.begin();
