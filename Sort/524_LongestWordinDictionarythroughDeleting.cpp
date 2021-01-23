3b51dfcaae21301eac4793bc50f8694cff0324fa
bruce1408 <summer56567@163.com>
2021-01-24 01:39:41 +0800

    524

--- Sort/524_LongestWordinDictionarythroughDeleting.cpp
+++ Sort/524_LongestWordinDictionarythroughDeleting.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,43 +8,12 @@ using namespace std;
  * 删除后得到的字典中的最长单词
 */
 
-/*第一种方法；用排序，按字符串的长度由小到大，如果长度相同，按照字幕顺序，然后遍历
-  字典里的每一个字符，看待测的字符是不是和他匹配，如果是，返回，因为遍历每个，所以
-  后面出现的也可能匹配，最后会返回一个最后匹配的字符*/
-// class Solution
-// {
-// public:
-// string findLongestWord(string s, vector<string>& d)
-// {
-// sort(d.begin(), d.end(),comp);
-// for (string str : d)
-// {
-// int i = 0;
-// for (char c : s)
-// {
-// if (i < str.size() && c == str[i])
-// ++i;
-// }
-// if (i == str.size()) return str;
-// }
-// return "";
-// }
-// static bool comp(string a,string b)
-// {
-// if(a.size() == b.size())
-// return a<b;
-// else
-// return a.size()>b.size();
-
-// }
-// };
-
-/*不需要排序；只是用一个记录的数字来更新就可以了
-  这个算法很好理解，思路很好！！
-  和上面的思路大致一样，只不过没有排序，一直在更新最新的带选取的字符串。如果找到匹
-  配的字符串，是否i和字符串的长度一样并且str长度>=存放结果的字符串，然后再看，str字符串
-  要比你原来的字符串长度长，或者如果相同的话，我的首字母要小于你的首字母，也就是
-  相同长度去字母最小*/
+/**
+ * 方法 1，不需要排序；只是用一个记录的数字来更新就可以了
+ * 这个算法很好理解，思路很好！！
+ * 和上面的思路大致一样，只不过没有排序，一直在更新最新的带选取的字符串。如果找到匹配的字符串，
+ * 是否i和字符串的长度一样并且str长度>=存放结果的字符串，然后再看，str字符串
+ * 要比你原来的字符串长度长，或者如果相同的话，我的首字母要小于你的首字母，也就是相同长度去字母最小*/
 
 class Solution
 {
@@ -70,7 +38,7 @@ public:
 };
 
 /**
- * 方法 3，遍历字符串数组d，然后寻找s的字符是否和d可以匹配。
+ * 方法 2，遍历字符串数组d，然后寻找s的字符是否和d可以匹配。
 */
 bool compare(string s, string a)
 {
@@ -78,9 +46,7 @@ bool compare(string s, string a)
 	while (i < s1 && j < a1)
 	{
 		if (s[i] == a[j])
-		{
 			i++, j++;
-		}
 		else
 			i++;
 	}
@@ -90,10 +56,11 @@ bool compare(string s, string a)
 		return false;
 }
 
-string findLongestWord3(string s, vector<string> &d)
+string findLongestWord2(string s, vector<string> &d)
 {
 
 	string ans = "";
+	sort(d.begin(), d.end());
 	for (int i = 0; i < d.size(); i++)
 	{
 		if (compare(s, d[i]))
@@ -109,11 +76,10 @@ int main()
 {
 	string s = "abpcplea";
 	vector<string> d = {"ale", "apple", "monkey", "plea", "aplea"};
-	// vector<string> d = {"a", "b", "c"};
 	string s1;
 	// Solution ss;
 	// s1 = ss.findLongestWord(s, d);
-	s1 = findLongestWord3(s, d);
+	s1 = findLongestWord2(s, d);
 	cout << s1 << endl;
 	return 0;
 }
