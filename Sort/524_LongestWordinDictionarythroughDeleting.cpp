852b68ddc34d761304cc86ce705590c4a278b4d0
bruce1408 <summer56567@163.com>
2021-01-24 09:47:07 +0800

    524

--- Sort/524_LongestWordinDictionarythroughDeleting.cpp
+++ Sort/524_LongestWordinDictionarythroughDeleting.cpp
@@ -13,29 +13,29 @@ using namespace std;
  * 这个算法很好理解，思路很好！！
  * 和上面的思路大致一样，只不过没有排序，一直在更新最新的带选取的字符串。如果找到匹配的字符串，
  * 是否i和字符串的长度一样并且str长度>=存放结果的字符串，然后再看，str字符串
- * 要比你原来的字符串长度长，或者如果相同的话，我的首字母要小于你的首字母，也就是相同长度去字母最小*/
+ * 要比你原来的字符串长度长，或者如果相同的话，我的首字母要小于你的首字母，也就是相同长度去字母最小
+ */
 
-class Solution
+/**
+ * 方法 1，不用进行排序步骤。
+*/
+string findLongestWord1(string s, vector<string> &d)
 {
-public:
-	string findLongestWord(string s, vector<string> &d)
+	string res = "";
+	for (string str : d)
 	{
-		string res = "";
-		for (string str : d)
+		int i = 0;
+		for (char c : s)
 		{
-			int i = 0;
-			for (char c : s)
-			{
-				if (i < str.size() && c == str[i])
-					++i;
-			}
-			if (i == str.size() && str.size() >= res.size())
-				if (str.size() > res.size() || str < res) // 大于和等于两种情况的时候!!
-					res = str;
+			if (i < str.size() && c == str[i])
+				++i;
 		}
-		return res;
+		if (i == str.size() && str.size() >= res.size())
+			if (str.size() > res.size() || str < res) // 大于和等于两种情况的时候!!
+				res = str;
 	}
-};
+	return res;
+}
 
 /**
  * 方法 2，遍历字符串数组d，然后寻找s的字符是否和d可以匹配。
@@ -46,14 +46,10 @@ bool compare(string s, string a)
 	while (i < s1 && j < a1)
 	{
 		if (s[i] == a[j])
-			i++, j++;
-		else
-			i++;
+			j++;
+		i++;
 	}
-	if (j == a1)
-		return true;
-	else
-		return false;
+	return a1 == j;
 }
 
 string findLongestWord2(string s, vector<string> &d)
@@ -77,9 +73,7 @@ int main()
 	string s = "abpcplea";
 	vector<string> d = {"ale", "apple", "monkey", "plea", "aplea"};
 	string s1;
-	// Solution ss;
-	// s1 = ss.findLongestWord(s, d);
-	s1 = findLongestWord2(s, d);
+	s1 = findLongestWord1(s, d);
 	cout << s1 << endl;
 	return 0;
 }
---
+++ Sort/912_SortArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include << algorithm>
+using namespace std;
+
+/**
+ * 912 数组排序，给一个无序的数组， 然后排序即可
+ * 这里直接使用快排的模板即可
+*/
+void quick_sort(vector<int> &res, int l, int r)
+{
+    if (l >= r)
+        return;
+    int i = l - 1, j = r + 1;
+    int x = res[(l + r) / 2];
+    while (i < j)
+    {
+
+        while (res[++i] < x)
+            ;
+        while (res[--j] > x)
+            ;
+        if (i < j)
+            swap(res[i], res[j]);
+    }
+    quick_sort(res, l, j);
+    quick_sort(res, j + 1, r);
+}
+
+vector<int> sortArray(vector<int> &res)
+{
+    quick_sort(res, 0, res.size() - 1);
+    return res;
+}
+
+int main()
+{
+    vector<int> res = {5, 2, 3, 1};
+    sortArray(res);
+}
