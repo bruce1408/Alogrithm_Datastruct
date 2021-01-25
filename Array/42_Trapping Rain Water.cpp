0c18b26fd0449a726c8c00ce4b03c73492258281
bruce1408 <summer56567@163.com>
2021-01-25 17:51:13 +0800

    trap

--- Array/42_Trapping Rain Water.cpp
+++ Array/42_Trapping Rain Water.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<stack>
  using namespace std;
 
 /**
@@ -12,6 +13,14 @@
  * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
  * 
 */
+
+/**
+ * 方法 1，使用一个单调栈来做
+*/
+int trap(vector<int>& height)
+{
+    stack<int>res;
+}
  int main()
  {
 
