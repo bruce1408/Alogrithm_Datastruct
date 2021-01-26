070c1d3b6304739fd96b9b43018aa9c6e5286dae
bruce1408 <summer56567@163.com>
2021-01-26 17:15:32 +0800

    74 search 2d

--- Array/74_Search_a_2D_Matrix.cpp
+++ Array/74_Search_a_2D_Matrix.cpp
@@ -23,8 +23,7 @@ using namespace std;
  */
 
 /**
- * 方法 1，暴力搜索使用两层for循环来做，注意这里两层for循环是变体，
- * 增加了因为找到条件之后就终止
+ * 方法 1，暴力搜索
 */
 bool searchMatrix1(vector<vector<int>> &matrix, int target)
 {
@@ -51,17 +50,90 @@ bool searchMatrix1(vector<vector<int>> &matrix, int target)
 }
 
 /**
- * 方法 2，可以使用二分查找的思路来做，二分查找用来查找二维数组的情况
+ * 方法 2，可以使用二分查找的思路来做，用两个二分来确定范围
+ * 第一个二分来确定在哪一行，第二个二分判断在哪一个列
+ * 使用第一个二分确定行之后，要分情况讨论一下，
+ * 首先如果l是第一行的话直接确定，
+ * 如果l是其他行的话，判断上一行的最后一个数是不是大于等于t，如果大于的话，说明在l-1这一行
+ * 否则最后应该是在l行不用减一
+ * 确定行之后，在确定列，还是使用二分查找即可
  */
-bool searchMatrix2(vector<vector<int>> &mat, int t)
+bool searchMatrix(vector<vector<int>> &mat, int t)
 {
-    
+
+    if (mat.empty() || mat[0].empty())
+        return false;
+    int row = mat.size(), col = mat[0].size();
+    // if (row == 1 && col == 1) //  这个判断条件和上面的等价
+    //     return mat[0][0] == t ? true : false;
+    int l = 0, r = row - 1;
+    // 先确定在哪一行
+    int k = 0;
+    if (row > 1)
+    {
+        while (l < r)
+        {
+            int mid = (l + r) >> 1;
+            if (mat[mid][0] >= t)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        if (mat[l][0] == t)
+            return true;
+        // 行的三种情况
+        if (l == 0) // 第一行
+            k = 0;
+        else if (mat[l - 1][col - 1] >= t)
+            k = l - 1;
+        else
+            k = l;
+    }
+
+    l = 0, r = col - 1;
+    while (l < r)
+    {
+        int mid = (l + r) >> 1;
+        if (mat[k][mid] >= t)
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    if (mat[k][l] == t)
+        return true;
+    return false;
 }
+
+/**
+ * 方法 3，整个二维数组是严格有序的，所以可以把二维数组转换为1位数组来组，要注意映射空间关系
+ * mid -> [mid / col][mid % col]
+ * 然后按照一位二分来做即可
+ * */
+bool searchMatrix3(vector<vector<int>> &mat, int t)
+{
+    if (mat.empty() || mat[0].empty())
+        return false;
+    int row = mat.size(), col = mat[0].size();
+    int l = 0, r = row * col - 1;
+    while (l < r)
+    {
+        int mid = (l + r) >> 1;
+        if (mat[mid / col][mid % col] >= t)
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    if (mat[l / col][l % col] == t)
+        return true;
+    else
+        return false;
+}
+
 int main()
 {
     vector<vector<int>> res = {
         {1, 3, 5, 7},
         {10, 11, 16, 20},
         {23, 30, 34, 50}};
-    cout << searchMatrix2(res, 3) << endl;
+    cout << searchMatrix2(res, 1) << endl;
 }
