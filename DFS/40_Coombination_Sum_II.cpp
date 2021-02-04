9a4006973916edf4678ea33629f8d5ad71ad026b
bruce1408 <summer56567@163.com>
2021-02-05 00:33:00 +0800

    40

--- DFS/40_Coombination_Sum_II.cpp
+++ DFS/40_Coombination_Sum_II.cpp
@@ -33,7 +33,7 @@ void dfs(vector<int> &c, int t, int start, vector<int> &out, vector<vector<int>>
         if (i > start && c[i] == c[i - 1]) // 去重
             continue;
         out.push_back(c[i]);
-        dfs(c, t - c[i], i + 1, out, res);
+        dfs(c, t - c[i], i + 1, out, res); // 这里的i+1表示该c[i]元素不可以重复使用
         out.pop_back();
     }
 }
@@ -46,6 +46,44 @@ vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
     dfs(candidates, target, 0, out, res);
     return res;
 }
+
+/**
+ * 方法 2，dfs的背包模板写法
+*/
+class Solution
+{
+public:
+    vector<int> out;
+    vector<vector<int>> res;
+    vector<vector<int>> combinationSum2(vector<int> &c, int t)
+    {
+        sort(c.begin(), c.end());
+        if (c.empty())
+            return {};
+        dfs(c, t, 0, 0);
+        return res;
+    }
+
+    void dfs(vector<int> &c, int t, int sum, int index)
+    {
+        if (t == sum)
+        {
+            res.push_back(out);
+            return;
+        }
+        if (sum > t || index == c.size())
+            return;
+
+        int k = index + 1;
+        while (k < c.size() && c[index] == c[k])
+            k++;
+
+        dfs(c, t, sum, k);
+        out.push_back(c[index]);
+        dfs(c, t, sum + c[index], index + 1); // index+1 表示第c[index]数字不可以重复使用
+        out.pop_back();
+    }
+};
 
 int main()
 {
