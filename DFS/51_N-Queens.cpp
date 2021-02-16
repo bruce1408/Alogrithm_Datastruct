21f6f29493f00c53d2d668ed2317e35a75039bbc
bruce1408 <summer56567@163.com>
2021-02-17 00:44:13 +0800

    51

---
+++ DFS/51_N-Queens.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+*/
+
+/**
+ * 方法 1，N皇后可以上下左右移动，也可以在对角线移动，所以设置三个数组分别记录当前位置的列，对角线
+ * 是不是存在，然后这里主对角线和副对角线的方程如下：
+ * 对角线映射到下标
+ * 正： y = x + b        b = y - x + n(偏移量保证正）
+ * 反： y = -x + b       b = y + x
+ * b就是映射下的坐标
+*/
+class Solution
+{
+public:
+    vector<string> out;
+    vector<vector<string>> res;
+    int n;
+    vector<bool> col, dg, udg;
+
+    vector<vector<string>> solveNQueens(int m)
+    {
+        n = m;
+        col = vector<bool>(n);
+        dg = udg = vector<bool>(n * 2);
+        out = vector<string>(n, string(n, '.'));
+        dfs(0);
+        return res;
+    }
+
+    void dfs(int k)
+    {
+        if (k == n)
+        {
+            res.push_back(out);
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (!col[i] && !dg[k - i + n] && !udg[k + i])
+            {
+                col[i] = dg[k - i + n] = udg[k + i] = true;
+                out[k][i] = 'Q';
+                dfs(k + 1);
+                out[k][i] = '.';
+                col[i] = dg[k - i + n] = udg[k + i] = false;
+            }
+        }
+    }
+};
