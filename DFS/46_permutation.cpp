2f813d477abb01e9b381ddb68e7076419cebc618
bruce1408 <summer56567@163.com>
2021-02-16 18:21:37 +0800

    46

--- DFS/46_permutation.cpp
+++ DFS/46_permutation.cpp
@@ -21,44 +21,39 @@ using namespace std;
  * 给一个数组，数字互不相同，求这组数的全排列，这里主要负数
 */
 
-void dfsLeetcode(int index, vector<int> &path, vector<bool> &visited, vector<int> &nums, vector<vector<int>> &temp)
-{
-    int len = nums.size();
-    if (index == len)
-    {
-        vector<int> res;
-        for (int i = 0; i < len; i++)
-        {
-            res.push_back(path[i]);
-        }
-        temp.push_back(res);
-        return;
-    }
-    for (int i = 0; i < len; i++)
-    {
-        if (visited[i] == false)
-        {
-            visited[i] = true;
-            path[index] = nums[i];
-            dfsLeetcode(index + 1, path, visited, nums, temp);
-            visited[i] = false;
-        }
-    }
-}
-
+vector<int> out;
+vector<vector<int>> res;
 vector<vector<int>> permute(vector<int> &nums)
 {
+    vector<bool> visited(nums.size(), 0);
 
-    // 假设这个数组是已经排序好的
-    int n = nums.size();
-    // sort(nums.begin(), nums.end());
-    vector<vector<int>> temp;
-    vector<int> path(n, 0);
-    vector<bool> visited(n, false);
-    dfsLeetcode(0, path, visited, nums, temp);
-    return temp;
+    dfs(nums, 0, visited);
+    return res;
 }
 
+void dfs(vector<int> &nums, int index, vector<bool> &visited)
+{
+    if (index == nums.size())
+    {
+        res.push_back(out);
+        return;
+    }
+    if (index > nums.size())
+        return;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (visited[i] == true)
+            continue;
+        else
+        {
+            out.push_back(nums[i]);
+            visited[i] = true;
+            dfs(nums, index + 1, visited);
+            visited[i] = false;
+            out.pop_back();
+        }
+    }
+}
 int main()
 {
     vector<int> ans = {0, -1, 1};
