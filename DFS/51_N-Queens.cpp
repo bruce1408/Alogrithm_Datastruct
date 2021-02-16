faedca3e0085172b73f59197ff2baab6afef2b8b
bruce1408 <summer56567@163.com>
2021-02-17 00:46:54 +0800

    51

--- DFS/51_N-Queens.cpp
+++ DFS/51_N-Queens.cpp
@@ -27,29 +27,31 @@ public:
     vector<vector<string>> solveNQueens(int m)
     {
         n = m;
-        col = vector<bool>(n);
-        dg = udg = vector<bool>(n * 2);
-        out = vector<string>(n, string(n, '.'));
+        col = vector<bool>(n);                   // 列
+        dg = udg = vector<bool>(n * 2);          // 两条对角线
+        out = vector<string>(n, string(n, '.')); // out 初始化为'.'
         dfs(0);
         return res;
     }
 
     void dfs(int k)
     {
-        if (k == n)
+        if (k == n) // 如果k==n那么就是最后一行，保存结果
         {
             res.push_back(out);
             return;
         }
         for (int i = 0; i < n; i++)
         {
+            // 如果当前i列和对角线没有存放元素，就操作如下
             if (!col[i] && !dg[k - i + n] && !udg[k + i])
             {
+                // 置位true
                 col[i] = dg[k - i + n] = udg[k + i] = true;
-                out[k][i] = 'Q';
+                out[k][i] = 'Q'; // 元素占位Q
                 dfs(k + 1);
                 out[k][i] = '.';
-                col[i] = dg[k - i + n] = udg[k + i] = false;
+                col[i] = dg[k - i + n] = udg[k + i] = false; // 恢复现场
             }
         }
     }
