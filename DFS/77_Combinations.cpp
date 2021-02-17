a61be58744b3c4010b4dbe828dbc9275df55f4b7
bruce1408 <summer56567@163.com>
2021-02-17 10:45:58 +0800

    77

---
+++ DFS/77_Combinations.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+/**
+ * 给定两个数字，求出所有的组合
+*/
+// 给两个数字进行全排列c(4,2) = 6
+void helper(int n, int k, vector<int> out, vector<vector<int>> &res, int begin)
+{
+    if (out.size() == k)
+    {
+        res.push_back(out);
+        return;
+    }
+    for (int i = begin; i <= n; i++)
+    {
+        out.push_back(i);
+        helper(n, k, out, res, i + 1);
+        out.pop_back();
+    }
+}
+vector<vector<int>> combine(int n, int k)
+{
+    if (k > n || k < 0)
+        return {};
+    vector<vector<int>> res;
+    vector<int> out;
+    helper(n, k, out, res, 1);
+    return res;
+}
+
+// method2
+vector<vector<int>> combine1(int n, int k)
+{
+    if (k > n || k < 0)
+        return {};
+    vector<vector<int>> res = combine1(n - 1, k - 1);
+    for (auto &i : res)
+        i.push_back(n);
+    for (auto &i : combine(n - 1, k))
+        res.push_back(i);
+    return res;
+}
