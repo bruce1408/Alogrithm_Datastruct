efb20fc8cd6dc5118d8fc942e24197abdf733370
bruce1408 <summer56567@163.com>
2021-02-23 20:01:17 +0800

    306

--- DFS/306_Additive_Number.cpp
+++ DFS/306_Additive_Number.cpp
@@ -16,44 +16,49 @@ using namespace std;
 
 string add(string x, string y)
 {
-    vector<int>a, b, c;
-    for(int i=x.size()-1;i>=0;i--) a.push_back(x[i]- '0');
-    for(int i=y.size()-1;i>=0;i--) b.push_back(y[i]- '0');
-    for(int i=0,t = 0;i<a.size() || i < b.size() || t;i++)
+    vector<int> a, b, c;
+    for (int i = x.size() - 1; i >= 0; i--)
+        a.push_back(x[i] - '0');
+    for (int i = y.size() - 1; i >= 0; i--)
+        b.push_back(y[i] - '0');
+    for (int i = 0, t = 0; i < a.size() || i < b.size() || t; i++)
     {
-        if(i < a.size()) t+=a[i];
-        if(i < b.size()) t+=b[i];
+        if (i < a.size())
+            t += a[i];
+        if (i < b.size())
+            t += b[i];
         c.push_back(t % 10);
-        t /=10;
+        t /= 10;
     }
     string z;
-    for(int i=c.size()-1; i>=0; i--)
+    for (int i = c.size() - 1; i >= 0; i--)
     {
-        z+=to_string(c[i]);
+        z += to_string(c[i]);
     }
     return z;
 }
 
 bool isAdditiveNumber(string num)
 {
-    for(int i=0;i<num.size();i++)
+    for (int i = 0; i < num.size(); i++)
     {
-        for(int j =i+1;j+1<num.size();j++)
+        for (int j = i + 1; j + 1 < num.size(); j++)
         {
             int a = -1, b = i, c = j;
-            while(true)
+            while (true)
             {
                 // 前导0的情况去掉
-                if(b - a > 1 && num[a+1] =='0' || c-b>1 && num[b+1]=='0') break;
-                auto x = num.substr(a+1, b-a), y = num.substr(b+1, c- b);
+                if (b - a > 1 && num[a + 1] == '0' || c - b > 1 && num[b + 1] == '0')
+                    break;
+                auto x = num.substr(a + 1, b - a), y = num.substr(b + 1, c - b);
                 auto z = add(x, y);
-                if(z != num.substr(c+1, z.size())) break;
-                a = b, b = c, c+=z.size();
-                if(c+1==num.size()) return true;
+                if (z != num.substr(c + 1, z.size()))
+                    break;
+                a = b, b = c, c += z.size();
+                if (c + 1 == num.size())
+                    return true;
             }
-
         }
     }
     return false;
-
 }
