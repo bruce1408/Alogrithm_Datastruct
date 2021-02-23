d16721154952d07f919f22d632cdd670b4f41815
bruce1408 <summer56567@163.com>
2021-02-23 18:55:42 +0800

    trie

--- DFS/131_Palindrome_Partitioning.cpp
+++ DFS/131_Palindrome_Partitioning.cpp
@@ -13,6 +13,10 @@ vector<vector<bool>>f;
 /**
  * 方法 1，使用dfs，先用一个二维数组记录这个字符串的所有回文串的位置，然后方便后面
  * 查找的使用
+ * 判断ij这一段是不是回文串的时候，如果i和j相同，那么ij位置是true
+ * 如果si==sj,两种情况，要么是i+1 > j-1,说明是两个字符，要么是f[i+1][j-1]=true
+ * 那么f[i][j] = true;说明是回文串的情况
+ * 然后开始dfs搜索遍历情况
 */
 vector<vector<string>> partition(string s) {
     int n = s.size();
@@ -44,10 +48,10 @@ void dfs(string &s, int u)
     {
         for(int i = u; i<s.size(); i++)
         {
-            if(f[u][i])
+            if(f[u][i]) // 是回文串的情况下，把i-u+1长度的s加进去即可
             {
                 out.push_back(s.substr(u, i-u+1));
-                dfs(s, u+1);
+                dfs(s, i+1);
                 out.pop_back();
 
             }
