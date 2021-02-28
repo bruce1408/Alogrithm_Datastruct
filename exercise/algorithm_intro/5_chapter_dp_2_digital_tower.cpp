0743157d81e3b1631098cd7395ecc1d19b99ef54
bruce1408 <summer56567@163.com>
2021-02-28 20:32:23 +0800

    dp

--- exercise/algorithm_intro/5_chapter_dp_2_digital_tower.cpp
+++ exercise/algorithm_intro/5_chapter_dp_2_digital_tower.cpp
@@ -58,8 +58,30 @@ int main()
     */
 }
 
+int main()
+{
+    cin>>n;
+    for(int i= 1;i<=n;i++)
+    {
+        for(int j = 1;j<=i;j++)
+        {
+            cin>>f[i][j];
+        }
+    }
+
+    for(int i = n-1;i;i--)
+    {
+        for(int j= 1; j<=i;j++)
+        {
+            f[i][j] = max(f[i+1][j], f[i+1][j+1]) + f[i][j];
+        }
+    }
+
+    cout<<f[1][1]<<endl;
+}
+
 /**
- * 方法 2，从上往下计算 
+ * 方法 2，从上往下计算，不推荐算法，需要判断边界
 */
 const int N = 510, INF = 1e9;
 int n;
