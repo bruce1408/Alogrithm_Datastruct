8039f9411fce0531a9381c1bec9d37411eeb692f
bruce1408 <summer56567@163.com>
2021-03-01 23:43:20 +0800

    53

--- Dynamic_Programming/53_Maximum_Subarray.cpp
+++ Dynamic_Programming/53_Maximum_Subarray.cpp
@@ -116,11 +116,34 @@ int maxSubArray3(vector<int> &nums)
     return helper(nums, 0, nums.size() - 1);
 }
 
+/**
+ * 方法 4，使用dp动态规划这个算法
+ * 1.设 f(i)f(i) 表示以第i个数字为结尾的最大连续子序列的总和是多少。
+ * 2.初始化 f(0)=nums[0]f(0)=nums[0]。
+ * 3.转移方程 f(i)=max(f(i−1)+nums[i], nums[i]); 
+ * f(i)=max(f(i−1)+nums[i],nums[i])。可以理解为当前有两种决策，一种是将第i个数字和前边的数字拼接起来；
+ * 另一种是第i个数字单独作为一个新的子序列的开始。
+ * 4.最终答案为 ans=max(f(k)), 0≤k<nans=max(f(k)), 0≤k<n 。
+*/
+int maxSubArray4(vector<int> &nums)
+{
+    int n = nums.size(), ans;
+    vector<int> f(n);
+    f[0] = nums[0];
+    ans = f[0];
+    for (int i = 1; i < n; i++)
+    {
+        f[i] = max(f[i - 1] + nums[i], nums[i]);
+        ans = max(ans, f[i]);
+    }
+    return ans;
+}
+
 int main()
 {
     vector<int> nums = {-1};
     cout << maxSubArray3(nums) << endl;
-    cout << maxSubArray_(nums) << endl;
+    cout << maxSubArray4(nums) << endl;
 
     return 0;
 }
