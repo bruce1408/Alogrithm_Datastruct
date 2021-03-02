655a71ee0b7745bbd941f72e36da4d85af6af3b5
bruce1408 <summer56567@163.com>
2021-03-02 21:42:37 +0800

    45

--- Dynamic_Programming/45_Jupm_Game_II.cpp
+++ Dynamic_Programming/45_Jupm_Game_II.cpp
@@ -13,7 +13,7 @@ using namespace std;
  * f[i]可以是从0~i-1跳过来的，时间复杂度是O(n^2)
 */
 
-int jump(vector<int> &nums)
+int jump1(vector<int> &nums)
 {
     int n = nums.size();
     vector<int> f(n + 1);
@@ -32,15 +32,16 @@ int jump(vector<int> &nums)
 }
 
 /**
- * 方法 1，使用动态规划来做
+ * 方法 2，使用动态规划来做
  * 状态f[i]表示从0点到i的最少步数
  * 初始化状态f为0，然后开始遍历，i从1开始，j从0开始
  * 判断j和当前的nums[j] 和是不是小于当前的i，如果小于当前i
  * 说明j的位置跳nums[j]是跳不到i的位置的，所以j要往后自加一位
  * 否则j的位置跳nums[j]可以跳到i的位置，那么j不用自加
  * 状态f[i] = f[j] + 1即可
+ * 时间复杂度是O(n)
 */
-int jump(vector<int> &nums)
+int jump2(vector<int> &nums)
 {
     int n = nums.size(), j = 0;
     vector<int> f(n);
@@ -50,9 +51,7 @@ int jump(vector<int> &nums)
             j++;
         f[i] = f[j] + 1;
     }
-    for (auto i : f)
-        cout << i << " ";
-    cout << endl;
+
     return f[n - 1];
 }
 
