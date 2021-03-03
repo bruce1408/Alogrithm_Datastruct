1f80f005fef0b9a5c9ac1ca05f4b82eb70d82854
bruce1408 <summer56567@163.com>
2021-03-04 01:36:14 +0800

    115

---
+++ Dynamic_Programming/115_Distinct_Subsequences.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/**
+ * 115 不同的序列
+ * 给定字符s和t，计算s的子序列中t出现的次数
+*/
+
+/**
+ * 方法 1，使用动态规划来做
+*/
+int numDistinct(string s, string t) 
+{
+
+}
