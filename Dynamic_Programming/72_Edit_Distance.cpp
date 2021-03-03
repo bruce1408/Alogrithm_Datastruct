075eaf635a8a107fec339ae1aab38413083b9d20
bruce1408 <summer56567@163.com>
2021-03-03 20:19:24 +0800

    72

--- Dynamic_Programming/72_Edit_Distance.cpp
+++ Dynamic_Programming/72_Edit_Distance.cpp
@@ -2,7 +2,13 @@
 #include<vector>
 using namespace std;
 
-int minDistance(string word1, string word2) 
+int minDistance(string w1, string w2) 
 {
-    
+    int n = w1.size(), b = w`2.size();
+
+    string a = ' ' + w1, b = ' ' + w2;
+    int n = a.size(), b = w2.size();
+    vector<vector<int>>f(n, vector<int>(m));
+
+    int a = 
 }
