7dc683681262ac215d4eff7a257475de22829588
bruce1408 <summer56567@163.com>
2021-03-03 23:48:20 +0800

    343

--- Dynamic_Programming/343_Integer_Break.cpp
+++ Dynamic_Programming/343_Integer_Break.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+/**
+ * 343 整数拆分，把一个整数进行拆分，将其拆分成至少两个整数的和，然后使得这个
+ * 整数的乘积的最大化，返回这个乘积的最大乘积即可；
+ * 
+*/
+
+/**
+ * 方法 1，使用动态规划算法来做
+*/
 int integerBreak(int n)
 {
     
