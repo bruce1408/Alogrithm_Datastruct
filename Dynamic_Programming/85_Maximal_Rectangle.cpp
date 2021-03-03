4edc69ce7dfad7478c25c675aa902ee34da3624c
bruce1408 <summer56567@163.com>
2021-03-03 21:03:18 +0800

    dp

---
+++ Dynamic_Programming/85_Maximal_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/**
+ * 85 最大矩形；给定一个二维矩阵，然后矩阵元素只包含0-1两个数
+ * 然后找到面积最大的矩形块输出即可 
+*/
+
+/**
+ * 方法 1，使用动态规划算法来做
+ * fij状态表示
+*/
+int maximalRectangle(vector<vector<char>>& matrix) 
+{
+
+}
