ffee1e863b5be804dd458e928f48d24650f54b6e
bruce1408 <summer56567@163.com>
2021-03-05 01:52:47 +0800

    221

--- Dynamic_Programming/221_Maximal_Square.cpp
+++ Dynamic_Programming/221_Maximal_Square.cpp
@@ -75,17 +75,23 @@ int maximalSquare(vector<vector<char> > &matrix)
 {
     if (matrix.empty() || matrix[0].empty())
         return 0;
-    int n = matrix.size(), m = matrix[0].size();
-    vector<vector<int> > f(n + 1, vector<int>(m + 1));
-
-    int res = 0;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            if (matrix[i - 1][j - 1] == '1')
+    int m = matrix.size(), n = matrix[0].size(), res = 0;
+    vector<vector<int> > dp(m, vector<int>(n, 0));
+    
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = matrix[i][j] - '0';
+            else if (matrix[i][j] == '1')
             {
-                f[i][j] = min(f[i - 1][j], min(f[i][j - 1], f[i - 1][j - 1])) + 1;
-                res = max(res, f[i][j]);
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
             }
-
+            res = max(res, dp[i][j]);
+        }
+    }
     return res * res;
 }
+}
+;
