6e7dc23bb568a396740c5371b51d7e286a3f1576
bruce1408 <summer56567@163.com>
2021-03-05 01:37:02 +0800

    221

--- Dynamic_Programming/221_Maximal_Square.cpp
+++ Dynamic_Programming/221_Maximal_Square.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /**
@@ -14,9 +14,11 @@ using namespace std;
  * 否则f[i][j]等于f[i][j-1]上一个列的宽度+1;=>f[i][j] = f[i][j-1] + 1;
  * 
 */
-class Solution {
+class Solution
+{
 public:
-    int maximalSquare(vector<vector<char>>& s) {
+    int maximalSquare(vector<vector<char> > &s)
+    {
         int n = s.size();
         if (n == 0)
             return 0;
@@ -24,39 +26,66 @@ public:
         // int n = s.size(), m = s[0].size();
 
         // 状态fij表示ij结尾的格子的最长宽度是多少
-        vector<vector<int>>f(n, vector<int>(m));
+        vector<vector<int> > f(n, vector<int>(m));
         // 状态初始化部分
 
-        for(int i = 0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = 0; j<m; j++)
-            // 如果s[i][j]是1的话，说明有值
-            if(s[i][j] == '1')
-            {
-                // j=0表示只有1列，宽度是1
-                if(j == 0) f[i][j] = 1;
-                // 否则ij表示的是f[i][j-1] + 1;
-                else f[i][j] = f[i][j-1] + 1;
-            }
-            else f[i][j] = 0; // 如果s[i][j]等于0的话，那么就是宽度为0
+            for (int j = 0; j < m; j++)
+                // 如果s[i][j]是1的话，说明有值
+                if (s[i][j] == '1')
+                {
+                    // j=0表示只有1列，宽度是1
+                    if (j == 0)
+                        f[i][j] = 1;
+                    // 否则ij表示的是f[i][j-1] + 1;
+                    else
+                        f[i][j] = f[i][j - 1] + 1;
+                }
+                else
+                    f[i][j] = 0; // 如果s[i][j]等于0的话，那么就是宽度为0
         }
 
         int ans = 0;
-        for(int i = 0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = 0;j<m;j++)
+            for (int j = 0; j < m; j++)
             {
                 int len = INT_MAX;
-                for(int k = i;k<n;k++)
+                for (int k = i; k < n; k++)
                 {
                     len = min(len, f[k][j]);
-                    if(len == 0) break;
+                    if (len == 0)
+                        break;
                     // 这里u表示的高度，然后和85题目不一样的是u和len相同表示正方形，不是矩形
-                    int u = (k - i + 1); 
-                    if(len == u)  ans = max(ans, len * u);
+                    int u = (k - i + 1);
+                    if (len == u)
+                        ans = max(ans, len * u);
                 }
             }
         }
         return ans;
     }
 };
+
+/**
+ * 方法 2，使用动态规划来做，和上面的思路一样，但是写法不一样
+*/
+int maximalSquare(vector<vector<char> > &matrix)
+{
+    if (matrix.empty() || matrix[0].empty())
+        return 0;
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int> > f(n + 1, vector<int>(m + 1));
+
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (matrix[i - 1][j - 1] == '1')
+            {
+                f[i][j] = min(f[i - 1][j], min(f[i][j - 1], f[i - 1][j - 1])) + 1;
+                res = max(res, f[i][j]);
+            }
+
+    return res * res;
+}
