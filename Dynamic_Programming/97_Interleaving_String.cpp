0d169a3964765040a9b7e35eb500cf2544f1d9a0
bruce1408 <summer56567@163.com>
2021-03-05 10:01:30 +0800

    97

--- Dynamic_Programming/97_Interleaving_String.cpp
+++ Dynamic_Programming/97_Interleaving_String.cpp
@@ -16,12 +16,29 @@ using namespace std;
  * f[i][j] = f[i-1,j]
  * 同理，f[i][j] = f[i][j-1];
 */
+
 bool isInterleave(string s1, string s2, string s3)
 {
     int n = s1.size(), m = s2.size();
-    if(s3.size() != n+m) return false;
-    vector<vector<bool>>f(n, vector<bool>(m));
+    if (s3.size() != n + m)
+        return false;
+    s1 = ' ' + s1, s2 = ' ' + s2, s3 = ' ' + s3;
+    vector<vector<bool> > f(n + 1, vector<bool>(m + 1));
 
-    
-
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 && j == 0)
+                f[i][j] = true;
+            else
+            {
+                if (i && s1[i] == s3[i + j])
+                    f[i][j] = f[i - 1][j];
+                if (j && s2[j] == s3[i + j])
+                    f[i][j] = f[i][j] || f[i][j - 1];
+            }
+        }
+    }
+    return f[n][m];
 }
