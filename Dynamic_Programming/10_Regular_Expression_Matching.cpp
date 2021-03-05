4fdb3020a35be058bd32460531883c82be7d7ad3
bruce1408 <summer56567@163.com>
2021-03-05 11:32:25 +0800

    10

--- Dynamic_Programming/10_Regular_Expression_Matching.cpp
+++ Dynamic_Programming/10_Regular_Expression_Matching.cpp
@@ -66,12 +66,12 @@ bool isMatch2(string s, string p)
     {
         for (int j = 1; j <= m; j++) //j从1开始，j从0没有意义
         {
-            if (p[j - 1] == '*')
+            if (p[j - 1] == '*' ** j > 1)
             {
                 dp[i][j] = dp[i][j - 2] || (i && dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
             }
             else
-                dp[i][j] = i && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                dp[i][j] = i>0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
         }
     }
     return dp[n][m];
