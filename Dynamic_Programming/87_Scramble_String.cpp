135821f7111f96b8b67c73ed890751b3a2bcd656
bruce1408 <summer56567@163.com>
2021-03-05 20:43:44 +0800

    87

---
+++ Dynamic_Programming/87_Scramble_String.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/**
+ * 87 扰乱字符串即可
+ * 给定一个字符串然后用二叉树来表示，然后挑选一个叶子节点翻转，
+ * 翻转之后组成新的字符串即可
+*/
+
+bool isScramble(string s1, string s2) 
+{
+    
+}
+
