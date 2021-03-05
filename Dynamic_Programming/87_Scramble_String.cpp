39f1fc9ac6fd30439d97a84291ce40c08e4a9032
bruce1408 <summer56567@163.com>
2021-03-05 21:16:07 +0800

    87

--- Dynamic_Programming/87_Scramble_String.cpp
+++ Dynamic_Programming/87_Scramble_String.cpp
@@ -1,15 +1,34 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /**
  * 87 扰乱字符串即可
  * 给定一个字符串然后用二叉树来表示，然后挑选一个叶子节点翻转，
  * 翻转之后组成新的字符串即可
+ * 判断s1是不是干扰之后产生的字符串s2
 */
 
-bool isScramble(string s1, string s2) 
+class Solution
 {
-    
-}
+public:
+    bool isScramble(string s1, string s2)
+    {
+        int n = s1.size(), m = s2.size();
+        if (s1 == s2)
+            return true;
+        string bs1 = s1, bs2 = s2;
+        sort(bs1.begin(), bs1.end()), sort(bs2.begin(), bs2.end());
+        if (bs1 != bs2)
+            return false;
 
+        for (int i = 1; i <= n - 1; i++)
+        {
+            if (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))
+                return true;
+            if (isScramble(s1.substr(0, i), s2.substr(m - i)) && isScramble(s1.substr(i), s2.substr(0, m - i)))
+                return true;
+        }
+        return false;
+    }
+};
