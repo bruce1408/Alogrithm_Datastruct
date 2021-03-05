c5bf828765b2dfc201f2f9d34a630c3e5057c507
bruce1408 <summer56567@163.com>
2021-03-05 21:32:08 +0800

    87

--- Dynamic_Programming/87_Scramble_String.cpp
+++ Dynamic_Programming/87_Scramble_String.cpp
@@ -9,26 +9,27 @@ using namespace std;
  * 判断s1是不是干扰之后产生的字符串s2
 */
 
-class Solution
+/**
+ * 方法 1，使用递归来做
+*/
+bool isScramble(string s1, string s2)
 {
-public:
-    bool isScramble(string s1, string s2)
-    {
-        int n = s1.size(), m = s2.size();
-        if (s1 == s2)
-            return true;
-        string bs1 = s1, bs2 = s2;
-        sort(bs1.begin(), bs1.end()), sort(bs2.begin(), bs2.end());
-        if (bs1 != bs2)
-            return false;
-
-        for (int i = 1; i <= n - 1; i++)
-        {
-            if (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))
-                return true;
-            if (isScramble(s1.substr(0, i), s2.substr(m - i)) && isScramble(s1.substr(i), s2.substr(0, m - i)))
-                return true;
-        }
+    int n = s1.size(), m = s2.size();
+    if (s1 == s2)
+        return true;
+    string bs1 = s1, bs2 = s2;
+    sort(bs1.begin(), bs1.end()), sort(bs2.begin(), bs2.end());
+    if (bs1 != bs2)
         return false;
+
+    for (int i = 1; i <= n - 1; i++)
+    {
+        // s1的前i个字符和s2的前i个字符
+        if (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))
+            return true;
+        // s1的前i个字符和s2的后i个字符来做
+        if (isScramble(s1.substr(0, i), s2.substr(m - i)) && isScramble(s1.substr(i), s2.substr(0, m - i)))
+            return true;
     }
-};
+    return false;
+}
