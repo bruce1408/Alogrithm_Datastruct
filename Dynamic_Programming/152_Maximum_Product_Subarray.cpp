09597949f9adf4f59598ca13f6b24f169b17055f
bruce1408 <summer56567@163.com>
2021-03-06 00:34:14 +0800

    152 maxsubarray

--- Dynamic_Programming/152_Maximum_Product_Subarray.cpp
+++ Dynamic_Programming/152_Maximum_Product_Subarray.cpp
@@ -2,7 +2,18 @@
 #include<vector>
 using namespace std;
 
+/**
+ * 152 乘积最大的子数组
+ * 给定一个数组，然后找到连续的子序列是的乘积最大
+*/
+
+/**
+ * 方法 1，使用动态规划来做
+ * 状态f[i]表示数字结尾的所有的序列的方案
+ * 状态划分：
+ * 
+*/
 int maxProduct(vector<int>& nums) 
 {
-    
+
 }
