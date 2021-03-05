b4dbccdfe8e7e5f0f99d8b59061db08742f40142
bruce1408 <summer56567@163.com>
2021-03-05 20:37:14 +0800

    32

--- Dynamic_Programming/32_Longest_Valid_Parntheses.cpp
+++ Dynamic_Programming/32_Longest_Valid_Parntheses.cpp
@@ -11,6 +11,10 @@ using namespace std;
  * 
  * 输入：")()())"
  * 输出：4
+ */
+
+/**
+ * 方法 1，推荐做法，也可以用动态规划，但是状态划分比较绕
  * 使用栈来记录，栈保存的是当前字符的下标，start 变量来记录合法括号串的起始位置
 */
 int longestValidParentheses(string s)
@@ -44,7 +48,7 @@ int longestValidParentheses(string s)
  * 如果是(()长度应该是2，但是上述条件cover不到这种情况，所以就再从后往前遍历一遍即可，采用类似的机制，不过这次不是右括号大于左括号了
  * 而是，左括号大于右括号那么就置位0，最后返回res
 */
-int longestValidParentheses(string s)
+int longestValidParentheses2(string s)
 {
     int res = 0, left = 0, right = 0, n = s.size();
     for (int i = 0; i < n; ++i)
@@ -63,6 +67,31 @@ int longestValidParentheses(string s)
             res = max(res, 2 * left);
         else if (left > right) // 这里不同的地方就是左括号数大于右括号数就重新置为0
             left = right = 0;
+    }
+    return res;
+}
+
+/**
+ * 方法 3，使用动态规划来做，思路比较绕
+ * 
+*/
+int longestValidParentheses3(string s)
+{
+    if (s.empty())
+        return 0;
+    vector<int> dp(s.size() + 1, 0);
+    int res = 0;
+    for (int i = 2; i <= s.size(); i++)
+    {
+        if (s[i - 1] == ')')
+        {
+            // 这个好理解
+            if (s[i - 2] == '(')
+                dp[i] = dp[i - 2] + 2;
+            else if (i - dp[i - 1] - 1 > 0 && s[i - dp[i - 1] - 2] == '(')
+                dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2];
+            res = max(res, dp[i]);
+        }
     }
     return res;
 }
