8c789a0fc6753ed9fe740eeadb690dbeb4969460
bruce1408 <summer56567@163.com>
2021-03-06 22:50:43 +0800

    139

--- Dynamic_Programming/139_Word_Break.cpp
+++ Dynamic_Programming/139_Word_Break.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 /**
+ * 139 单词拆分；
+ * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * 说明：
+ * 
+ * 拆分时可以重复使用字典中的单词。你可以假设字典中没有重复的单词。
+ * 示例 1：
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"
+*/
+
+/**
  * 方法 1，拆分单词，然后在字典中逐个查找即可
 */
 bool dfs(string s, unordered_set<string> &wordSet, int start, vector<int> &memo)
