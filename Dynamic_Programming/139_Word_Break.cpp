d06695dce86c1c5482ffc1f1360a253e5c683530
bruce1408 <summer56567@163.com>
2021-03-06 23:33:03 +0800

    139 word break

--- Dynamic_Programming/139_Word_Break.cpp
+++ Dynamic_Programming/139_Word_Break.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 /**
@@ -7,7 +8,8 @@ using namespace std;
  * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
  * 说明：
  * 
- * 拆分时可以重复使用字典中的单词。你可以假设字典中没有重复的单词。
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
  * 示例 1：
  * 输入: s = "leetcode", wordDict = ["leet", "code"]
  * 输出: true
@@ -17,7 +19,7 @@ using namespace std;
 /**
  * 方法 1，拆分单词，然后在字典中逐个查找即可
 */
-bool dfs(string s, unordered_set<string> &wordSet, int start, vector<int> &memo)
+bool check(string s, unordered_set<string> &wordSet, int start, vector<int> &memo)
 {
     if (start >= s.size())
         return true;
@@ -25,7 +27,7 @@ bool dfs(string s, unordered_set<string> &wordSet, int start, vector<int> &memo)
         return memo[start];
     for (int i = start + 1; i <= s.size(); ++i)
     {
-        if (wordSet.count(s.substr(start, i - start)) && dfs(s, wordSet, i, memo))
+        if (wordSet.count(s.substr(start, i - start)) && check(s, wordSet, i, memo))
         {
             return memo[start] = 1;
         }
@@ -37,5 +39,12 @@ bool wordBreak(string s, vector<string> &wordDict)
 {
     unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
     vector<int> memo(s.size(), -1);
-    return dfs(s, wordSet, 0, memo);
+    return check(s, wordSet, 0, memo);
 }
+
+/**
+ * 方法 2，使用动态规划来做
+ * 状态表示：f[i]表示字符串s的0~i内地子串是否可以拆分。因为空串也可以拆分，所以f[0] = true
+ * 且f数组的长度要比s串的长度要大于1，就是因为空串的情况。
+ * 状态划分：
+*/
