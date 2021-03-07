be68f4958d88b529819ac63af914c1e89b9bfec6
bruce1408 <summer56567@163.com>
2021-03-07 16:19:23 +0800

    139 wordbreak

--- Dynamic_Programming/472_Concatenated_Words.cpp
+++ Dynamic_Programming/472_Concatenated_Words.cpp
@@ -2,7 +2,17 @@
 #include<vector>
 using namespace std;
 
+/**
+ * 472 给定一个单词数组，然后里面的单词都不重复，然后输出这个单词中所有的连接词语即可
+ * 连接词 的定义为：一个字符串完全是由至少两个给定数组中的单词组成的。
+ * 输入：words = ["cat","dog","catdog"]
+ * 输出：["catdog"]
+*/
+
+/**
+ * 方法 1，和139-140题目类似
+*/
 vector<string> findAllConcatenatedWordsInADict(vector<string>& words) 
 {
-    
+
 }
