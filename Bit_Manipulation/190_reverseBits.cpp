39c04ede56954a58cc6ec4ede34eca8aba30da0f
bruce1408 <summer56567@163.com>
2021-03-08 14:08:05 +0800

    190 reversebits

--- Bit_Manipulation/190_reverseBits.cpp
+++ Bit_Manipulation/190_reverseBits.cpp
@@ -5,9 +5,10 @@ using namespace std;
  * 给定一个无符号数字n，然后翻转这个数字
 */
 
-/** 移位操作，大概意思就是，每次数n向右移动一位，如果是
- * 1，那么res 左移一位，刚开始是0，并且加1，如果n右移一位
- * 是0，那么res左移一位之后加0.循环往复
+/** 
+ * 方法 1，移位操作，设置一个res保存结果，如果n和1与的结果是1，那么
+ * res左移一位，且加上1，如果n和1相与结果是0，那么就是左移一位加0；
+ * n每次右移一位即可
  * */
 uint32_t reverseBits(uint32_t n)
 {
@@ -25,10 +26,4 @@ uint32_t reverseBits(uint32_t n)
         n = n >> 1;
     }
     return res;
-}
-
-int main()
-{
-    uint32_t n = 987;
-    cout << reverseBits(n) << endl;
 }
