43bab55ce0db981895105c127f5f3c24d8116d17
bruce1408 <summer56567@163.com>
2021-03-09 18:10:40 +0800

    232 stack implement queue

--- Stack/232_ImplementQueueUsingStacks.cpp
+++ Stack/232_ImplementQueueUsingStacks.cpp
@@ -1,130 +1,77 @@
-/*
-  此题目的是实现队列，我们可以用链表，但是此题需要用栈来实现，
-  栈的特点是后进先出，而队列是先进先出的形式，所以思路就是用两个
-  栈来相互倒腾。这样才能实现此功能！
-  */
 #include <stack>
-#include<iostream>
+#include <iostream>
 using namespace std;
-//比较简单，两个堆栈实现
-//感觉效率非常的差
-// leetcode上类似的解法 效率也一般
-// https://discuss.leetcode.com/topic/28514/easy-java-solution-just-edit-push-method
-class MyQueue {
-  public:
-    /** Initialize your data structure here. */
-    MyQueue() {}
 
-    /** Push element x to the back of queue. */
-    void push(int x) { myStack.push(x); }
+/**
+ * 232 使用栈来实现队列
+ * 此题目的是实现队列，我们可以用链表，但是此题需要用栈来实现，
+ * 栈的特点是后进先出，而队列是先进先出的形式，所以思路就是用两个栈来相互保存。这样才能实现此功能！
+ */
 
-    /** Removes the element from in front of queue and returns that element. */
-    int pop() 
-	{
-        while (!myStack.empty()) //把主栈的元素全部给辅栈，然后主栈清空；
-		{
-            myStackTemp.push(myStack.top());
-            myStack.pop();
-        }
-        int x = myStackTemp.top();//辅栈的top元素就是队列对应的第一个元素。
-        myStackTemp.pop();        //然后删除这个元素。
-        while (!myStackTemp.empty()) //接着再把辅栈的元素装回主栈。
-		{
-            myStack.push(myStackTemp.top());
-            myStackTemp.pop();     //辅栈清空；
-        }
-        return x;
-    }
-
-    /** Get the front element. */
-    int peek()/* Get the front element*/   //返回队列首元素，但不删除
-	{
-        while (!myStack.empty()) 
-		{
-            myStackTemp.push(myStack.top());
-            myStack.pop();
-        }
-        int x = myStackTemp.top(); //没有删除的操作
-        while (!myStackTemp.empty()) 
-		{
-            myStack.push(myStackTemp.top());
-            myStackTemp.pop();
-        }
-        return x;
-    }
-
-    /** Returns whether the queue is empty. */
-    bool empty() { return myStack.empty(); }
-
-  private:
-    stack<int> myStack;//一个主栈
-    stack<int> myStackTemp;//辅助栈
-};
-
-
-
-/*MyQueue_0 leetcode上比较好的解法  代码比较优雅*/ 
-// https://discuss.leetcode.com/topic/17974/short-o-1-amortized-c-java-ruby
-class MyQueue_0 {
-  public:
-    /** Initialize your data structure here. */
-    MyQueue_0() {}
-
-    /** Push element x to the back of queue. */
-    void push(int x) { input.push(x); }
-
-    /** Removes the element from in front of queue and returns that element. */
-    int pop() {
-        int x = peek();
-        output.pop();
-        return x;
-    }
-
-    /** Get the front element. */
-    int peek() 
-	{
-        if (output.empty()) 
-		{
-            while (!input.empty()) 
-			{
-                output.push(input.top());
-                input.pop();
-            }
-        }
-        return output.top();
-    }
-
-    /** Returns whether the queue is empty. */
-    bool empty() { return input.empty() && output.empty(); }
-
-  private:
-    // stack<int> input,output;//辅助栈
-    stack<int> input;//辅助栈
-    stack<int> output;//辅助栈
-};
-
-
-int main()
+/**
+ * 方法 1，使用两个栈*
+ */
+class MyQueue
 {
-	MyQueue_0 q;
-	
-	int num;
-	cout<<"请输入数字："<<endl;
-	while(cin>>num)
-	{
-		q.push(num);
-	}
-	
-	cout<<"the top elements is: "<<q.peek()<<endl;
-	cout<<"the q num is:"<<endl;
-	
-	while(!q.empty())
-	{
-		cout<<q.pop()<<" ";
-	}
+public:
+    /** Initialize your data structure here. */
+    stack<int> a, b;
+    MyQueue()
+    {
+    }
 
-	
-	return 0;
-		
-	
-}
+    /** Push element x to the back of queue. */
+    void push(int x)
+    {
+        a.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop()
+    {
+        while (a.size() > 1)
+        {
+            b.push(a.top());
+            a.pop();
+        }
+        int t = a.top();
+        a.pop();
+        while (b.size())
+        {
+            a.push(b.top());
+            b.pop();
+        }
+        return t;
+    }
+
+    /** Get the front element. */
+    int peek()
+    {
+        while (a.size() > 1)
+        {
+            b.push(a.top());
+            a.pop();
+        }
+        int t = a.top();
+        while (b.size())
+        {
+            a.push(b.top());
+            b.pop();
+        }
+        return t;
+    }
+    /** Returns whether the queue is empty. */
+    bool empty()
+    {
+        return a.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
