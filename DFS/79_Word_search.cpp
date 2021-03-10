cf9fb0c42695f0439e886a7e007be483ce008b81
bruce1408 <summer56567@163.com>
2021-03-11 00:32:47 +0800

    word search

--- DFS/79_Word_search.cpp
+++ DFS/79_Word_search.cpp
@@ -12,30 +12,35 @@ using namespace std;
 /**
  * 方法 1，使用深度优先搜索
 */
-bool dfs(vector<vector<char>> &board, string &word, int start, int x, int y)
+bool dfs(vector<vector<char> > &board, string word, int start, int x, int y)
 {
 	if (board[x][y] != word[start])
 		return false;
 	if (start == word.size() - 1)
 		return true;
-	int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-
 	char t = board[x][y];
-	board[x][y] = '.'; // 用点表示已经走过了这个格子了
+	board[x][y] = '.';
+	int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 	for (int i = 0; i < 4; i++)
 	{
 		int a = dx[i] + x, b = dy[i] + y;
-		if (a < 0 || a >= board.size() || b < 0 || b >= board[0].size() || board[a][b] == '.')
-			continue;
-		if (dfs(board, word, start + 1, a, b))
-			return true;
+		// 方法 1
+		// if(a < 0 || a >= board.size() || b < 0 || b >= board[0].size() || board[a][b]=='.') continue;
+		// if(dfs(board, word, start+1, a, b)) return true;
+		// 方法 2
+		if (a >= 0 && a < board.size() && b >= 0 && b < board[0].size())
+		{
+			if (dfs(board, word, start + 1, a, b))
+				return true;
+		}
 	}
 	board[x][y] = t;
 	return false;
 }
 
-bool exist(vector<vector<char>> &board, string word)
+bool exist(vector<vector<char> > &board, string word)
 {
+
 	for (int i = 0; i < board.size(); i++)
 	{
 		for (int j = 0; j < board[i].size(); j++)
@@ -47,17 +52,56 @@ bool exist(vector<vector<char>> &board, string word)
 	return false;
 }
 
+/**
+ * 方法 2，优化上面的代码项目，推荐做法
+ */
+bool dfs1(vector<vector<char> > &board, string &word, int x, int y, int u, vector<vector<bool> > &visited)
+{
+	if (board[x][y] != word[u])
+		return false;
+	if (u == word.size() - 1)
+		return true;
+	visited[x][y] = true;
+	int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+	for (int i = 0; i < 4; i++)
+	{
+		int a = dx[i] + x, b = dy[i] + y;
+		if (a >= 0 && a < board.size() && b >= 0 && b < board[0].size() && visited[a][b] == false)
+		{
+			if (dfs1(board, word, a, b, u + 1, visited))
+				return true;
+		}
+	}
+	visited[x][y] = false;
+	return false;
+}
+
+bool exist1(vector<vector<char> > &board, string word)
+{
+	int n = board.size(), m = board[0].size();
+	vector<vector<bool> > visited(n, vector<bool>(m, false));
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			if (dfs1(board, word, i, j, 0, visited))
+				return true;
+		}
+	}
+	return false;
+}
+
 int main()
 {
-	vector<vector<char>> matrix_char =
+	vector<vector<char> > matrix_char =
 		{
 			{'a', 'b', 't', 'g'},
 			{'c', 'f', 'c', 's'},
 			{'j', 'd', 'e', 'h'}};
 
-	string a = "absfd";
+	string a = "abfdeh";
 
-	if (exist(matrix_char, a))
+	if (exist1(matrix_char, a))
 		cout << "find a path in matrix;" << endl;
 	else
 		cout << a << " :has no path in matrix;" << endl;
