dab1276e4a508d7bd379d67378252d349a0e4f2c
bruce1408 <summer56567@163.com>
2021-03-11 01:01:09 +0800

    160 intersection

--- Linked_List/160_Intersection_ofTwoLinkedLists.cpp
+++ Linked_List/160_Intersection_ofTwoLinkedLists.cpp
@@ -57,7 +57,7 @@ ListNode *getIntersectionNode1(ListNode *headA, ListNode *headB)
 /**
  * 方法 2，是利用环的思想。推荐写法
  * 题目中强调了链表中不存在环，但是我们可以用环的思想来做，
- * 我们让两条链表分别从各自的开头开始往后遍历，当其中一条遍历到末尾时，我们跳到另一个条链表的开头继续遍历。
+ * 我们让两条链表分别从各自的开头开始往后遍历，当其中一条遍历到末尾时，该链表跳到另一个条链表的开头继续遍历，另一个链表也是如此
  * 两个指针最终会相等，而且只有两种情况，一种情况是在交点处相遇，另一种情况是在各自的末尾的空节点处相等。
  * 为什么一定会相等呢，因为两个指针走过的路程相同，是两个链表的长度之和，所以一定会相等。
  * a+b+c = a+c+b
@@ -77,8 +77,8 @@ ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB)
 	ListNode *a = headA, *b = headB;
 	while (a != b)
 	{
-		a = a ? a->next : headB;
-		b = b ? b->next : headA;
+		a = a ? a->next : headB; // a如果不是空的情况，a = a->next,否则a指向b
+		b = b ? b->next : headA; // if(b) b = b->next;else b = headA;
 	}
 	return a;
 }
