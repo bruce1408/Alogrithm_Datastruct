abe36c94f89f45f44dae2f645cf07f17ac260157
bruce1408 <summer56567@163.com>
2021-03-12 03:03:52 +0800

    update code list

--- Linked_List/142_Linked_List_Cycle_II.cpp
+++ Linked_List/142_Linked_List_Cycle_II.cpp
@@ -1,33 +1,69 @@
 #include<iostream>
-// 判断环的入口，在相遇的地方，让一个指针回到head，然后每次走一步即可。
+using namespace std;
 /**
+ * 142 找到环的入口的链表
  * 题目的要求是一个带环的链表，如果有环，那么给出这个环的入口，否则返回null
  * 思路：
  * 判断环的入口，设置两个指针，一个快指针每次走两步，一个慢指针每次走一步，如果相遇，那么就让慢指针回到head第一个节点，然后快慢指针
  * 每次都走一步，下次再相遇就是环的入口处。
 */
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x):val(x),next(NULL){}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*slow = head, *fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) break;
+        }
+        if(fast==NULL || fast->next==NULL) return NULL;
+        fast = head;
+        while(fast != slow)
+        {
+            slow = slow ->next; 
+            fast = fast->next;
+        }
+        return fast; 
+    }
 };
 
-ListNode *detectCycle(ListNode *head)
-{
-    ListNode * slow = head, *fast = head;
-    while(fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if(fast == slow) break;
+
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next) return 0;
+        ListNode *first = head, *second = head;
+
+        while (first && second)
+        {
+            first = first->next;
+            second = second->next;
+            if (second) second = second->next;
+            else return 0;
+
+            if (first == second)
+            {
+                first = head;
+                while (first != second)
+                {
+                    first = first->next;
+                    second = second->next;
+                }
+                return first;
+            }
+        }
+
+        return 0;
     }
-    if(!fast || !fast->next) return NULL;
-    slow = head;
-    while(slow!=fast)
-    {
-        slow = slow->next;
-        fast = fast->next;
-    }
-    return slow;
-}
+};
