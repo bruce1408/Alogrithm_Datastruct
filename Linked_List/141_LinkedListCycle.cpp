78aabf94589943bb8b4ba8a8ef4e3c0ff7a440f6
bruce1408 <summer56567@163.com>
2021-03-12 02:55:15 +0800

    update coda list

--- Linked_List/141_LinkedListCycle.cpp
+++ Linked_List/141_LinkedListCycle.cpp
@@ -1,8 +1,6 @@
-/*判断是否是有链表环*/
 #include <iostream>
 #include <cstring>
 using namespace std;
-
 struct ListNode
 {
     char c;
@@ -10,20 +8,10 @@ struct ListNode
     ListNode(char x, ListNode *next_) : c(x), next(next_) {}
 };
 
-// int main()
-// {
-// 	ListNode *p = head;
-// 	ListNode *head = &a;
-// 	ListNode a = {'a',&b};
-// 	ListNode b = {'b',&c};
-// 	ListNode c = {'c',&d};
-// 	ListNode d = {'d',&e};
-// 	ListNode e = {'e',&f};
-// 	ListNode f = {'f',&d};
-// 	return 0;
-// }
-
-/*判断是否链表环*/
+/**
+ * 141 判断链表啊是否有环
+ * 判断是否链表环
+ * */
 bool hasCycle(struct ListNode *head)
 {
     ListNode *fast = head;
@@ -39,7 +27,9 @@ bool hasCycle(struct ListNode *head)
     return false; //如果到最后没有跳出，那么就是说链表没有环
 }
 
-/*求链表环的第一个结点*/
+/**
+ * 求链表环的第一个结点
+ * */
 class Solution
 {
 public:
@@ -51,21 +41,6 @@ public:
 
         ListNode *fast = head;
         ListNode *slow = head;
-
-        // do
-        // {
-        // if(fast != NULL)
-        // fast = fast->next;
-        // else
-        // return NULL;
-        // if(fast != NULL)
-        // fast = fast->next;
-        // else
-        // return NULL;
-        // slow = slow->next;
-        // }while(fast != slow);
-
-        //这种方法有错误
         while (fast->next->next && fast->next)
         {
 
@@ -86,53 +61,3 @@ public:
         return slow;
     }
 };
-
-/*C++寻找环的第一个入口交点的第二种方法*/
-
-// ListNode *detectCycle(ListNode *head) {
-// if (head == NULL || head->next == NULL)
-// return NULL;
-
-// ListNode *slow  = head;
-// ListNode *fast  = head;
-// ListNode *entry = head;
-
-// while (fast->next && fast->next->next) {
-// slow = slow->next;
-// fast = fast->next->next;
-// if (slow == fast) {                      // there is a cycle
-// while(slow != entry) {               // found the entry location
-// slow  = slow->next;
-// entry = entry->next;
-// }
-// return entry;
-// }
-// }
-// return NULL;                                 // there has no cycle
-// }
-
-// 判断链表是否有环。代码的循环条件是while(fast && fast->next)
-while (fast && fast->next)
-{
-    slow = slow->next;
-    fast = fast->next->next;
-    if (fast == slow)
-        return true; //如果是环一定会推出循环，否则不是换，最后一个链表也会推出循环
-}
-// 另外判断一个环的入口怎么办，就是在两个环第一次相交的地方，停下来，让其中一个指针从头开始，然后两个指针每次走一步。再次相遇的地方就是入口的交点
-while (fast && fast->next)
-{
-    slow = slow->next;
-    fast = fast->next->next;
-    if (slow == fast)
-        break;
-}
-if (!fast || !fast->next)
-    return NULL;
-slow = head;
-while (slow != fast)
-{
-    slow = slow->next;
-    fast = fast->next;
-}
-return slow;
