fd5012381125c094fdad77d444da058df4a9e22f
bruce1408 <summer56567@163.com>
2021-04-07 03:08:14 +0800

    update code

--- Binary_Search/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ Binary_Search/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -49,6 +49,31 @@ int findMin2(vector<int> &nums)
     return nums[l];
 }
 
+/**
+ * 方法 3，使用二分查找，推荐算法
+ * 使用二分，这里考虑就是如果旋转之后前后可能会有相同的元素的情况。
+ * 在这种情况下，我们去掉后面旋转后的元素的重复值即可
+*/
+int findMin(vector<int> &nums)
+{
+    int l = 0, r = nums.size() - 1;
+    // 如果有前后相同的元素，直接去掉后面的元素避免二分失效
+    while (l < r && nums[l] == nums[r])
+        r--;
+    if (nums[l] <= nums[r])
+        return nums[l];
+
+    while (l < r)
+    {
+        int mid = (l + r) >> 1;
+        if (nums[mid] < nums[0])
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    return nums[l];
+}
+
 int main()
 {
     vector<int> res = {4, 5, 6, 7, 0, 1, 2};
