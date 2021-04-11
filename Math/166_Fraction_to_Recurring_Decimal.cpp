cce8cb0cdb81fa7739dabadf74167e1911272b69
bruce1408 <summer56567@163.com>
2021-04-11 19:52:41 +0800

    update code

--- Math/166_Fraction_to_Recurring_Decimal.cpp
+++ Math/166_Fraction_to_Recurring_Decimal.cpp
@@ -7,28 +7,33 @@ using namespace std;
 */
 
 /**
- * 方法 1
+ * 方法 1，按照正常的逻辑思路一步一步求解即可
 */
-string fractionToDecimal(int s, int t)
+string fractionToDecimal(int x1, int y1)
 {
     typedef long long LL;
-    LL x = s, y = t;
+    LL x = x1, y = y1;
     if (x % y == 0)
         return to_string(x / y);
     string res;
-    if (x < 0 ^ y < 0)
+
+    // 如果有负数的情况
+    if ((x < 0) ^ (y < 0))
         res += '-';
     x = abs(x), y = abs(y);
+
+    // 整数部分
     res += to_string(x / y) + '.';
-    x = x % y;
+    x %= y;
+
+    // 余数部分开始
     unordered_map<LL, int> hash;
     while (x)
     {
         hash[x] = res.size();
         x *= 10;
         res += to_string(x / y);
-        x = x % y;
-
+        x %= y;
         if (hash.count(x))
         {
             res = res.substr(0, hash[x]) + '(' + res.substr(hash[x]) + ')';
