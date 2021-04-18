fda628bffdf6a29b4aa3f899d36f21e7ecbb7655
bruce1408 <summer56567@163.com>
2021-04-18 19:04:53 +0800

    pdate

--- Dynamic_Programming/123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
+++ Dynamic_Programming/123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /**
@@ -9,9 +9,20 @@ using namespace std;
 
 /**
  * 方法 1，凡是交易两次的问题，在枚举的时候使用前后缀分解来做！
- * 
 */
-int maxProfit(vector<int>& prices) 
+int maxProfit(vector<int> &prices)
 {
-     
+    if (prices.empty())
+        return 0;
+    int n = prices.size(), g[n][3] = {0}, l[n][3] = {0};
+    for (int i = 1; i < prices.size(); ++i)
+    {
+        int diff = prices[i] - prices[i - 1];
+        for (int j = 1; j <= 2; ++j)
+        {
+            l[i][j] = max(g[i - 1][j - 1] + max(diff, 0), l[i - 1][j] + diff);
+            g[i][j] = max(l[i][j], g[i - 1][j]);
+        }
+    }
+    return g[n - 1][2];
 }
