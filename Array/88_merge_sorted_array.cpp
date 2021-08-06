c411159851330b664ea54157b1b59439cd821ab9
bruce1408 <summer56567@163.com>
2021-08-06 11:51:27 +0800

    new code in leetcode

--- Array/88_merge_sorted_array.cpp
+++ Array/88_merge_sorted_array.cpp
@@ -37,7 +37,7 @@ int main()
 {
     vector<int> a = {1, 2, 3, 0, 0, 0};
     vector<int> b = {2, 5, 6};
-    merge2(a, 3, b, 3);
+    merge1(a, 3, b, 3);
     for (auto i : a)
     {
         cout << i << " ";
